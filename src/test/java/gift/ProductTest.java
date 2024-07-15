21f89cb9f95ebc98ac673dc2c05cd7194e688bc0
doyooon <ian661087@gmail.com>
2024-07-15 16:39:46 +0900

    refactor ProductTest.java : 스프링 컨테이너가 띄워지지 않는 환경으로 단위테스트 작성

--- src/test/java/gift/ProductTest.java
+++ src/test/java/gift/ProductTest.java
@@ -2,32 +2,27 @@ package gift;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 import gift.product.Product;
-import gift.product.ProductService;
-import jakarta.validation.ConstraintViolationException;
+import gift.product.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class ProductTest {
-    @Autowired
-    private ProductService productService;
 
     @DisplayName("비즈니스 로직 테스트")
     @Test
     void domain(){
+        ProductRepository productRepository = mock(ProductRepository.class);
         //given
-        Product product = new Product(null, "카카오", 1234, null);
+        String name = "카카오123";
 
         //when
-        ConstraintViolationException exception = assertThrows(ConstraintViolationException.class, ()-> productService.createProduct(product));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()-> new Product(null,name,1234,null));
 
         //then
-        assertThat(exception.getMessage()).isEqualTo("createProduct.newProduct.name: \"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.");
-
+        assertThat(exception.getMessage()).isEqualTo("\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.");
     }
 
 }
