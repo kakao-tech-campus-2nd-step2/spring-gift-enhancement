f79d5607d4815bf53207dc577037415ec8ecc7e3
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-15 14:06:06 +0900

    Feat: 카테고리 도메인 설계
    카테고리 엔티티 설계를 구현했다. category의 필드는 id, name, color,
    imageUrl, description으로 이뤄져 있으며 모든 필드는 null이 불가하도록
    설정했다.

---
+++ src/main/java/gift/model/Category.java
@@ -0,0 +1,43 @@
+package gift.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String color;
+    @Column(nullable = false)
+    private String imageUrl;
+    @Column(nullable = false)
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String color, String imageUrl, String description) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public Category(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public void updateCategory(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+}
