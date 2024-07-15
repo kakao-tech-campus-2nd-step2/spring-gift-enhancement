79652a9e3cbbbccceb5c08f2fd70251cb87fe2be
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-15 14:02:01 +0900

    feat: Category 엔티티, 레포지토리 추가

---
+++ src/main/java/gift/entity/Category.java
@@ -0,0 +1,50 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String color;
+
+    private String imageUrl;
+
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
