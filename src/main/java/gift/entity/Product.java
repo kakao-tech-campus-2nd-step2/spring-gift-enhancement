dcaca7cc74710f5158fd70a422547a345a1ed954
us4c0d3 <jwseok312@gmail.com>
2024-07-15 16:19:33 +0900

    feat(Category): 수정된 요구사항 및 제공된 DDL에 맞게 엔티티 수정 및 관계 매핑

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -2,9 +2,12 @@ package gift.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,11 @@ public class Product {
 
     @Column(nullable = false)
     private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false,
+        foreignKey = @ForeignKey(name = "fk_product_category_id_ref_categories_id"))
+    private Category category;
 
     protected Product() {
     }
