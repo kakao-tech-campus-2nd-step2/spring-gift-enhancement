521705adab3758127085ac1053e0f668a0d1431e
Woojae Jeong <woo6388@naver.com>
2024-07-15 16:56:28 +0900

    style : 불필요한 import문 삭제

--- src/main/java/gift/Repository/WishRepository.java
+++ src/main/java/gift/Repository/WishRepository.java
@@ -2,13 +2,10 @@ package gift.Repository;
 
 import gift.Model.Member;
 import gift.Model.Product;
-import gift.Model.ResponseWishDTO;
 import gift.Model.Wish;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
