b7178295d2c085a458d8a60d125db6dd7d1b9a02
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-15 13:15:53 +0900

    feat: 상품의 존재 여부 검증 추가

--- src/main/java/gift/wishlist/service/WishService.java
+++ src/main/java/gift/wishlist/service/WishService.java
@@ -2,6 +2,8 @@ package gift.wishlist.service;
 
 import gift.common.exception.WishNotFoundException;
 import gift.member.model.Member;
+import gift.product.model.Product;
+import gift.product.repository.ProductRepository;
 import gift.wishlist.dto.WishRequest;
 import gift.wishlist.dto.WishResponse;
 import gift.wishlist.model.Wish;
@@ -18,15 +20,18 @@ import org.springframework.stereotype.Service;
 public class WishService {
 
     private final WishRepository wishRepository;
+    private final ProductRepository productRepository;
 
-    public WishService(WishRepository wishRepository) {
+    public WishService(WishRepository wishRepository, ProductRepository productRepository) {
         this.wishRepository = wishRepository;
+        this.productRepository = productRepository;
     }
 
     public WishResponse addWish(Member member, WishRequest request) {
+        Product product = validateProductExist(request.getProduct().getId());
         Wish wish = new Wish();
         wish.setMember(member);
-        wish.setProduct(request.getProduct());
+        wish.setProduct(product);
 
         wishRepository.save(wish);
         return new WishResponse(wish.getId(), wish.getProduct());
@@ -58,6 +63,12 @@ public class WishService {
             throw new WishNotFoundException("Wishlist에 ID: " + id + "인 상품은 존재하지 않습니다.");
         }
         wishRepository.deleteById(id);
+    }
+
+    private Product validateProductExist(Long productId) {
+        return productRepository.findById(productId)
+            .orElseThrow(() -> new IllegalArgumentException(
+                "Product ID: " + productId + "인 상품은 존재하지 않습니다."));
     }
 
 }
