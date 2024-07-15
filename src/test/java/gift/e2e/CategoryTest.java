dd9ee69e70d7f8bf1f229aac7049c37a46357a19
us4c0d3 <jwseok312@gmail.com>
2024-07-16 02:16:43 +0900

    feat(Category): 특정 id의 카테고리를 조회하는 getCategory 기능 추가

--- src/test/java/gift/e2e/CategoryTest.java
+++ src/test/java/gift/e2e/CategoryTest.java
@@ -2,6 +2,7 @@ package gift.e2e;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import gift.dto.category.CategoryResponse;
 import java.net.URI;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,5 +42,36 @@ public class CategoryTest {
         // then
         assertThat(actualResponse.getStatusCode().is2xxSuccessful()).isTrue();
         assertThat(actualResponse.getBody()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("get id 1L category test")
+    void getId1LCategoryTest() {
+        // given
+        var url = "http://localhost:" + port + "/api/categories/1";
+        var request = new RequestEntity<>(HttpMethod.GET, URI.create(url));
+
+        // when
+        var actualResponse = restTemplate.exchange(request, CategoryResponse.class);
+
+        // then
+        assertThat(actualResponse.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(actualResponse.getBody()).isNotNull();
+        assertThat(actualResponse.getBody().id()).isEqualTo(1L);
+        assertThat(actualResponse.getBody().name()).isEqualTo("Category 1");
+    }
+
+    @Test
+    @DisplayName("get category by not exist id test")
+    void getCategoryByNotExistIdTest() {
+        // given
+        var url = "http://localhost:" + port + "/api/categories/999";
+        var request = new RequestEntity<>(HttpMethod.GET, URI.create(url));
+
+        // when
+        var actualResponse = restTemplate.exchange(request, CategoryResponse.class);
+
+        // then
+        assertThat(actualResponse.getStatusCode().is4xxClientError()).isTrue();
     }
 }
