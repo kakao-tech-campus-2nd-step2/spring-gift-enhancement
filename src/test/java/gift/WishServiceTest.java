7ab8de051229ed42235c1d4b779adb5088fbab96
doyooon <ian661087@gmail.com>
2024-07-15 19:42:16 +0900

    refactor WishServiceTest.java : 반환 타입 수정

--- src/test/java/gift/WishServiceTest.java
+++ src/test/java/gift/WishServiceTest.java
@@ -7,14 +7,19 @@ import gift.member.Member;
 import gift.member.MemberRepository;
 import gift.product.Product;
 import gift.product.ProductRepository;
-import gift.wishes.Wish;
+import gift.wishes.WishPageResponse;
+import gift.wishes.WishRepository;
+import gift.wishes.WishResponse;
 import gift.wishes.WishService;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 
 @SpringBootTest
+@Transactional
 public class WishServiceTest {
 
     @Autowired
@@ -23,10 +28,15 @@ public class WishServiceTest {
     private MemberRepository memberRepository;
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private WishRepository wishRepository;
 
-    @Test
-    void pageTest(){
-        Member member = new Member(null, "user", "user@gmail.com", "1234");
+    @BeforeEach
+    void setup(){
+        memberRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        wishRepository.deleteAllInBatch();
+        Member member = new Member(1L, "user", "user@gmail.com", "1234");
         Member newMember = memberRepository.save(member);
 
         for(int i = 0;i<100;i++){
@@ -34,12 +44,21 @@ public class WishServiceTest {
             Product newProduct = productRepository.save(product);
             wishService.createWish(newMember.getId(), newProduct.getId(), 1L);
         }
-
-        Page<Wish> wishPage = wishService.getWishPage(newMember.getId(), 0);
+        productRepository.flush();
+    }
+    @Test
+    void pageTest(){
+        WishPageResponse wishPage = wishService.getWishPage(1L, 0);
         assertAll(
-            () -> assertThat(wishPage.getTotalPages()).isEqualTo(10),
-            () -> assertThat(wishPage.getTotalElements()).isEqualTo(100),
-            () -> assertThat(wishPage.getSize()).isEqualTo(10)
+            () -> assertThat(wishPage.totalPage()).isEqualTo(10),
+            () -> assertThat(wishPage.totalElements()).isEqualTo(100),
+            () -> assertThat(wishPage.currentPage()).isEqualTo(0)
         );
+    }
+
+    @Test
+    void listTest(){
+        List<WishResponse> wishResponseList = wishService.findByMemberId(2L);
+        assertThat(wishResponseList.size()).isEqualTo(100);
     }
 }
