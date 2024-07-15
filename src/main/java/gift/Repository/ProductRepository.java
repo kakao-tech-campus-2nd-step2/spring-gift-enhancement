31e36f213043404e2503a3e849e37286b8398b3c
Woojae Jeong <woo6388@naver.com>
2024-07-15 16:56:12 +0900

    refactor : findAll 메소드 정의 삭제

--- src/main/java/gift/Repository/ProductRepository.java
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package gift.Repository;
 
 import gift.Model.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    Page<Product> findAll(Pageable pageable);
 }
