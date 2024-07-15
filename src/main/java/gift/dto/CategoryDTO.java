ad6bee93622a51e0e457c4e40e4eecd77054f525
kuma4188 <psb4188@gmail.com>
2024-07-15 14:37:18 +0900

    feat : categoryDTO 생성

---
+++ src/main/java/gift/dto/CategoryDTO.java
@@ -0,0 +1,59 @@
+package gift.dto;
+
+
+public class CategoryDTO {
+    private int id;
+    private String name;
+    private String color;
+    private String imageUrl;
+    private String description;
+
+    public CategoryDTO(int id, String name, String color, String imageUrl, String description) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
