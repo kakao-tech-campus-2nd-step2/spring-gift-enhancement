1d9905df2f81cd36c66171439dbe90262f826df8
xkrwjdals <developjmt@gmail.com>
2024-07-15 20:33:09 +0900

    feat: Implement CategoryRepositoryTest
    - Modify ProductRepositoryTest and WishListRepositoryTest to use Category entity

--- src/test/java/gift/ProductRepositoryTest.java
+++ src/test/java/gift/ProductRepositoryTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import gift.exception.ErrorCode;
 import gift.exception.RepositoryException;
+import gift.model.Category;
 import gift.model.Product;
+import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,9 +21,16 @@ public class ProductRepositoryTest {
 
     @Autowired
     private ProductRepository products;
+    @Autowired
+    private CategoryRepository categories;
 
+    private final Category category = new Category(1L, "test_category", "000000", "category-image.url", "");
     private Product createProduct(long id, String name, long price, String imageUrl) {
-        return new Product(id, name, price, imageUrl);
+        return new Product(id, name, price, imageUrl, category);
+    }
+    @BeforeEach
+    void setUp() {
+        categories.save(category);
     }
 
     @Test
@@ -47,7 +57,7 @@ public class ProductRepositoryTest {
     @Test
     void findAll() {
         var product1 = createProduct(1L, "Product A", 4500, "image-productA.url");
-        var product2 = createProduct(2L, "Product B", 4500, "image-productB.url");
+        var product2 = createProduct(2L, "Product B", 5500, "image-productB.url");
         products.save(product1);
         products.save(product2);
         assertThat(products.findAll().size()).isEqualTo(2);
@@ -56,8 +66,8 @@ public class ProductRepositoryTest {
     @Test
     void findPage() {
         var product1 = createProduct(1L, "Product A", 4500, "image-productA.url");
-        var product2 = createProduct(2L, "Product B", 4500, "image-productB.url");
-        var product3 = createProduct(3L, "Product C", 4500, "image-productC.url");
+        var product2 = createProduct(2L, "Product B", 5500, "image-productB.url");
+        var product3 = createProduct(3L, "Product C", 6500, "image-productC.url");
         products.save(product1);
         products.save(product2);
         products.save(product3);
