e478e6112b4cadad439f345b94768721cb344e81
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-15 16:54:48 +0900

    feat : Product Service 카테고리 동기화 처리 등

--- src/main/java/gift/product/ProductService.java
+++ src/main/java/gift/product/ProductService.java
@@ -1,5 +1,8 @@
 package gift.product;
 
+import gift.category.Category;
+import gift.category.CategoryRepository;
+import gift.category.CategoryService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,9 +16,11 @@ import java.util.Objects;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Page<Product> getProductPages(int pageNum, int size, String sortBy, String sortDirection) {
@@ -32,7 +37,10 @@ public class ProductService {
     }
 
     public Product insertNewProduct(ProductDTO newProduct) {
-        Product product = new Product(newProduct.getName(), newProduct.getPrice(), newProduct.getImageUrl());
+        Long categoryID = newProduct.getCategoryID();
+        Category category = categoryRepository.findById(categoryID).orElseThrow();
+        Product product = new Product(newProduct.getName(), newProduct.getPrice(), newProduct.getImageUrl(), category);
+        category.addProduct(product);
         return productRepository.save(product);
     }
 
@@ -42,11 +50,19 @@ public class ProductService {
         product.setName(updateProduct.getName());
         product.setPrice(updateProduct.getPrice());
         product.setImageUrl(updateProduct.getImageUrl());
+
+        product.getCategory().removeProduct(product);
+        Category category = categoryRepository.findById(updateProduct.getCategoryID()).orElseThrow();
+        product.setCategory(category);
+        category.addProduct(product);
+
         return product;
     }
 
     public void deleteProduct(Long id) {
-        if (findByID(id) != null) productRepository.deleteById(id);
+        Product product = productRepository.findById(id).orElseThrow();
+        product.getCategory().removeProduct(product);
+        productRepository.deleteById(id);
     }
 
     public List<Product> findAllProducts() {
