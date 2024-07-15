5819a1842f99c65659ce557f95c71f2829c64e51
Profile-exe <kang67572346@gmail.com>
2024-07-15 15:50:26 +0900

    feat(Category): 카테고리 엔티티 구현

---
+++ src/main/java/gift/category/entity/Category.java
@@ -0,0 +1,46 @@
+package gift.category.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
