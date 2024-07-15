45356ec838b8186c2828f27523abb3a8a36a6299
Dalsungmin <cnww123@naver.com>
2024-07-15 14:50:55 +0900

    Step0 기본 코드 준비
    일단 지난주 3단계에 제출했던 최종 코드 불러왔습니다!

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,84 @@
+package gift.controller;
+
+import gift.exception.InvalidProductException;
+import gift.exception.ProductNotFoundException;
+import gift.model.Product;
+import gift.service.ProductService;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController( ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String getProducts(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size, Model model) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> productPage = productService.getAllProducts(pageable);
+        model.addAttribute("productPage", productPage);
+        model.addAttribute("product", new Product());
+        return "product-list";
+    }
+
+    @PostMapping("/add")
+    public String addProduct(@Valid @ModelAttribute Product product, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            return "product-list";
+        }
+
+        productService.addProduct(product);
+        redirectAttributes.addFlashAttribute("message", "Product added successfully!");
+        return "redirect:/products";
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(@Valid @ModelAttribute Product product, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            return "product-list";
+        }
+        productService.updateProduct(product.getId(), product);
+        redirectAttributes.addFlashAttribute("message", "Product updated successfully!");
+        return "redirect:/products";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+        productService.deleteProduct(id);
+        redirectAttributes.addFlashAttribute("message", "Product deleted successfully!");
+        return "redirect:/products";
+    }
+
+    @GetMapping("/view/{id}")
+    public String getProductDetails(@PathVariable("id") Long id, Model model, RedirectAttributes redirectAttributes) {
+        Optional<Product> product = productService.getProductById(id);
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+            return "product-detail"; // product-details.html 뷰 반환
+        } else {
+            redirectAttributes.addFlashAttribute("errorMessage", "Product not found");
+            return "redirect:/products";
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/gift/model/Member.java
@@ -0,0 +1,64 @@
+package gift.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "member_table")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WishList> wishlists = new ArrayList<>();
+
+
+
+    public Member() {
+    }
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
