3529335ff3670eedb4c514700642551bb3f693f3
test1 <test>
2024-07-16 01:44:39 +0900

    fix : getter 수정

--- src/main/java/gift/DTO/CategoryDto.java
+++ src/main/java/gift/DTO/CategoryDto.java
@@ -33,9 +33,9 @@ public class CategoryDto {
 
   public void setImageUrl(String imageUrl){this.imageUrl=imageUrl;}
 
-  public String getImageUrl(){return this.getImageUrl();}
+  public String getImageUrl(){return this.imageUrl;}
 
   public void setDescription(String description){this.description=description;}
 
-  public String getDescription(){return this.getDescription();}
+  public String getDescription(){return this.description;}
 }
