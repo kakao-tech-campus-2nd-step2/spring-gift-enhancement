978b9dcc6920a1f9f6f7a2134b3959bfcb7e8709
us4c0d3 <jwseok312@gmail.com>
2024-07-16 02:17:05 +0900

    style: reformatting

--- src/test/resources/sql/insert_three_categories.sql
+++ src/test/resources/sql/insert_three_categories.sql
@@ -1,6 +1,11 @@
-DELETE FROM categories;
-ALTER TABLE categories ALTER COLUMN id RESTART WITH 1;
+DELETE
+FROM categories;
+ALTER TABLE categories
+    ALTER COLUMN id RESTART WITH 1;
 
-INSERT INTO categories (name) VALUES ('Category 1');
-INSERT INTO categories (name) VALUES ('Category 2');
-INSERT INTO categories (name) VALUES ('Category 3');
+INSERT INTO categories (name)
+VALUES ('Category 1');
+INSERT INTO categories (name)
+VALUES ('Category 2');
+INSERT INTO categories (name)
+VALUES ('Category 3');
