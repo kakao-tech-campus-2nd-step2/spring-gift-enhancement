17ae0fdb8ea3e5d07a32938e5e00b88931ab0744
ajy4304 <ajy4304@naver.com>
2024-07-16 19:59:12 +0900

    feat(CategoryController): 카테고리 컨트롤러 추가

---
+++ src/main/java/gift/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package gift.controller;
+
+import gift.model.Category;
+import gift.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    // 모든 카테고리 조회
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+}
