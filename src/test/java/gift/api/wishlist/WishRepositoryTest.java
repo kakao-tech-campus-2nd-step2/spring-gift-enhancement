97dd046022d0b771ff8f37de30d2d58e47ce75de
Hyewon Seok <hws2008@naver.com>
2024-07-16 22:34:37 +0900

    test: fix tests due to category

--- src/test/java/gift/api/wishlist/WishRepositoryTest.java
+++ src/test/java/gift/api/wishlist/WishRepositoryTest.java
@@ -2,11 +2,6 @@ package gift.api.wishlist;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import gift.api.member.Member;
-import gift.api.member.MemberRepository;
-import gift.api.member.Role;
-import gift.api.product.Product;
-import gift.api.product.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,23 +10,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 class WishRepositoryTest {
 
     @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private ProductRepository productRepository;
-    @Autowired
     private WishRepository wishRepository;
 
     @Test
     void deleteById() {
-        Member member = new Member("member@test.com", "passWD12", Role.USER);
-        Product product = new Product("product", 1000, "https://test/image");
-        Wish wish = new Wish(member, product, 5);
+        WishId wishId = new WishId(1L, 1L);
+        assertThat(wishRepository.existsById(wishId)).isTrue();
 
-        memberRepository.save(member);
-        productRepository.save(product);
-        wishRepository.save(wish);
-
-        WishId wishId = new WishId(member.getId(), product.getId());
         wishRepository.deleteById(wishId);
 
         assertThat(wishRepository.findById(wishId)).isEmpty();
