a82c1272c75aedfc71b147813d41e5bb16b25dec
momnpa333 <momnpa333@gmail.com>
2024-07-16 23:52:19 +0900

    feat: findById N+1 때문에 fetchjoin 사용

--- src/main/java/gift/repository/product/ProductJpaRepository.java
+++ src/main/java/gift/repository/product/ProductJpaRepository.java
@@ -1,6 +1,7 @@
 package gift.repository.product;
 
 import gift.model.product.Product;
+import java.util.Optional;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,4 +19,8 @@ public interface ProductJpaRepository extends JpaRepository<Product, Long>, Prod
 
     @Override
     Page<Product> findByNameContaining(String name, Pageable pageable);
+
+    @Override
+    @Query("SELECT p FROM Product p JOIN FETCH p.category WHERE p.id = :id")
+    Optional<Product> findById(Long id);
 }
