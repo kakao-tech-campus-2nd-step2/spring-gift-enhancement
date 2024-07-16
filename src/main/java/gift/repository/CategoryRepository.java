f6a63596c098cc52816c896f246f4e7797e34f7e
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-16 22:29:45 +0900

    feat: Add Category Repository

---
+++ src/main/java/gift/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package gift.repository;
+
+import gift.entity.Category;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+
+}
