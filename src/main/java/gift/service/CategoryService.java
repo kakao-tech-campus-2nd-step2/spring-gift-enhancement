5eb819c76a1ad077fd2e6ad333d1d90ed785bfb6
dkswoals <a4445656@gmail.com>
2024-07-16 11:34:26 +0900

    feat: CategoryService 구현

---
+++ src/main/java/gift/service/CategoryService.java
@@ -0,0 +1,36 @@
+package gift.service;
+
+import gift.model.categories.Category;
+import gift.model.categories.CategoryDTO;
+import gift.repository.CategoryRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public CategoryDTO insertCategory(CategoryDTO categoryDTO) {
+        return categoryRepository.save(categoryDTO.toEntity()).toDTO();
+    }
+
+    public CategoryDTO findCategoryByName(String name){
+        return categoryRepository.findByName(name).orElseThrow().toDTO();
+    }
+
+    public List<CategoryDTO> getCategoryList() {
+        return categoryRepository.findAll().stream().map(Category::toDTO)
+            .collect(Collectors.toList());
+    }
+
+    public CategoryDTO updateCategory(CategoryDTO categoryDTO) {
+        return categoryRepository.save(categoryDTO.toEntity()).toDTO();
+    }
+
+}
