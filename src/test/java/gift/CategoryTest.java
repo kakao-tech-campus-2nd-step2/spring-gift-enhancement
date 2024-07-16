bcdf6d08f4ac62071265da7e6c3f7b9c02f297ab
Awhn <ahnshwan@outlook.com>
2024-07-16 22:37:09 +0900

    test : write test for entity class

--- src/test/java/gift/CategoryTest.java
+++ src/test/java/gift/CategoryTest.java
@@ -1,47 +1,42 @@
 package gift;
 
 import gift.entity.Category;
-import gift.entity.Product;
 import gift.repository.CategoryRepository;
-import gift.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @Transactional
-public class ProductTest {
-    @Autowired
-    private ProductRepository productRepository;
+public class CategoryTest {
     @Autowired
     private CategoryRepository categoryRepository;
-
-    private Product testProduct;
+    @Autowired
+    private TestEntityManager entityManager;
     private Category testCategory;
 
     @BeforeEach
     public void setUp() {
         testCategory = new Category(1, "test", "test", "test", "test");
         categoryRepository.save(testCategory);
-
-        testProduct = new Product(1, testCategory,1, "test", "testURL");
-        productRepository.save(testProduct);
     }
 
     @AfterEach
     public void tearDown() {
-        productRepository.deleteAll();
         categoryRepository.deleteAll();
     }
 
     @Test
-    void testFindById() {
-        Product foundProduct = productRepository.findById(testProduct.getId());
-        assertEquals(testProduct.getId(), foundProduct.getId());
+    void testFindAll() {
+        List<Category> foundedCategories = categoryRepository.findAll();
+        assertEquals(testCategory.getId(), foundedCategories.get(0).getId());
     }
 }
