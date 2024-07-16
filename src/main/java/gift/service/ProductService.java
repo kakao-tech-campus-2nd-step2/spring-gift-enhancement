5eccfe24d5a77d0f7a1af5c588632e43dc3db236
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-16 14:43:04 +0900

    refactor : Category와의 연관관계 고려한 코드 수정

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,8 +1,10 @@
 package gift.service;
 
+import gift.model.category.Category;
 import gift.model.product.Product;
 import gift.model.product.ProductRequest;
 import gift.model.product.ProductResponse;
+import gift.repository.category.CategoryRepository;
 import gift.repository.product.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,9 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
 
@@ -26,13 +30,15 @@ public class ProductService {
     }
 
     public ProductResponse createProduct(ProductRequest productRequest) {
-        Product product = productRequest.toEntity();
+        Category category = categoryRepository.findById(productRequest.categoryId()).orElseThrow();
+        Product product = productRequest.toEntity(category);
         return ProductResponse.from(productRepository.save(product));
     }
 
     public ProductResponse updateProduct(Long id, ProductRequest updatedProduct) {
         Product product = productRepository.findById(id).orElseThrow();
-        product.update(updatedProduct);
+        Category category = categoryRepository.findById(updatedProduct.categoryId()).orElseThrow();
+        product.update(updatedProduct, category);
         return ProductResponse.from(productRepository.save(product));
     }
 
