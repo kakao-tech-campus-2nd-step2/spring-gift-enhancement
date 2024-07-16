ff805704e9fa7d762b959c0754a6f652f2faed66
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-16 14:42:05 +0900

    feat : Category DTO 생성

--- src/main/java/gift/model/category/CategoryRequest.java
+++ src/main/java/gift/model/category/CategoryRequest.java
@@ -2,11 +2,15 @@ package gift.model.category;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CartegoryRequest(
+public record CategoryRequest(
     Long id,
     @NotBlank
     String name,
     String color,
+    String imageUrl,
     String description) {
+    public Category toEntity(String name, String color, String imageUrl, String description) {
+        return new Category(name, color, imageUrl, description);
+    }
 
 }
