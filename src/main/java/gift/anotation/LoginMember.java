db1f68e6df97817980fc5c3bd5ff93b26bf796bc
mingjuu <tisu79@naver.com>
2024-07-16 11:13:13 +0900

    fix : import * 수정

--- src/main/java/gift/anotation/LoginMember.java
+++ src/main/java/gift/anotation/LoginMember.java
@@ -1,6 +1,10 @@
 package gift.anotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Documented;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
