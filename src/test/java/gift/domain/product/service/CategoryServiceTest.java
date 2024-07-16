3c41352ae7ddde9725b4860e85deb83fa36d1929
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-16 16:03:30 +0900

    test: 카테고리 전체 조회 기능 테스트

---
+++ src/test/java/gift/domain/product/service/CategoryServiceTest.java
@@ -0,0 +1,47 @@
+package gift.domain.product.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import gift.domain.product.dao.CategoryJpaRepository;
+import gift.domain.product.entity.Category;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class CategoryServiceTest {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @MockBean
+    private CategoryJpaRepository categoryJpaRepository;
+
+
+    @Test
+    @DisplayName("카테고리 전체 조회 서비스 테스트")
+    void readAll() {
+        // given
+        List<Category> expected = List.of(
+            new Category(1L, "교환권", "#FFFFFF", "https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png", "test"),
+            new Category(2L, "상품권", "#FFFFFF", "https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png", "test"),
+            new Category(3L, "뷰티", "#FFFFFF", "https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png", "test"),
+            new Category(4L, "패션", "#FFFFFF", "https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png", "test")
+        );
+        given(categoryJpaRepository.findAll()).willReturn(expected);
+
+        // when
+        List<Category> actual = categoryService.readAll();
+
+        // then
+        then(categoryJpaRepository).should().findAll();
+        assertThat(actual).isEqualTo(expected);
+    }
+}
