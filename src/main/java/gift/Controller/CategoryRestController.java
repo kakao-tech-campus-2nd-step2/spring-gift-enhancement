3aefbe6044367aad9d05fb62bba80907a176c9e0
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-16 14:40:23 +0900

    feat : CategoryRestController 클래스 추가 과제 요구사항의 카테고리 목록을 불러오는 기능 구현되어 있음

--- src/main/java/gift/Controller/CategoryRestController.java
+++ src/main/java/gift/Controller/CategoryRestController.java
@@ -1,7 +1,24 @@
 package gift.Controller;
 
+import gift.Model.DTO.CategoryDTO;
+import gift.Service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class CategoryController {
+@RequestMapping("/api/categories")
+public class CategoryRestController {
+    private final CategoryService categoryService;
+
+    public CategoryRestController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<CategoryDTO> read(){
+        return categoryService.read();
+    }
 }
