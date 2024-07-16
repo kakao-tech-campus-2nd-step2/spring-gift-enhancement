fd53a828a8d271091e40e5800e0999100318b841
Profile-exe <kang67572346@gmail.com>
2024-07-17 01:29:50 +0900

    feat(OptionErrorCode): 옵션 에러코드와 이에 대응하는 비즈니스 예외 구현

---
+++ src/main/java/gift/option/exception/OptionErrorCode.java
@@ -0,0 +1,34 @@
+package gift.option.exception;
+
+import gift.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum OptionErrorCode implements ErrorCode {
+    DUPLICATED_OPTION_NAME("O001", HttpStatus.BAD_REQUEST, "상품의 옵션 이름은 중복될 수 없습니다."),
+    ;
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    OptionErrorCode(String code, HttpStatus httpStatus, String message) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
