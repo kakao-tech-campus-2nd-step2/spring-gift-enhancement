d6bf7368fca036f6b9043b9194951bd3d8c30a59
dkswoals <a4445656@gmail.com>
2024-07-16 12:25:28 +0900

    refactor: 입력 유효성 예외 처리 로직 예외 변경
    코드 중복 제거

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
 package gift.exception;
 
+import gift.exception.CustomException.CustomArgumentNotValidException;
 import gift.exception.CustomException.CustomException;
-import gift.exception.CustomException.EmailNotFoundException;
-import gift.exception.CustomException.PassWordMissMatchException;
 import io.jsonwebtoken.JwtException;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +9,6 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,18 +21,14 @@ public class GlobalExceptionHandler {
         return handleException(e.getErrorCode(), null);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValidException(
-        MethodArgumentNotValidException e) {
-        ErrorCode errorCode = ErrorCode.INVALID_INPUT;
+    @ExceptionHandler(CustomArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCustomArgumentNotValidException(
+        CustomArgumentNotValidException e) {
+        ErrorCode errorCode = e.getErrorCode();
         Map<String, String> errors = new HashMap<>();
         for (FieldError fieldError : e.getFieldErrors()) {
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
-        if(e instanceof EmailNotFoundException)
-            errorCode = ErrorCode.EMAIL_NOT_FOUND;
-        if(e instanceof PassWordMissMatchException)
-            errorCode = ErrorCode.PASSWORD_MISMATCH;
         return handleException(errorCode, errors);
     }
 
@@ -44,7 +38,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
-    public ResponseEntity<?> handleDuplicateKeyException(DuplicateKeyException e){
+    public ResponseEntity<?> handleDuplicateKeyException(DuplicateKeyException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
