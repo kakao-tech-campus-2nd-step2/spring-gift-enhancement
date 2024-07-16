a330915a2a253d61666d77c97ef37247161bf086
alsswl <alswl4223@naver.com>
2024-07-16 21:29:02 +0900

    fix : 에러 메세지 수정

--- src/main/java/gift/exception/EtcExceptionHandler.java
+++ src/main/java/gift/exception/EtcExceptionHandler.java
@@ -14,7 +14,7 @@ public class EtcExceptionHandler {
             Exception exception
     ){
         Map<String,String> errors = new HashMap<>();
-        errors.put("errerEtc",exception.getMessage());
+        errors.put("errerEtc","예상치 못한 에러가 발생했습니다. 다시 시도해주세요");
         return ResponseEntity.badRequest().body(errors);
     }
 }
