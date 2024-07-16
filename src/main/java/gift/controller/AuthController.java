c3fe0bdd34abb7b9402251cd71f73f1d12e1e2c6
GamJaDo <kim0615157@gmail.com>
2024-07-16 23:02:31 +0900

    chore: 출력 문구 수정

--- src/main/java/gift/controller/AuthController.java
+++ src/main/java/gift/controller/AuthController.java
@@ -24,7 +24,7 @@ public class AuthController {
 	@PostMapping("/register")
 	public ResponseEntity<String> register(@Valid @RequestBody User user, BindingResult bindingResult){
 		authService.createUser(user, bindingResult);
-        return ResponseEntity.ok("User registered successfully");
+        return ResponseEntity.ok("User registered successfully.");
 	}
 	
 	@PostMapping("/login")
