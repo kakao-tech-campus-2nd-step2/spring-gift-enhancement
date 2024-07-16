a4aff1ed8b1fee1cf0c46be806952069ca095b34
Woojae Jeong <woo6388@naver.com>
2024-07-17 03:16:12 +0900

    feat(RequestProduct) : categoryId 필드 추가
    Product에 category가 추가됐으므로 categoryId를 추가

--- src/main/java/gift/Model/RequestProduct.java
+++ src/main/java/gift/Model/RequestProduct.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record RequestProduct (
+public record RequestProduct(
         @Pattern(
                 regexp = "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣 ()\\[\\]+\\-\\&/_]*$",
                 message = "상품 이름에는 허용된 특수 문자만 포함될 수 있습니다: (), [], +, -, &, /, _"
@@ -13,9 +13,11 @@ public record RequestProduct (
                 message = "상품 이름에 '카카오'가 포함된 문구는 담당 MD와 협의가 필요합니다.")
         @NotNull(message = "상품 이름은 필수입니다")
         @Size(max = 15, message = "상품 이름은 최대 15자 입니다.")
-    String name,
-    int price,
-    String imageUrl
-){}
-
+        String name,
+        int price,
+        String imageUrl,
+        @NotNull(message = "카테고리 Id값은 필수입니다")
+        Long categoryId
+) {
+}
 
