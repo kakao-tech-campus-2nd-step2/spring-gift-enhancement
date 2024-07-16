ad83805222b3a65b66b9f38d787fe5d0893d7a54
young-in <hardworkingini@gmail.com>
2024-07-16 18:09:55 +0900

    refactor(webconfig) : WebConfig 클래스를 WebMvcConfigurationSupport를 상속받도록 변경하고, addArgumentResolvers 메서드를 오버라이드

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -6,16 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurationSupport {
     private final MemberService memberService;
     public WebConfig(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(memberService));
     }
