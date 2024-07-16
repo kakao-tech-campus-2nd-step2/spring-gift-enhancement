841dc0819a7c57a5f291bb6222c3c043e12470df
wvssm <neoramiregi@gmail.com>
2024-07-17 02:24:05 +0900

    refactor(ErrorResponse): toEntity에 Builder 패턴 적용

--- src/main/java/gift/dto/response/CategoryResponse.java
+++ src/main/java/gift/dto/response/CategoryResponse.java
@@ -1,10 +1,19 @@
 package gift.dto.response;
 
 import gift.domain.Category;
-import gift.domain.Member;
 
 public record CategoryResponse(Long id, String name, String color, String imageUrl, String description) {
     public static CategoryResponse from(final Category category){
         return new CategoryResponse(category.getId(), category.getName(), category.getColor(), category.getImageUrl(), category.getDescription());
+    }
+
+    public Category toEntity() {
+        return new Category.Builder()
+                .id(this.id)
+                .name(this.name)
+                .color(this.color)
+                .imageUrl(this.imageUrl)
+                .description(this.description)
+                .build();
     }
 }
