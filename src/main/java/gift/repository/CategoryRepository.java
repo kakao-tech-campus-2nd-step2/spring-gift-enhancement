1b70c70b80957649d9cddea6aea7ffd0c88d9c83
wvssm <neoramiregi@gmail.com>
2024-07-17 02:31:30 +0900

    feat(CategoryRepository): findByName 메서드 추가

--- src/main/java/gift/repository/CategoryRepository.java
+++ src/main/java/gift/repository/CategoryRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findById(Long id);
+    Optional<Category> findByName(String name);
 }
