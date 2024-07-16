a0cff18572c49ce005edf13264d5769e720241f0
us4c0d3 <jwseok312@gmail.com>
2024-07-16 14:38:27 +0900

    feat(ProductDto): imageUrl의 엔티티, url 스펙에 맞게 유효성 검증 조건 추가

--- src/main/java/gift/dto/product/CreateProductRequest.java
+++ src/main/java/gift/dto/product/CreateProductRequest.java
@@ -2,6 +2,8 @@ package gift.dto.product;
 
 import gift.validation.annotation.RestrictedKeyword;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -16,6 +18,8 @@ public record CreateProductRequest(
 
     @Positive Integer price,
 
+    @NotNull
+    @NotBlank
     String imageUrl
 ) {
 
--- src/main/java/gift/dto/product/UpdateProductRequest.java
+++ src/main/java/gift/dto/product/UpdateProductRequest.java
@@ -2,6 +2,7 @@ package gift.dto.product;
 
 import gift.validation.annotation.RestrictedKeyword;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -16,6 +17,8 @@ public record UpdateProductRequest(
 
     @Positive Integer price,
 
+    @NotNull
+    @NotBlank
     String imageUrl
 ) {
 
