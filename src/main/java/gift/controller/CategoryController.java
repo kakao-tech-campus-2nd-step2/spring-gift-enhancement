1ed04ed2e4876722c38e67a9189a24d73623c313
GamJaDo <kim0615157@gmail.com>
2024-07-16 23:03:05 +0900

    feat: Category 컨트롤러 생성
    조회, 생성, 수정

---
+++ src/main/java/gift/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package gift.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gift.model.Category;
+import gift.service.CategoryService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> getAllCategories(){
+		List<Category> categories = categoryService.getAllCategories();
+		return ResponseEntity.ok(categories);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> addCategory(@Valid @RequestBody Category category, BindingResult bindingResult){
+		Category createdCategory = categoryService.createCategory(category, bindingResult);
+		return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateCategory(@PathVariable("id") Long id, @Valid Category category,
+			BindingResult bindingResult){
+		categoryService.updateCategory(id, category, bindingResult);
+		return new ResponseEntity<>("Category updated successfully.", HttpStatus.OK);
+	}
+}
