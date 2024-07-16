613714eadf85b57a4a6343369c7fc142fa180522
dkswoals <a4445656@gmail.com>
2024-07-16 13:53:31 +0900

    feat: category 엔티티 id 생성 속성 추가

--- src/main/java/gift/model/categories/Category.java
+++ src/main/java/gift/model/categories/Category.java
@@ -2,11 +2,13 @@ package gift.model.categories;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Category {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String name;
