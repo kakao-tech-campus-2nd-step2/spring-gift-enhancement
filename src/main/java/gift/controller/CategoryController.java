ab996dfeeb1003a72018fcd2748110775c9f6e9b
noojmogstel <goaudgo@naver.com>
2024-07-16 17:45:07 +0900

    feat(CategoryController): Category Controller 추가

---
+++ src/main/java/gift/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package gift.controller;
+
+import gift.entity.Category;
+import gift.service.CategoryService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategory(){
+        return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
+    }
+
+
+
+
+
+}
