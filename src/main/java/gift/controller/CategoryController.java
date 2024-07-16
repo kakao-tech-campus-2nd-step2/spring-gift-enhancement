37c60195a35031b286b88e58edbbe871b995662d
박민규 <pmq0101@naver.com>
2024-07-16 18:18:46 +0900

    feat(CategoryController): 카테고리 수정 api 추가

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,4 +49,12 @@ public class CategoryController {
             categoryListResponseDTO);
     }
 
+    @PutMapping("/category/{id}")
+    public ResponseEntity<SuccessBody<Long>> updateCategory(
+        @PathVariable("id") Long categoryId,
+        @Valid @RequestBody CategoryRequestDTO categoryRequestDTO) {
+
+        Long updatedCategoryId = categoryService.updateCategory(categoryId, categoryRequestDTO);
+        return ApiResponseGenerator.success(HttpStatus.OK, "카테고리가 수정되었습니다.", updatedCategoryId);
+    }
 }
