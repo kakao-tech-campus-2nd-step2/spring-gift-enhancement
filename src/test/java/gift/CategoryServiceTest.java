e6988dcd7a857ef473e5639853e12c799755cad7
sseooh <sseooh@gmail.com>
2024-07-16 12:08:10 +0900

    test: 테스트 코드 수정

--- src/test/java/gift/CategoryServiceTest.java
+++ src/test/java/gift/CategoryServiceTest.java
@@ -1,7 +1,8 @@
-package gift.service;
+package gift;
 
 import gift.domain.Category;
 import gift.repository.CategoryRepository;
+import gift.service.CategoryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
