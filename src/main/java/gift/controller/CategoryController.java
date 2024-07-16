28ac76d134063316f5f1d559d9587e6eb1660428
박민규 <pmq0101@naver.com>
2024-07-16 17:51:20 +0900

    feat(CategoryController): 카테고리 조회 api 추가

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -3,10 +3,14 @@ package gift.controller;
 import gift.dto.common.apiResponse.ApiResponseBody.SuccessBody;
 import gift.dto.common.apiResponse.ApiResponseGenerator;
 import gift.dto.requestDTO.CategoryRequestDTO;
+import gift.dto.responseDTO.CategoryListResponseDTO;
+import gift.dto.responseDTO.CategoryResponseDTO;
 import gift.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +31,21 @@ public class CategoryController {
         @Valid @RequestBody CategoryRequestDTO categoryRequestDTO) {
         Long categoryId = categoryService.addCategory(categoryRequestDTO);
         return ApiResponseGenerator.success(HttpStatus.CREATED, "카테고리가 생성되었습니다.", categoryId);
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<SuccessBody<CategoryResponseDTO>> getOneCategory(
+        @PathVariable("id") Long categoryId) {
+        CategoryResponseDTO categoryResponseDTO = categoryService.getOneCategory(categoryId);
+        return ApiResponseGenerator.success(HttpStatus.OK, "id : " + categoryId + " 카테고리를 조회했습니다.",
+            categoryResponseDTO);
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<SuccessBody<CategoryListResponseDTO>> getAllCategory() {
+        CategoryListResponseDTO categoryListResponseDTO = categoryService.getAllCategories();
+        return ApiResponseGenerator.success(HttpStatus.OK, "모든 카테고리를 조회했습니다.",
+            categoryListResponseDTO);
     }
 
 }
