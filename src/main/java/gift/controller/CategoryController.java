548437daa216d5b005a078027c34edf6632803c6
박민규 <pmq0101@naver.com>
2024-07-16 17:07:24 +0900

    feat(CategoryController): CategoryController 카테고리 추가 api

---
+++ src/main/java/gift/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package gift.controller;
+
+import gift.dto.common.apiResponse.ApiResponseBody.SuccessBody;
+import gift.dto.common.apiResponse.ApiResponseGenerator;
+import gift.dto.requestDTO.CategoryRequestDTO;
+import gift.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<SuccessBody<Long>> addProduct(
+        @Valid @RequestBody CategoryRequestDTO categoryRequestDTO) {
+        Long categoryId = categoryService.addCategory(categoryRequestDTO);
+        return ApiResponseGenerator.success(HttpStatus.CREATED, "카테고리가 생성되었습니다.", categoryId);
+    }
+
+}
