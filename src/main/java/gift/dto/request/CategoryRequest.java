6e88a3fb26b6a5c67c71cf2dcf6f95e6b7ba9dbc
wvssm <neoramiregi@gmail.com>
2024-07-17 02:25:17 +0900

    feat(CategoryRequest): 카테고리 이름에 제약사항 걸기

--- src/main/java/gift/dto/request/CategoryRequest.java
+++ src/main/java/gift/dto/request/CategoryRequest.java
@@ -1,8 +1,14 @@
 package gift.dto.request;
 
 import gift.domain.Category;
+import jakarta.validation.constraints.NotBlank;
 
-public record CategoryRequest(String name, String color, String imageUrl, String description) {
+public record CategoryRequest(
+        @NotBlank(message = "카테고리 이름은 공백일 수 없습니다.")
+        String name,
+        String color,
+        String imageUrl,
+        String description) {
     public Category toEntity(){
         return new Category.Builder()
                 .name(this.name)
