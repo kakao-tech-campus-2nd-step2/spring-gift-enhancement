07429417ea937e2ef9ae490a751c5e74d5da1bff
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-16 16:17:36 +0900

    update: data.sql수정

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,19 +1,24 @@
 -- Inserting initial data for products
-INSERT INTO product (name, price, image_url)
-VALUES ('Product A', 1000, 'image1.jpg');
-INSERT INTO product (name, price, image_url)
-VALUES ('Product B', 1500, 'image2.jpg');
-INSERT INTO product (name, price, image_url)
-VALUES ('Product C', 2000, 'image3.jpg');
-INSERT INTO Product (name, price, image_url)
+INSERT INTO category (name) VALUES ('test1');
+INSERT INTO category (name) VALUES ('test2');
+INSERT INTO category (name) VALUES ('test3');
+
+-- Inserting initial data for products
+INSERT INTO product (name, price, image_url,category_id)
+VALUES ('Product A', 1000, 'image1.jpg',1L);
+INSERT INTO product (name, price, image_url,category_id)
+VALUES ('Product B', 1500, 'image2.jpg',1);
+INSERT INTO product (name, price, image_url,category_id)
+VALUES ('Product C', 2000, 'image3.jpg',1);
+INSERT INTO Product (name, price, image_url,category_id)
 VALUES ('커피', 100,
-        'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
-INSERT INTO Product (name, price, image_url)
+        'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg',2L);
+INSERT INTO Product (name, price, image_url,category_id)
 VALUES ('콜라', 100,
-        'https://img.danawa.com/prod_img/500000/059/749/img/13749059_1.jpg?_v=20220524145210');
-INSERT INTO Product (name, price, image_url)
+        'https://img.danawa.com/prod_img/500000/059/749/img/13749059_1.jpg?_v=20220524145210',3L);
+INSERT INTO Product (name, price, image_url,category_id)
 VALUES ('몬스터', 200,
-        'https://img.danawa.com/prod_img/500000/658/896/img/17896658_1.jpg?_v=20220923092758');
+        'https://img.danawa.com/prod_img/500000/658/896/img/17896658_1.jpg?_v=20220923092758',3L);
 
 
 -- Wish 테이블에 데이터 삽입
