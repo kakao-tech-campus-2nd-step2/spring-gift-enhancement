2f7e6ff8b426ff476b2a9df9eed458b1c5321843
cussle <cussle@kakao.com>
2024-07-16 15:59:35 +0900

    feat: 데이터베이스 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,17 @@
 -- 상품(PRODUCTS) 스키마
 CREATE TABLE IF NOT EXISTS product (
-    id        BIGINT auto_increment PRIMARY KEY,
-    name      VARCHAR (255) NOT NULL,
-    price     INT NOT NULL,
-    image_url VARCHAR (255) NOT NULL
+    id          BIGINT auto_increment PRIMARY KEY,
+    name        VARCHAR (15)    NOT NULL,
+    price       INT             NOT NULL,
+    image_url   VARCHAR (255)   NOT NULL,
+    category_id BIGINT          NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
+
+-- 카테고리(CATEGORY) 스키마
+CREATE TABLE IF NOT EXISTS category (
+    id   BIGINT auto_increment PRIMARY KEY,
+    name VARCHAR (255) NOT NULL
 );
 
 -- 회원(MEMBERS) 스키마
@@ -18,6 +26,6 @@ CREATE TABLE IF NOT EXISTS wish (
     id         BIGINT auto_increment PRIMARY KEY,
     member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    FOREIGN KEY ( member_id ) REFERENCES member ( id ),
-    FOREIGN KEY ( product_id ) REFERENCES product ( id )
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
 );
