edf05355732c9129a528cb3d3e279cca9e677cac
seojiu <seojiu5598@gmail.com>
2024-07-17 00:04:58 +0900

    feat(CategoryController.java, CategoryService.java): 전체 조회기능 구현

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package gift.controller;
 
+import gift.model.Category;
 import gift.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -12,5 +17,11 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
     }
 }
--- src/main/java/gift/service/CategoryService.java
+++ src/main/java/gift/service/CategoryService.java
@@ -2,6 +2,9 @@ package gift.service;
 
 import gift.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+import gift.model.Category;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -9,5 +12,9 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
     }
 }
