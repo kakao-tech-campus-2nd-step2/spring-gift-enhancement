2f3b41470830db69aae6a010011c60c41f3b1a71
Kangboom <jkl6147@naver.com>
2024-07-16 12:43:23 +0900

    feat: add Category field

--- src/main/java/gift/domain/product/entity/Product.java
+++ src/main/java/gift/domain/product/entity/Product.java
@@ -2,12 +2,15 @@ package gift.domain.product.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import gift.domain.category.entity.Category;
 import gift.domain.wishlist.entity.Wish;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
@@ -30,22 +33,27 @@ public class Product {
     @Column
     private String imageUrl;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private List<Wish> wishList;
 
     protected Product() {
     }
 
-    public Product(String name, int price, String imageUrl) {
-        this(null, name, price, imageUrl);
+    public Product(String name, int price, String imageUrl, Category category) {
+        this(null, name, price, imageUrl, category);
     }
 
-    public Product(Long id, String name, int price, String imageUrl) {
+    public Product(Long id, String name, int price, String imageUrl, Category category) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.wishList = new ArrayList<>();
+        this.category = category;
     }
 
     public Long getId() {
@@ -68,10 +76,19 @@ public class Product {
         return wishList;
     }
 
-    public void update(String name, int price, String imageUrl) {
+    public Category getCategory() {
+        return category;
+    }
+
+    public void updateAll(String name, int price, String imageUrl, Category category) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.category = category;
+    }
+
+    public void updateCategory(Category category){
+        this.category = category;
     }
 
     public void removeWish(Wish wish) {
