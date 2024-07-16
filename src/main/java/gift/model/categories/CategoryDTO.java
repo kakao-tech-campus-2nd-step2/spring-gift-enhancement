d6ab7d91288f813df1ee9a77ba4895ff9cd41a98
dkswoals <a4445656@gmail.com>
2024-07-16 13:53:50 +0900

    feat: categoryDTO 유효성 검증 추가

--- src/main/java/gift/model/categories/CategoryDTO.java
+++ src/main/java/gift/model/categories/CategoryDTO.java
@@ -1,8 +1,13 @@
 package gift.model.categories;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class CategoryDTO {
 
     private final Long id;
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
+    @Pattern(regexp = "[a-zA-Z0-9가-힣() +\\-&\\[\\]/_]*", message = "(),[],+,-,&,/,_ 를 제외한 특수문자는 사용이 불가합니다.")
     private final String name;
     private final String imgUrl;
 
