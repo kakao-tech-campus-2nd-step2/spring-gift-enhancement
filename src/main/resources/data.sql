6122906298ec371ba9d72d8f1d5e7143d687c081
JinSeoHyun <15roa1121@gmail.com>
2024-07-17 00:43:26 +0900

    fetch : 기본 카테고리 주입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Category (uni_number, name) VALUES (1, '패션');
+INSERT INTO Category (uni_number, name) VALUES (2, '뷰티');
+INSERT INTO Category (uni_number, name) VALUES (3, '식품');
+INSERT INTO Category (uni_number, name) VALUES (4, '디지털/가전');
+INSERT INTO Category (uni_number, name) VALUES (5, '인테리어');
+INSERT INTO Category (uni_number, name) VALUES (6, '출산/유아동');
