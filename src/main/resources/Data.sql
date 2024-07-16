80cf1220f809f0214c53f583092410ada747d0fd
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-16 19:47:03 +0900

    fix: sql은 long이 따로 없음

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -5,20 +5,20 @@ INSERT INTO category (name) VALUES ('test3');
 
 -- Inserting initial data for products
 INSERT INTO product (name, price, image_url,category_id)
-VALUES ('Product A', 1000, 'image1.jpg',1L);
+VALUES ('Product A', 1000, 'image1.jpg',1);
 INSERT INTO product (name, price, image_url,category_id)
-VALUES ('Product B', 1500, 'image2.jpg',1);
+VALUES ('Product B', 1500, 'image2.jpg',2);
 INSERT INTO product (name, price, image_url,category_id)
-VALUES ('Product C', 2000, 'image3.jpg',1);
+VALUES ('Product C', 2000, 'image3.jpg',3);
 INSERT INTO Product (name, price, image_url,category_id)
 VALUES ('커피', 100,
-        'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg',2L);
+        'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg',1);
 INSERT INTO Product (name, price, image_url,category_id)
 VALUES ('콜라', 100,
-        'https://img.danawa.com/prod_img/500000/059/749/img/13749059_1.jpg?_v=20220524145210',3L);
+        'https://img.danawa.com/prod_img/500000/059/749/img/13749059_1.jpg?_v=20220524145210',2);
 INSERT INTO Product (name, price, image_url,category_id)
 VALUES ('몬스터', 200,
-        'https://img.danawa.com/prod_img/500000/658/896/img/17896658_1.jpg?_v=20220923092758',3L);
+        'https://img.danawa.com/prod_img/500000/658/896/img/17896658_1.jpg?_v=20220923092758',3);
 
 
 -- Wish 테이블에 데이터 삽입
