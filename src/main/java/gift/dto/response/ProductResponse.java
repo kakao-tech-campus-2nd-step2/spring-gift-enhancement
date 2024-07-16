a3753bfcb98afb034c96b6b8d9de5caa09d2d8b1
wvssm <neoramiregi@gmail.com>
2024-07-17 02:40:01 +0900

    feat(ProductResponse): 객체 참조 대신 Category DTO 참조

--- src/main/java/gift/dto/response/ProductResponse.java
+++ src/main/java/gift/dto/response/ProductResponse.java
@@ -2,12 +2,12 @@ package gift.dto.response;
 
 import gift.domain.Product;
 
-public record ProductResponse(Long id, String name, int price, String imageUrl) {
+public record ProductResponse(Long id, String name, int price, String imageUrl, CategoryResponse categoryResponse) {
     public static ProductResponse from(final Product product){
-        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), CategoryResponse.from(product.getCategory()));
     }
 
     public Product toEntity(){
-        return new Product(this.id, this.name, this.price, this.imageUrl);
+        return new Product(this.id, this.name, this.price, this.imageUrl, this.categoryResponse.toEntity());
     }
 }
