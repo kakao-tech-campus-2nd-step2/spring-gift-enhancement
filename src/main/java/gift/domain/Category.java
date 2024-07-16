aa8f1f66a9abbf1fe7ed0ed01e92fea977a73be5
sunandrabbit <sunandrabbit@gamil.com>
2024-07-16 20:04:56 +0900

    feat(domain) : Category에 대한 dto 클래스 정의

---
+++ src/main/java/gift/domain/Category.java
@@ -0,0 +1,62 @@
+package gift.domain;
+
+public class Category {
+
+    private Long id;
+    private String name;
+    private String color;
+    private String imageUrl;
+    private String description;
+
+    public Category() {
+
+    }
+
+    public Category(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
