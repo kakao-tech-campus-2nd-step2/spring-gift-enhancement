d7d191d0efa073b94fb0502596d3f6bbd28b51fc
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-16 16:14:01 +0900

    update: getProductList와 getProduct가 ProductDetail로 반환합니다.

--- src/main/java/gift/mapper/ProductMapper.java
+++ src/main/java/gift/mapper/ProductMapper.java
@@ -1,9 +1,12 @@
 package gift.mapper;
 
 import gift.domain.Product.CreateProduct;
+import gift.domain.Product.ProductDetail;
 import gift.domain.Product.ProductSimple;
 import gift.domain.Product.UpdateProduct;
+import gift.entity.CategoryEntity;
 import gift.entity.ProductEntity;
+import gift.entity.WishEntity;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
@@ -23,15 +26,44 @@ public class ProductMapper {
 
         return new PageImpl<>(simpleList, all.getPageable(), all.getTotalElements());
     }
+    public Page<ProductDetail> toDetailList(Page<ProductEntity> all) {
+        List<ProductDetail> simpleList = all.stream()
+            .map(entity -> new ProductDetail(
+                entity.getId(),
+                entity.getName(),
+                entity.getPrice(),
+                entity.getImageUrl(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt(),
+                entity.getWish().stream().map(WishEntity::getId).collect(Collectors.toList()),
+                entity.getCategory().getId()
+            ))
+            .collect(Collectors.toList());
 
-    public ProductEntity toEntity(CreateProduct create) {
-        return new ProductEntity(create.getName(), create.getPrice(), create.getImageUrl());
+        return new PageImpl<>(simpleList, all.getPageable(), all.getTotalElements());
     }
 
-    public ProductEntity toUpdate(UpdateProduct update, ProductEntity entity) {
+    public ProductDetail toDetail(ProductEntity entity){
+        return new ProductDetail(
+            entity.getId(),
+            entity.getName(),
+            entity.getPrice(),
+            entity.getImageUrl(),
+            entity.getCreatedAt(),
+            entity.getUpdatedAt(),
+            entity.getWish().stream().map(WishEntity::getId).collect(Collectors.toList()),
+            entity.getCategory().getId());
+    }
+
+    public ProductEntity toEntity(CreateProduct create, CategoryEntity category) {
+        return new ProductEntity(create.getName(), create.getPrice(), create.getImageUrl(), category);
+    }
+
+    public ProductEntity toUpdate(UpdateProduct update, ProductEntity entity, CategoryEntity category) {
         entity.setPrice(update.getPrice());
         entity.setName(update.getName());
         entity.setImageUrl(update.getImageUrl());
+        entity.setCategory(category);
         return entity;
     }
 
