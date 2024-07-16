8b4314810082839b8a491425e744bcdb50c6e368
suno-boy <tnsgh3691@gmail.com>
2024-07-16 18:47:58 +0900

    feat: 카테고리 엔티티에 대한 CRUD기능을 제공하는 JpaRepository구현

---
+++ src/main/java/gift/Repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package gift.Repository;
+
+import gift.Entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    List<CategoryEntity> findByParentId(Long parentId);
+    Optional<CategoryEntity> findByCode(String code);
+}
