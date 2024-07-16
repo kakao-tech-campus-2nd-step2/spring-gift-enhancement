3439e9206e390ebd0b4854d46e6897eaa5d3cc1f
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-16 15:14:40 +0900

    feat: CategoryController 추가

---
+++ src/main/java/gift/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package gift.controller;
+
+import gift.domain.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getCategories() {
+        List<Category> responseBody = categoryService.getCategories();
+        return ResponseEntity.ok().body(responseBody);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category responseBody = categoryService.createCategory(category);
+        return ResponseEntity.ok().body(responseBody);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Category responseBody = categoryService.updateCategory(id, category);
+        return ResponseEntity.ok().body(responseBody);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        return categoryService.deleteCategory(id);
+    }
+}
+
