515bc53f161c4e3a287f42a2739a8adbec31e6ce
momnpa333 <momnpa333@gmail.com>
2024-07-16 22:56:29 +0900

    feat: Category Repository 제작

--- src/main/java/gift/repository/product/CategoryRepository.java
+++ src/main/java/gift/repository/product/CategoryRepository.java
@@ -1,10 +1,18 @@
 package gift.repository.product;
 
 import gift.model.product.Category;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository {
 
     Optional<Category> findById(Long aLong);
+
+    Category save(Category category);
+
+    void deleteById(Long id);
+
+    List<Category> findAll();
 
 }
