75a635c7eb445442e5977ebf697a66c3db2a28c0
sim-mer <gyu2781@gmail.com>
2024-07-16 17:37:59 +0900

    feat(product.persistence): Category Repository 작성

---
+++ src/main/java/gift/product/persistence/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package gift.product.persistence.repository;
+
+import gift.product.persistence.entity.Category;
+import java.util.List;
+
+public interface CategoryRepository {
+
+    Category getReferencedCategory(Long categoryId);
+
+    List<Category> getAllCategories();
+}
---
+++ src/main/java/gift/product/persistence/repository/CategoryRepositoryImpl.java
@@ -0,0 +1,24 @@
+package gift.product.persistence.repository;
+
+import gift.product.persistence.entity.Category;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CategoryRepositoryImpl implements CategoryRepository{
+    private final CategoryJpaRepository categoryJpaRepository;
+
+    public CategoryRepositoryImpl(CategoryJpaRepository categoryJpaRepository) {
+        this.categoryJpaRepository = categoryJpaRepository;
+    }
+
+    @Override
+    public Category getReferencedCategory(Long categoryId) {
+        return categoryJpaRepository.getReferenceById(categoryId);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryJpaRepository.findAll();
+    }
+}
