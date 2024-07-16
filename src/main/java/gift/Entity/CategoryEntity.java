d6cddd0de2d339862cbe865fc603f037248af38c
suno-boy <tnsgh3691@gmail.com>
2024-07-16 18:41:17 +0900

    feat: 카테고리 엔티티 구현

---
+++ src/main/java/gift/Entity/CategoryEntity.java
@@ -0,0 +1,76 @@
+package gift.Entity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private CategoryEntity parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CategoryEntity> children = new ArrayList<>();
+
+
+    // 아래는 Constructor, getter 및 setter
+
+    public CategoryEntity() {
+    }
+
+    public CategoryEntity(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryEntity getParent() {
+        return parent;
+    }
+
+    public void setParent(CategoryEntity parent) {
+        this.parent = parent;
+    }
+
+    public List<CategoryEntity> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<CategoryEntity> children) {
+        this.children = children;
+    }
+
+    public void addChild(CategoryEntity child) {
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+    public void removeChild(CategoryEntity child) {
+        child.setParent(null);
+        this.children.remove(child);
+    }
+}
