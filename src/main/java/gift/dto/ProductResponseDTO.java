2da32f7a9838ad73b58e79be1a4d78a0f9c80865
ajy4304 <ajy4304@naver.com>
2024-07-16 20:01:54 +0900

    feat(ProductResponseDTO): 카테고리 이름 필드 추가

--- src/main/java/gift/dto/ProductResponseDTO.java
+++ src/main/java/gift/dto/ProductResponseDTO.java
@@ -6,11 +6,13 @@ public class ProductResponseDTO {
     private String name;
     private int price;
     private String imageUrl;
+    private String categoryName;
 
     public ProductResponseDTO(Product product) {
         this.name = product.getName();
         this.price = product.getPrice();
         this.imageUrl = product.getImageUrl();
+        this.categoryName = product.getCategory().getName();
     }
 
     public String getName() {
