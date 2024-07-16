7c92e528358a79f7a0c42540a0d76b39039a51e5
GamJaDo <kim0615157@gmail.com>
2024-07-16 23:01:56 +0900

    feat: getAllProduct->getProducts(적절한 메서드 이름으로 변경)
    path 설정

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import gift.model.Product;
+import gift.service.CategoryService;
 import gift.service.ProductService;
 
 @Controller
@@ -17,46 +18,51 @@ public class AdminController {
 
     @Autowired
     private ProductService productService;
+    
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
     public String adminPage(Model model,
     		@PageableDefault(sort="name") Pageable pageable) {
-        Page<Product> productList = productService.getAllProducts(pageable);
+        Page<Product> productList = productService.getProducts(pageable);
         model.addAttribute("products", productList);
         return "admin";
     }
 
     @GetMapping("/new")
     public String addProductForm(Model model) {
-        model.addAttribute("product", new Product("", 0, ""));
+        model.addAttribute("product", new Product("", 0, "", null));
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "product-form";
     }
 
     @PostMapping
     public String addProduct(@ModelAttribute Product product, BindingResult bindingResult) {
         productService.createProduct(product, bindingResult);
-        return "redirect:/admin";
+        return "redirect:/admin/products";
     }
 
     @GetMapping("/edit/{id}")
     public String editProductForm(@PathVariable("id") Long id, Model model) {
         Product product = productService.getProduct(id);
         if (product == null) {
-            return "redirect:/admin";
+            return "redirect:/admin/products";
         }
         model.addAttribute("product", product);
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "product-form";
     }
 
     @PostMapping("/edit/{id}")
     public String editProduct(@PathVariable("id") Long id, @ModelAttribute Product updatedProduct, BindingResult bindingResult) {
         productService.updateProduct(id, updatedProduct, bindingResult);
-        return "redirect:/admin";
+        return "redirect:/admin/products";
     }
 
     @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProduct(id);
-        return "redirect:/admin";
+        return "redirect:/admin/products";
     }
 }
