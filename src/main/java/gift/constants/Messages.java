616923e921b7aa6fb05fe0537623acc94c5e60d7
donghyun kim <gimdonghyun5434@gmail.com>
2024-07-16 17:11:45 +0900

    3주차 피드백 반영

---
+++ src/main/java/gift/constants/Messages.java
@@ -0,0 +1,6 @@
+package gift.constants;
+
+public class Messages {
+    public static final String NOT_FOUND_MEMBER_BY_EMAIL_MESSAGE = "해당 email을 가진 member가 존재하지 않습니다";
+    public static final String NOT_FOUND_WISH_MESSAGE = "해당 wish가 존재하지 않습니다.";
+}
