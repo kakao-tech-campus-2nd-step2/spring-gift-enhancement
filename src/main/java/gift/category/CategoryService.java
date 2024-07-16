054c58f8ebe40a5b6915213a91bfd3c860eef819
Lee Hyungi <geist9110@naver.com>
2024-07-16 20:02:33 +0900

    style: Remove not used methods

--- src/main/java/gift/category/CategoryService.java
+++ src/main/java/gift/category/CategoryService.java
@@ -1,7 +1,5 @@
 package gift.category;
 
-import static gift.exception.ErrorMessage.CATEGORY_NOT_FOUND;
-
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +12,8 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
-    public Category addCategory(CategoryDTO categoryDTO) {
-        return categoryRepository.save(categoryDTO.toEntity());
-    }
-
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
-    public Category getCategoryByName(CategoryDTO categoryDTO) {
-        return categoryRepository.findByName(categoryDTO.getName())
-            .orElseThrow(() -> new IllegalArgumentException(CATEGORY_NOT_FOUND));
-    }
 }
