216b6e2561732002fe579d92e276a3f366268992
seojiu <seojiu5598@gmail.com>
2024-07-16 23:59:10 +0900

    feat(CategoryRepository.java, CategoryService.java): 리포지토리, 서비스 클래스 생성

---
+++ src/main/java/gift/service/CategoryService.java
@@ -0,0 +1,13 @@
+package gift.service;
+
+import gift.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+}
