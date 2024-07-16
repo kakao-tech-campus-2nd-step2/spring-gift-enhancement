4586a43b1a637d1e7be3f97f33aee16a4d1e5b57
rdme0 <we6610@gmail.com>
2024-07-16 19:09:45 +0900

    :recycle: refactor: 카테고리 추가로 인한 리팩터링

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,15 +1,18 @@
 package gift.service;
 
 import gift.dto.ProductDTO;
+import gift.dto.ProductRequestDTO;
+import gift.entity.Category;
 import gift.entity.Product;
 import gift.exception.BadRequestExceptions.BadRequestException;
 import gift.exception.BadRequestExceptions.InvalidIdException;
 import gift.exception.BadRequestExceptions.NoSuchProductIdException;
 import gift.exception.InternalServerExceptions.InternalServerException;
+import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
 import gift.repository.WishRepository;
+import gift.util.validator.databaseValidator.CategoryDatabaseValidator;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -24,17 +27,25 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final WishRepository wishRepository;
+    private final CategoryRepository categoryRepository;
+    private final CategoryDatabaseValidator categoryDatabaseValidator;
 
     @Autowired
-    public ProductService(ProductRepository productRepository, WishRepository wishRepository) {
+    public ProductService(ProductRepository productRepository, WishRepository wishRepository,
+            CategoryRepository categoryRepository,
+            CategoryDatabaseValidator categoryDatabaseValidator) {
         this.productRepository = productRepository;
         this.wishRepository = wishRepository;
+        this.categoryRepository = categoryRepository;
+        this.categoryDatabaseValidator = categoryDatabaseValidator;
     }
 
     @Transactional
-    public void addProduct(ProductDTO productDTO) throws RuntimeException {
+    public void addProduct(ProductRequestDTO productRequestDTO) throws RuntimeException {
         try {
-            Product product = productDTO.convertToProduct();
+            Category category = categoryDatabaseValidator.validate(productRequestDTO.categoryName());
+            Product product = productRequestDTO.convertToProduct(category);
+            categoryRepository.save(product.getCategory());
             productRepository.save(product);
         } catch (DataIntegrityViolationException e) {
             throw new BadRequestException("잘못된 제품 값을 입력했습니다. 입력 칸 옆의 설명을 다시 확인해주세요");
@@ -55,21 +66,16 @@ public class ProductService {
     }
 
     @Transactional
-    public void updateProduct(Long id, ProductDTO productDTO) throws RuntimeException {
+    public void updateProduct(Long id, ProductRequestDTO productRequestDTO) throws RuntimeException {
         try {
-            if (!id.equals(productDTO.getId())) {
+            if (!id.equals(productRequestDTO.id()))
                 throw new InvalidIdException("올바르지 않은 id입니다.");
-            }
 
-            Optional<Product> productInDb = productRepository.findById(id);
+            Category categoryToReplace = categoryRepository.findByName(productRequestDTO.categoryName()).orElseThrow(() -> new BadRequestException("그러한 카테코리를 찾을 수 없습니다."));;
+            Product productInDb = productRepository.findById(id).orElseThrow(() -> new NoSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id)));
+            Product productToUpdate = productRequestDTO.convertToProduct(categoryToReplace);
 
-            if (productInDb.isEmpty()) {
-                throw new NoSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id));
-            }
-
-            Product product = productDTO.convertToProduct();
-            Product productInDB = productInDb.get();
-            productInDB.changeProduct(product.getName(), product.getPrice(), product.getImageUrl());
+            productInDb.changeProduct(productToUpdate.getName(), productToUpdate.getPrice(), productToUpdate.getImageUrl(), productToUpdate.getCategory());
         } catch (BadRequestException e) {
             throw e;
         } catch (Exception e) {
