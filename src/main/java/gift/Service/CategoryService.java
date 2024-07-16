bf05ad14108d9ba2c393901ab547863c1eda8556
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-16 14:37:55 +0900

    feat : CategoryService 클래스 추가 category 데이터베이스의 CRUD 기능이 구현되어 있음

--- src/main/java/gift/Service/CategoryService.java
+++ src/main/java/gift/Service/CategoryService.java
@@ -1,2 +1,63 @@
-package gift.Service;public class CategoryService {
+package gift.Service;
+
+import gift.Exception.CategoryException;
+import gift.Model.DTO.CategoryDTO;
+import gift.Model.Entity.CategoryEntity;
+import gift.Repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void create(CategoryDTO categoryDTO){
+        Optional<CategoryEntity> categoryEntityOptional = categoryRepository.findByName(categoryDTO.name());
+
+        if(categoryEntityOptional.isPresent()){
+            throw new CategoryException();
+        }
+
+        categoryRepository.save(new CategoryEntity(categoryDTO.name(), categoryDTO.color(), categoryDTO.imageUrl(), categoryDTO.description()));
+    }
+
+    public List<CategoryDTO> read(){
+        List<CategoryEntity> categoryEntityList = categoryRepository.findAll();
+
+        List<CategoryDTO> categoryDTOList = new ArrayList<>();
+
+        for(CategoryEntity c: categoryEntityList){
+            categoryDTOList.add(new CategoryDTO(c.getId(), c.getName(), c.getColor(), c.getImageUrl(), c.getDescription()));
+        }
+
+        return categoryDTOList;
+    }
+
+    public void update(CategoryDTO categoryDTO){
+        Optional<CategoryEntity> categoryEntityOptional = categoryRepository.findByName(categoryDTO.name());
+
+        if(categoryEntityOptional.isEmpty()){
+            throw new CategoryException();
+        }
+        CategoryEntity categoryEntity = new CategoryEntity(categoryDTO.name(), categoryDTO.color(), categoryDTO.imageUrl(), categoryDTO.description());
+        categoryEntity.setId(categoryEntityOptional.get().getId());
+        categoryRepository.save(categoryEntity);
+    }
+
+    public void delete(Long id){
+        Optional<CategoryEntity> categoryEntityOptional = categoryRepository.findById(id);
+
+        if(categoryEntityOptional.isEmpty()){
+            throw new CategoryException();
+        }
+
+        categoryRepository.deleteById(id);
+    }
 }
