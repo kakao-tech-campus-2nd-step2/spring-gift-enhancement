e338724ec44b49332978e3532569d04a929d6a30
Kangboom <jkl6147@naver.com>
2024-07-16 12:42:46 +0900

    feat: add category dto

---
+++ src/main/java/gift/domain/category/dto/CategoryRequest.java
@@ -0,0 +1,37 @@
+package gift.domain.category.dto;
+
+public class CategoryRequest {
+
+    private String name;
+
+    private String color;
+
+    private String imageUrl;
+
+    private String description;
+
+    public CategoryRequest(){}
+
+    public CategoryRequest(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
