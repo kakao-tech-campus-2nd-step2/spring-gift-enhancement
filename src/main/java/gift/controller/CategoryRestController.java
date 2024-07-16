814c566bf0da2a58a9f8705b7b40dd43723d75cc
wvssm <neoramiregi@gmail.com>
2024-07-16 16:48:01 +0900

    feat(CategoryRestController): CategoryRestController 생성

--- src/main/java/gift/controller/CategoryRestController.java
+++ src/main/java/gift/controller/CategoryRestController.java
@@ -1,11 +1,13 @@
 package gift.controller;
 
 import gift.dto.request.CategoryRequest;
-import gift.repository.CategoryRepository;
+import gift.dto.response.CategoryResponse;
 import gift.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -20,5 +22,27 @@ public class CategoryRestController {
     public ResponseEntity<Void> addCategory(@RequestBody CategoryRequest categoryRequest){
         categoryService.save(categoryRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories(){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id){
+        categoryService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateCategoryById(@PathVariable Long id, @RequestBody CategoryRequest categoryRequest){
+        categoryService.updateById(id,categoryRequest);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
