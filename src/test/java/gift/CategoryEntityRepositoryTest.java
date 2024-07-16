0eb59785fd603ddeadddbd4ba4c301505709d188
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-16 14:48:38 +0900

    feat : CategoryRepository의 테스트 코드 추가

--- src/test/java/gift/CategoryEntityRepositoryTest.java
+++ src/test/java/gift/CategoryEntityRepositoryTest.java
@@ -1,2 +1,37 @@
-package gift;public class CategoryEntityRepositoryTest {
+package gift;
+
+import gift.Model.Entity.CategoryEntity;
+import gift.Repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+public class CategoryEntityRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void save(){
+        CategoryEntity expected = new CategoryEntity("test", "test", "test", "test");
+        CategoryEntity actual = categoryRepository.save(expected);
+        assertAll(
+                () -> assertThat(actual.getId()).isNotNull(),
+                () -> assertThat(actual.getName()).isEqualTo(expected.getName())
+        );
+    }
+
+    @Test
+    void findByName() {
+        String expectedName = "test1";
+        String expectedColor = "test2";
+        String expectedImageUrl = "test3";
+        String expectedDescription = "test4";
+        categoryRepository.save(new CategoryEntity(expectedName, expectedColor, expectedImageUrl, expectedDescription));
+        String actual = categoryRepository.findByName(expectedName).get().getName();
+        assertThat(actual).isEqualTo(expectedName);
+    }
 }
