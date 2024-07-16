85a820f9063127a40beeb5bb4e4892e8a2405b15
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-16 23:25:48 +0900

    feat: Product에 카테고리 컬럼 추가에 따른 코드 수정사항 반영

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -2,25 +2,31 @@ package gift.service;
 
 import gift.dto.ProductRequestDTO;
 import gift.dto.ProductsPageResponseDTO;
-import gift.dto.ProductsResponseDTO;
+import gift.model.Category;
 import gift.model.Product;
+import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public void createProduct(ProductRequestDTO productRequestDTO) {
+        Category category = categoryRepository.findById(productRequestDTO.categoryId()).orElse(null);
         Product product = new Product(productRequestDTO.name(),
                                       productRequestDTO.price(),
-                                      productRequestDTO.imageUrl());
+                                      productRequestDTO.imageUrl(),
+                                      category);
 
         productRepository.save(product);
     }
@@ -35,11 +41,13 @@ public class ProductService {
 
 
     public void updateProduct(Long id, ProductRequestDTO productRequestDTO) {
+        Category category = categoryRepository.findById(productRequestDTO.categoryId()).orElse(null);
         productRepository.findById(id).ifPresent(product -> {
-            Product updatedProduct = new Product(product.getId(),
-                    productRequestDTO.name(),
-                    productRequestDTO.price(),
-                    productRequestDTO.imageUrl());
+            Product updatedProduct = new Product(id,
+                                                 productRequestDTO.name(),
+                                                 productRequestDTO.price(),
+                                                 productRequestDTO.imageUrl(),
+                                                 category);
 
             productRepository.save(updatedProduct);
         });
