41d635f11ea5e662dfa3949bd0df147ae5eed6f9
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-16 19:09:52 +0900

    feat : CategoryService 생성

--- src/main/java/gift/model/category/Category.java
+++ src/main/java/gift/model/category/Category.java
@@ -1,18 +1,14 @@
 package gift.model.category;
 
-import gift.model.product.Product;
-import gift.model.product.ProductRequest;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "category")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/gift/service/CategoryService.java
+++ src/main/java/gift/service/CategoryService.java
@@ -1,2 +1,50 @@
-package gift.service;public class CategoryService {
+package gift.service;
+
+import gift.model.category.Category;
+import gift.model.category.CategoryRequest;
+import gift.model.category.CategoryResponse;
+import gift.repository.category.CategoryRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryResponse> getAllCategory() {
+        return categoryRepository.findAll().stream()
+            .map(CategoryResponse::from)
+            .collect(Collectors.toList());
+    }
+
+    public CategoryResponse getCategoryById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        return CategoryResponse.from(category);
+    }
+
+    public CategoryResponse addCategory(CategoryRequest categoryRequest) {
+        Category category = categoryRequest.toEntity(categoryRequest.name(), categoryRequest.color(),
+            categoryRequest.imageUrl(), categoryRequest.description());
+        return CategoryResponse.from(categoryRepository.save(category));
+    }
+
+    public CategoryResponse updateCategory(Long id, CategoryRequest categoryRequest) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        category.update(categoryRequest);
+        return CategoryResponse.from(categoryRepository.save(category));
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+
+
+
+
+
 }
