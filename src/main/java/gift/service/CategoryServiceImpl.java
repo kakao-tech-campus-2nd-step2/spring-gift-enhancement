de7dc4a091de56771078264a7c84e97317c4e428
minjae <minjae4650@gmail.com>
2024-07-17 02:34:47 +0900

    feat: CategoryServiceImpl 클래스 추가
    - CategoryService 인터페이스 구현
    - getAllCategories, createCategory, updateCategory, deleteCategory 메서드 구현
    - CategoryRepository 의존성 주입

---
+++ src/main/java/gift/service/CategoryServiceImpl.java
@@ -0,0 +1,37 @@
+package gift.service;
+
+import gift.model.Category;
+import gift.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category updatedCategory) {
+        return categoryRepository.findById(id).map(category -> {
+            category.setName(updatedCategory.getName());
+            return categoryRepository.save(category);
+        }).orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
