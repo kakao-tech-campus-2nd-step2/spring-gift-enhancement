561b3d1b93c3921513d808a8fa0fde03ac212d9b
momnpa333 <momnpa333@gmail.com>
2024-07-16 18:33:53 +0900

    feat: 카테고리 entity 제작

---
+++ src/main/java/gift/model/product/Category.java
@@ -0,0 +1,19 @@
+package gift.model.product;
+
+import gift.model.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Category extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+}
