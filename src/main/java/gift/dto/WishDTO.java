ba7e4c07cdf75a1a2d30c821529b2f79a17192e0
rdme0 <we6610@gmail.com>
2024-07-16 19:10:00 +0900

    :recycle: refactor: 레코드로 리팩터링

--- src/main/java/gift/dto/WishDTO.java
+++ src/main/java/gift/dto/WishDTO.java
@@ -1,48 +1,11 @@
 package gift.dto;
 
-import gift.entity.Product;
 import gift.entity.Wish;
-import java.util.Objects;
 
-public final class WishDTO {
-
-    private final Product product;
-    private final Integer quantity;
-
-    public WishDTO(Product product, Integer quantity) {
-        this.product = product;
-        this.quantity = quantity;
-    }
-
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
+public record WishDTO(ProductDTO productDTO, Integer quantity) {
 
     public static WishDTO convertToWishDTO(Wish wish) {
-        return new WishDTO(wish.getProduct(), wish.getQuantity());
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        var that = (WishDTO) obj;
-        return Objects.equals(this.product, that.product) &&
-                Objects.equals(this.quantity, that.quantity);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(product, quantity);
+        return new WishDTO(ProductDTO.convertToProductDTO(wish.getProduct()), wish.getQuantity());
     }
 
 
