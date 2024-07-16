7e8602830a6cd04c1a2e638d59dea1138965d435
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-16 15:16:38 +0900

    feat: 카테고리 엔티티 클래스 작성

---
+++ src/main/java/gift/domain/product/entity/Category.java
@@ -0,0 +1,40 @@
+package gift.domain.product.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 7, nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String color, String imageUrl, String description) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+}
--- src/main/java/gift/domain/product/entity/Product.java
+++ src/main/java/gift/domain/product/entity/Product.java
@@ -2,9 +2,12 @@ package gift.domain.product.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +17,10 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 
     @Column(nullable = false)
     private String name;
