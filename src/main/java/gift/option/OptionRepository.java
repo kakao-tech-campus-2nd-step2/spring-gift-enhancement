db4d650c16ba2a7f44f031e066ddde5c2716ddf8
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-16 21:59:35 +0900

    feat: Option Repository

---
+++ src/main/java/gift/option/OptionRepository.java
@@ -0,0 +1,11 @@
+package gift.option;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OptionRepository extends JpaRepository<Option, Long> {
+    public List<Option> findAllByProductId(Long id);
+}
