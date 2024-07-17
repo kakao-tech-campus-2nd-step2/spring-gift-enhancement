0e68c562d514e9af671d0e24fa51aef80e629f55
minju26 <mjalswn26@gmail.com>
2024-07-17 14:42:56 +0900

    feat: 전체 category 조회 구현

--- src/main/java/gift/domain/Category.java
+++ src/main/java/gift/domain/Category.java
@@ -3,12 +3,21 @@ package gift.domain;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "category")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/gift/dto/request/AddProductRequest.java
+++ src/main/java/gift/dto/request/AddProductRequest.java
@@ -1,15 +1,12 @@
 package gift.dto.request;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import static gift.constant.Message.*;
 
 public class AddProductRequest {
 
-    @NotEmpty(message = REQUIRED_FIELD_MSG)
+    @NotBlank(message = REQUIRED_FIELD_MSG)
     @Size(max = 15, message = LENGTH_ERROR_MSG)
     private String name;
 
@@ -17,8 +14,11 @@ public class AddProductRequest {
     @Positive(message = POSITIVE_NUMBER_REQUIRED_MSG)
     private int price;
 
-    @NotEmpty(message = REQUIRED_FIELD_MSG)
+    @NotBlank(message = REQUIRED_FIELD_MSG)
     private String imageUrl;
+
+    @NotBlank(message = REQUIRED_FIELD_MSG)
+    private String categoryName;
 
     public String getName() {
         return name;
