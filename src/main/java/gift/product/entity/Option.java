aefbde35ec5bf3c2835542541f9975139e3bb563
HSHwan <tnghks2866@gmail.com>
2024-07-17 16:20:31 +0900

    feat: Option 엔티티 및 DTO 추가

---
+++ src/main/java/gift/product/entity/Option.java
@@ -0,0 +1,63 @@
+package gift.product.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    protected Option() {
+    }
+
+    public Option(String name, Integer quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Option) {
+            return this.hashCode() == ((Option) obj).hashCode();
+        }
+        return false;
+    }
+
+}
