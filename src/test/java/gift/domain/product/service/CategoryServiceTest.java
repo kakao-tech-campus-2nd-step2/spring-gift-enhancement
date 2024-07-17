575ac5eb7b79ebd2e25fc85b82c80b1d2ccd125a
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-17 09:24:28 +0900

    test: CategoryService 테스트 케이스 추가

--- src/test/java/gift/domain/product/service/CategoryServiceTest.java
+++ src/test/java/gift/domain/product/service/CategoryServiceTest.java
@@ -1,12 +1,16 @@
 package gift.domain.product.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 import gift.domain.product.repository.CategoryJpaRepository;
 import gift.domain.product.entity.Category;
+import gift.exception.InvalidCategoryInfoException;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +47,31 @@ class CategoryServiceTest {
         // then
         then(categoryJpaRepository).should().findAll();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("카테고리 ID로 조회 서비스 테스트")
+    void readById_success() {
+        // given
+        Category expected = new Category(1L, "교환권", "#FFFFFF", "https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png", "test");
+        given(categoryJpaRepository.findById(anyLong())).willReturn(Optional.of(expected));
+
+        // when
+        Category actual = categoryService.readById(1L);
+
+        // then
+        then(categoryJpaRepository).should().findById(1L);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("카테고리 ID로 조회 실패 테스트")
+    void readById_fail() {
+        // given
+        given(categoryJpaRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> categoryService.readById(1L))
+            .isInstanceOf(InvalidCategoryInfoException.class);
     }
 }
