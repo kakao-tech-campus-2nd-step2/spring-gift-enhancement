b203362d813158e3dd80daaac4a04fbd42e28984
hjinshin <gudwls818@gmail.com>
2024-07-17 18:20:59 +0900

    feat: Product 조회 시 Option도 반환

--- src/main/java/gift/controller/dto/response/ProductResponse.java
+++ src/main/java/gift/controller/dto/response/ProductResponse.java
@@ -1,16 +1,20 @@
 package gift.controller.dto.response;
 
+import gift.model.Option;
 import gift.model.Product;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record ProductResponse(Long id, String name, int price,
                               String imageUrl, String category,
+                              List<OptionResponse> options,
                               LocalDateTime createdAt, LocalDateTime updatedAt) {
 
     public static ProductResponse from(Product product) {
         return new ProductResponse(product.getId(), product.getName(), product.getPrice(),
                 product.getImageUrl(), product.getCategory().getName(),
+                product.getOptions().stream().map(OptionResponse::from).toList(),
                 product.getCreatedAt(), product.getUpdatedAt());
     }
 }
