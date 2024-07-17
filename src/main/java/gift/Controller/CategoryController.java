8b0d1641d3bf3ac936ddb1784afaa907b7cefd2c
suno-boy <tnsgh3691@gmail.com>
2024-07-17 17:24:37 +0900

    feat: 카테고리 컨트롤러 계층 구현
    - 카테고리 자체에 대한 HTTP 요청의 응답 코드 생성
    - 사실 카테고리는 data.sql에 초기 데이터로 넣어버리는게 더 편할 것 같아서 이 코드들은 굳이 왜 존재해야하는지 모르겠음.
    - 그러나 초기 데이터를 넣는 것은 개발자가 할 수 있는 일이고, 사용자 입장에서도 카테고리 자체에 대한 관리를 할 수 있다고 이해함.

---
+++ src/main/java/gift/Controller/CategoryController.java
@@ -0,0 +1,41 @@
+package gift.Controller;
+
+import gift.DTO.CategoryDTO;
+import gift.Service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO createdCategory = categoryService.createCategory(categoryDTO);
+        return ResponseEntity.ok(createdCategory);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
+        CategoryDTO categoryDTO = categoryService.findById(id);
+        return ResponseEntity.ok(categoryDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(id, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
