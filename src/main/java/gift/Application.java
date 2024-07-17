8c467dbdac229dac90af922ddcc406afea4b5c1f
BaeJunho <bjh4498@knu.ac.kr>
2024-07-17 15:16:30 +0900

    refactor : 전처리 방식 변경
    Application 클래스에서 CommandLineRunner를 상속받아 처리하던 Category 전처리를 SQL 쿼리로 처리하는 방식으로 변경

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,28 +1,11 @@
 package gift;
 
-import gift.DTO.Category.CategoryRequest;
-import gift.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-    @Autowired
-    CategoryService categoryService;
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... strings) throws Exception {
-        String str = "교환권, 상품권, 뷰티, 패션, 식품, 리빙/도서, 레저/스포츠, 아티스트/캐릭터, 유아동/반려, 디지털/가전, 카카오프렌즈, 트렌드 선물, 백화점";
-        String[] split = str.split(", ");
-        for (String s : split) {
-            CategoryRequest category = new CategoryRequest(s);
-            categoryService.save(category);
-        }
     }
 }
