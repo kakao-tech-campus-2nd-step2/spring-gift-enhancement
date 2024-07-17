082067d1bde9d9232c0225bd8e4da0323520bb3e
dong_yxxn <g0326kdy@gmail.com>
2024-07-17 14:57:47 +0900

    refactor(Category): Constructor추가 - name단독

--- src/main/java/gift/category/domain/Category.java
+++ src/main/java/gift/category/domain/Category.java
@@ -27,6 +27,10 @@ public class Category {
     public Category() {
     }
 
+    public Category(String name) {
+        this.name = name;
+    }
+
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
