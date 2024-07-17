ca1b4f1ca706b41a7524fa6c77181971291073d2
Awhn <ahnshwan@outlook.com>
2024-07-17 20:58:29 +0900

    fix : handle error with optional

--- src/main/java/gift/service/CategoryService.java
+++ src/main/java/gift/service/CategoryService.java
@@ -3,10 +3,12 @@ package gift.service;
 import gift.domain.CategoryDTO;
 import gift.entity.Category;
 import gift.repository.CategoryRepository;
+import jakarta.persistence.PersistenceException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -19,46 +21,49 @@ public class CategoryService {
     public List<Category> findAll() {
         try {
             return categoryRepository.findAll();
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (PersistenceException e) {
+            throw new PersistenceException("Could not find all categories");
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected error");
         }
     }
 
-    public Category findById(int id) {
-        try {
-            return categoryRepository.findById(id);
-        } catch (NoSuchElementException e) {
-            throw new NoSuchElementException("Can't find category with id " + id);
+    public Optional<Category> findById(int id) {
+        var category = categoryRepository.findById(id);
+
+        if (category == null) {
+            return Optional.empty();
         }
+        return Optional.of(category);
     }
 
     public Category add(CategoryDTO categoryDTO) {
-        Category category = new Category(categoryDTO.name(), categoryDTO.color(), categoryDTO.imgUrl(), categoryDTO.description());
+        Category category = new Category(categoryDTO.name(), categoryDTO.color(), categoryDTO.imageUrl(), categoryDTO.description());
         try {
             return categoryRepository.save(category);
+        } catch (PersistenceException e) {
+            throw new PersistenceException("Could not find all categories");
         } catch (Exception e) {
-            throw new RuntimeException("Can't save category " + categoryDTO);
+            throw new RuntimeException("Unexpected error");
         }
     }
 
-    public Category update(int id, CategoryDTO categoryDTO) {
-
-        Category category = new Category(id, categoryDTO.name(), categoryDTO.color(), categoryDTO.imgUrl(), categoryDTO.description());
-
-        try {
-            return categoryRepository.save(category);
+    public Optional<Category> update(int id, CategoryDTO categoryDTO) {
+        Category category = categoryRepository.findById(id);
+        if (category == null) {
+            return Optional.empty();
         }
-        catch (NoSuchElementException e) {
-            throw new NoSuchElementException("Can't find category with id " + category.getId());
-        }
+        return Optional.of(categoryRepository.save(new Category(id, categoryDTO.name(), categoryDTO.color(), categoryDTO.imageUrl(), categoryDTO.description())));
     }
 
     public void delete(Integer id) {
         try {
             categoryRepository.deleteById(id);
-        }
-        catch (Exception e) {
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("There is no Category with id:" + id);
+        } catch (PersistenceException e) {
+            throw new PersistenceException("Can't delete category " + id);
+        } catch (Exception e) {
             throw new RuntimeException("Can't delete category " + id);
         }
     }
