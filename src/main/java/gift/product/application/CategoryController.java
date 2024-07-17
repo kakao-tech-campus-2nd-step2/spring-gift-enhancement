8924690908c3ebecb2d962aa07873537a9236725
sanghee0820 <lsh277604@gmail.com>
2024-07-17 22:46:59 +0900

    feat: Category 삭제 API 추가

--- src/main/java/gift/product/application/CategoryController.java
+++ src/main/java/gift/product/application/CategoryController.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,11 @@ public class CategoryController {
                 .toList();
         return ResponseEntity.ok()
                 .body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
     }
 }
