feea254b022c91ab66b4f6b4877b69bcc08cd123
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-17 22:35:13 +0900

    test: Member Argument Resolver 등록으로 인한 테스트코드 수정

--- src/test/java/gift/member/application/MemberServiceTest.java
+++ src/test/java/gift/member/application/MemberServiceTest.java
@@ -73,49 +73,25 @@ public class MemberServiceTest {
     @Test
     void 이메일_업데이트_테스트() {
         // Given
-        MemberEmailUpdateCommand command = new MemberEmailUpdateCommand(1L, "new@example.com");
-        when(memberRepository.findById(command.id())).thenReturn(Optional.of(member));
+        MemberEmailUpdateCommand command = new MemberEmailUpdateCommand("new@example.com");
 
         // When
-        assertDoesNotThrow(() -> memberService.updateEmail(command));
+        assertDoesNotThrow(() -> memberService.updateEmail(command, member));
 
         // Then
         Assertions.assertThat(member.getEmail()).isEqualTo("new@example.com");
-        verify(memberRepository, times(1)).findById(command.id());
-    }
-
-    @Test
-    void 이메일_업데이트_테스트_회원_없음() {
-        // Given
-        MemberEmailUpdateCommand command = new MemberEmailUpdateCommand(1L, "new@example.com");
-        when(memberRepository.findById(command.id())).thenReturn(Optional.empty());
-
-        // When & Then
-        assertThrows(NotFoundException.class, () -> memberService.updateEmail(command));
     }
 
     @Test
     void 비밀번호_업데이트_테스트() {
         // Given
-        MemberPasswordUpdateCommand command = new MemberPasswordUpdateCommand(1L, "newPassword");
-        when(memberRepository.findById(command.id())).thenReturn(Optional.of(member));
+        MemberPasswordUpdateCommand command = new MemberPasswordUpdateCommand("newPassword");
 
         // When
-        assertDoesNotThrow(() -> memberService.updatePassword(command));
+        assertDoesNotThrow(() -> memberService.updatePassword(command, member));
 
         // Then
         Assertions.assertThat(member.getPassword()).isEqualTo("newPassword");
-        verify(memberRepository, times(1)).findById(command.id());
-    }
-
-    @Test
-    void 비밀번호_업데이트_테스트_회원_없음() {
-        // Given
-        MemberPasswordUpdateCommand command = new MemberPasswordUpdateCommand(1L, "newPassword");
-        when(memberRepository.findById(command.id())).thenReturn(Optional.empty());
-
-        // When & Then
-        assertThrows(NotFoundException.class, () -> memberService.updatePassword(command));
     }
 
     @Test
@@ -159,15 +135,13 @@ public class MemberServiceTest {
     @Test
     void 회원_삭제_테스트() {
         // Given
-        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
         doNothing().when(memberRepository).delete(member);
         doNothing().when(wishlistRepository).deleteAllByMemberId(member.getId());
 
         // When
-        assertDoesNotThrow(() -> memberService.delete(member.getId()));
+        assertDoesNotThrow(() -> memberService.delete(member));
 
         // Then
-        verify(memberRepository, times(1)).findById(member.getId());
         verify(memberRepository, times(1)).delete(member);
         verify(wishlistRepository, times(1)).deleteAllByMemberId(member.getId());
     }
