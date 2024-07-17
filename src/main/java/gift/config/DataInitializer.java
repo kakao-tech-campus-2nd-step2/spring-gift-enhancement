3e7753f66df4c943370abc9c8446a0df02ce7f6c
J-1ac <tkdwns26@naver.com>
2024-07-17 22:30:28 +0900

    feat: 관리자 화면 상품 추가 카테고리 지정 기능 구현

--- src/main/java/gift/config/DataInitializer.java
+++ src/main/java/gift/config/DataInitializer.java
@@ -54,7 +54,7 @@ public class DataInitializer {
                 Product product = new Product(
                     "Product " + i,
                     random.nextLong(100000) + 1000,
-                    "http://example.com/image" + i + ".jpg",
+                    "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240508101036_6c7f02cb957848a69a25018a664a3c89.jpg",
                     randomCategory
                 );
                 products.add(product);
