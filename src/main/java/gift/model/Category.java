0274a1f4a115f0173b4bca2934ea69542f85db7d
seojiu <seojiu5598@gmail.com>
2024-07-17 22:03:36 +0900

    fix(ProductRepositoryTests.java, WishlistRepositoryTests.java, Category.java): 테스트 코드 수정

--- src/main/java/gift/model/Category.java
+++ src/main/java/gift/model/Category.java
@@ -32,6 +32,10 @@ public class Category {
         this.name = name;
     }
 
+    public Category(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
--- src/test/java/gift/repositoryTest/ProductRepositoryTests.java
+++ src/test/java/gift/repositoryTest/ProductRepositoryTests.java
@@ -1,6 +1,8 @@
 package gift.repositoryTest;
 
+import gift.model.Category;
 import gift.model.Product;
+import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,14 @@ class ProductRepositoryTests {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Test
     void testSaveAndFindProduct() {
-        Product product = new Product("상품1", 1000, "http://example.com/image.jpg");
+        Category category = new Category("TestCategory");
+        categoryRepository.save(category);
+        Product product = new Product("상품1", 1000, "http://example.com/image.jpg",category);
         productRepository.save(product);
 
         Optional<Product> foundProduct = productRepository.findById(product.getId());
@@ -29,17 +35,19 @@ class ProductRepositoryTests {
 
     @Test
     void testSaveValidNameProduct() {
+        Category category = new Category("TestCategory");
+        categoryRepository.save(category);
         assertThrows(IllegalArgumentException.class, () -> {
-            Product product = new Product("카카오", 1000, "http://example.com/image.jpg");
+            Product product = new Product("카카오", 1000, "http://example.com/image.jpg",category);
             productRepository.save(product);
         });
 
         assertThrows(IllegalArgumentException.class, () -> {
-            Product product = new Product("kakao", 1000, "http://example.com/image.jpg");
+            Product product = new Product("kakao", 1000, "http://example.com/image.jpg",category);
             productRepository.save(product);
         });
 
-        Product validProduct = new Product("상품2", 2000, "http://example.com/image2.jpg");
+        Product validProduct = new Product("상품2", 2000, "http://example.com/image2.jpg",category);
         productRepository.save(validProduct);
         Optional<Product> foundProduct = productRepository.findById(validProduct.getId());
         assertThat(foundProduct).isPresent();
