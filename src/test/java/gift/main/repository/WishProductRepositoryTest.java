8ad261043a26df7aae39d719b2e910e46fdd56f4
JinSeoHyun <15roa1121@gmail.com>
2024-07-18 08:44:36 +0900

    remove(WishProductRepositoryTest) : 유저 삭제에 대한 테스트는 userRepository로 옮김

--- src/test/java/gift/main/repository/WishProductRepositoryTest.java
+++ src/test/java/gift/main/repository/WishProductRepositoryTest.java
@@ -1,65 +1,33 @@
 package gift.main.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import gift.main.controller.AdminProductController;
-import gift.main.entity.Category;
-import gift.main.entity.Product;
-import gift.main.entity.User;
-import gift.main.entity.WishProduct;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class WishProductRepositoryTest {
+
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final EntityManager entityManager;
+    private final WishProductRepository wishProductRepository;
+
     @Autowired
-    private  WishProductRepository wishProductRepository;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private ProductRepository productRepository;
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Test
-    public void 유저삭제시_해당위시리스트_정상삭제() {
-        //이부분 정상 작동 X
-        User user = userRepository.save(new User("테스트용", "email", "123", "ADMIN"));
-        User seller = userRepository.save(new User("셀러", "셀러", "123", "ADMIN"));
-
-        Product product1 = productRepository.save(new Product("테스트용1", 1000, "url", seller, categoryRepository.getById(1l)));
-        Product product2 = productRepository.save(new Product("테스트용2", 1000, "url", seller, categoryRepository.getById(1l)));
-
-        wishProductRepository.save(new WishProduct(product1, user));
-        wishProductRepository.save(new WishProduct(product2, user));
-
-
-        Assertions.assertFalse(wishProductRepository.findAll().isEmpty());
-        System.out.println("wishProductRepository.findAll() = " + wishProductRepository.findAll());
-        Assertions.assertDoesNotThrow(() -> userRepository.delete(user));
-        Assertions.assertFalse(() -> userRepository.existsByEmail("email"));
-        System.out.println("wishProductRepository.findAll() = " + wishProductRepository.findAll());
-        Assertions.assertTrue(wishProductRepository.findAll().isEmpty());
-
-
-    }
-
-
-
-
-    @Test
-    public void 값삭제하기() {
-
+    WishProductRepositoryTest(UserRepository userRepository,
+                              ProductRepository productRepository,
+                              CategoryRepository categoryRepository,
+                              EntityManager entityManager,
+                              WishProductRepository wishProductRepository) {
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+        this.entityManager = entityManager;
+        this.wishProductRepository = wishProductRepository;
     }
 
 
