968a85421d363b3ecb94806bfcca2210d1a7741f
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-18 03:46:31 +0900

    feat: CategoryDto 생성

---
+++ src/main/java/gift/dto/RequestCategoryDto.java
@@ -0,0 +1,32 @@
+package gift.dto;
+
+public class RequestCategoryDto {
+
+    private String name;
+    private String color;
+    private String imageUrl;
+    private String description;
+
+    public RequestCategoryDto(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
