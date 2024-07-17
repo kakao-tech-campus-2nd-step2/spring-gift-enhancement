cd6786a3b6f214f4951baa7962894a10b9c37277
JJY <ddglackrp@gmail.com>
2024-07-17 23:07:05 +0900

    feat "Option Repository 추가"

---
+++ src/main/java/gift/repository/option/OptionRepository.java
@@ -0,0 +1,14 @@
+package gift.repository.option;
+
+import gift.domain.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+
+    Optional<Option> findOptionByNameAndProductId(String name, Long productId);
+
+    List<Option> findOptionsByProductId(Long productId);
+}
