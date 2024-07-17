678350eaeec898bb407c613db69cd930f1c85a69
sunandrabbit <sunandrabbit@gamil.com>
2024-07-17 14:08:57 +0900

    refactor(MemberEntity) : Service에 존재하던 DTO 변환 클래스를 옮김

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -28,8 +28,11 @@ public class MemberService {
         memberRepository.findByEmail(member.getEmail())
             .ifPresent(m -> { throw new AlreadyExistsException("Already Exists Member"); });
         member.setPassword(PasswordUtil.encodePassword(member.getPassword()));
-        MemberEntity loginMemberEntity = memberRepository.save(dtoToEntity(member));
-        return new Token(jwtUtil.generateToken(entityToDto(loginMemberEntity)));
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setEmail(member.getEmail());
+        memberEntity.setPassword(member.getPassword());
+        MemberEntity loginMemberEntity = memberRepository.save(memberEntity);
+        return new Token(jwtUtil.generateToken(MemberEntity.toDto(loginMemberEntity)));
     }
 
     @Transactional
@@ -37,18 +40,9 @@ public class MemberService {
         MemberEntity memberEntity = memberRepository.findByEmail(member.getEmail())
             .orElseThrow(() -> new ForbiddenException("Invalid email or Not Exists Member"));
         if (PasswordUtil.matches(member.getPassword(), memberEntity.getPassword())) {
-            return new Token(jwtUtil.generateToken(entityToDto(memberEntity)));
+            return new Token(jwtUtil.generateToken(MemberEntity.toDto(memberEntity)));
         }
         throw new ForbiddenException("Invalid password");
-    }
-
-    private Member entityToDto(MemberEntity memberEntity) {
-        return new Member(memberEntity.getId(), memberEntity.getEmail(),
-            memberEntity.getPassword());
-    }
-
-    private MemberEntity dtoToEntity(Member member) {
-        return new MemberEntity(member.getEmail(), member.getPassword());
     }
 
 }
