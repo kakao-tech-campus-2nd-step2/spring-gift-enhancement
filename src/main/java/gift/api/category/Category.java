156b6f8a81d44b0d4c1903e9e1822a98c4adee57
Hyewon Seok <hws2008@naver.com>
2024-07-18 02:54:57 +0900

    fix: add missing category attributes

--- src/main/java/gift/api/category/Category.java
+++ src/main/java/gift/api/category/Category.java
@@ -1,6 +1,7 @@
 package gift.api.category;
 
 import gift.global.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -8,16 +9,30 @@ import jakarta.persistence.UniqueConstraint;
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(name = "uk_category", columnNames = {"name"}))
 public class Category extends BaseEntity {
+    @Column(nullable = false)
     private String name;
+    @Column(length = 7, nullable = false)
+    private String color;
+    @Column(nullable = false)
+    private String imageUrl;
+    private String description;
 
     protected Category(){
     }
 
-    public Category(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO CATEGORY(name) VALUES ('교환권');
-INSERT INTO CATEGORY(name) VALUES ('상품권');
-INSERT INTO CATEGORY(name) VALUES ('뷰티');
-INSERT INTO CATEGORY(name) VALUES ('패션');
-INSERT INTO CATEGORY(name) VALUES ('식품');
+INSERT INTO CATEGORY(name, color, image_url, description) VALUES ('교환권', '#6c95d1', 'https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png', '');
+INSERT INTO CATEGORY(name, color, image_url, description) VALUES ('상품권', '#8fcc5d', 'https://any-image.net/giftcard.png', '');
+INSERT INTO CATEGORY(name, color, image_url, description) VALUES ('뷰티', '#e09edc', 'https://any-image.net/beauty.png', '');
+INSERT INTO CATEGORY(name, color, image_url, description) VALUES ('패션', '#304b99', 'https://any-image.net/fashion.png', '');
+INSERT INTO CATEGORY(name, color, image_url, description) VALUES ('식품', '#42bf3c', 'https://any-image.net/food.png', '');
 
 INSERT INTO PRODUCT(category_id, name, price, image_url) VALUES (1, '아이스 카페 아메리카노 T', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
 INSERT INTO PRODUCT(category_id, name, price, image_url) VALUES (1, '아이스 카페 라떼 T', 5000, 'https://some/cafe/latte/image');
