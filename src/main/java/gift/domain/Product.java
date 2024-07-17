4cf7aaf9a51288a9185b4999aac5d97448680697
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-17 20:37:37 +0900

    feat: ProductServiceTest 수정

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -38,6 +38,14 @@ public class Product {
         this.category = category;
     }
 
+    public Product(long id, String name, int price, String imageUrl, Category category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+    }
+
     public long getId() {
         return id;
     }
--- src/test/java/gift/service/ProductServiceTest.java
+++ src/test/java/gift/service/ProductServiceTest.java
@@ -1,63 +1,177 @@
 package gift.service;
 
-import gift.dto.request.ProductRequest;
+import gift.domain.Category;
 import gift.domain.Product;
+import gift.dto.request.ProductRequest;
+import gift.exception.CategoryNotFoundException;
+import gift.exception.InvalidProductDataException;
 import gift.exception.ProductNotFoundException;
+import gift.repository.category.CategorySpringDataJpaRepository;
 import gift.repository.product.ProductSpringDataJpaRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
-    @Autowired
-    private ProductService productService;
-
-    @MockBean
+    @Mock
     private ProductSpringDataJpaRepository productRepository;
 
-    @Test
-    public void testRegister() {
-        ProductRequest productRequest = new ProductRequest("Product 1", 100, "test-url", "testCategory");
-        Product product = Product.RequestToEntity(productRequest);
+    @Mock
+    private CategorySpringDataJpaRepository categoryRepository;
 
-        given(productRepository.save(Mockito.any(Product.class))).willReturn(product);
+    @InjectMocks
+    private ProductService productService;
 
-        Product savedProduct = productService.register(productRequest);
+    private List<Product> mockProducts;
+    private List<Category> mockCategories;
 
-        assertThat(savedProduct.getName()).isEqualTo("Product 1");
+    @BeforeEach
+    public void setUp() {
+        mockCategories = new ArrayList<>();
+        mockCategories.add(new Category(1L, "패션"));
+        mockCategories.add(new Category(2L, "뷰티"));
     }
 
     @Test
-    public void testFindOne() {
+    public void testRegisterProduct() {
+        ProductRequest productRequest = new ProductRequest("상의", 800, "상의.jpg", "패션");
+        Category mockCategory = mockCategories.getFirst();
+        when(categoryRepository.findByName("패션")).thenReturn(Optional.of(mockCategory));
+
+        Product registeredProduct = productService.register(productRequest);
+
+        assertEquals("상의", registeredProduct.getName());
+        assertEquals(800, registeredProduct.getPrice());
+        assertEquals("상의.jpg", registeredProduct.getImageUrl());
+        assertEquals(mockCategory, registeredProduct.getCategory());
+
+        verify(categoryRepository, times(1)).findByName("패션");
+        verify(productRepository, times(1)).save(any(Product.class));
+    }
+
+    @Test
+    public void testRegisterProductCategoryNotFound() {
+        ProductRequest productRequest = new ProductRequest("상의", 800, "상의.jpg", "패션");
+        when(categoryRepository.findByName("패션")).thenReturn(Optional.empty());
+
+        assertThrows(CategoryNotFoundException.class, () -> productService.register(productRequest));
+
+        verify(categoryRepository, times(1)).findByName("패션");
+        verify(productRepository, never()).save(any());
+    }
+
+    @Test
+    public void testGetProducts() {
+        Pageable pageable = Pageable.unpaged();
+        when(productRepository.findAll(pageable)).thenReturn(Page.empty());
+
+        Page<Product> products = productService.getProducts(pageable);
+
+        assertNotNull(products);
+        assertEquals(0, products.getTotalElements());
+
+        verify(productRepository, times(1)).findAll(pageable);
+    }
+
+    @Test
+    public void testFindOneProduct() {
+        mockProducts = new ArrayList<>();
+        mockProducts.add(new Product(1L, "상의", 1000, "상의.jpg", mockCategories.getFirst()));
+
         Long productId = 1L;
-        Product product = new Product("Product 1", 100, "test-url", "testCategory");
-        product.setId(productId);
+        when(productRepository.findById(productId)).thenReturn(Optional.of(mockProducts.getFirst()));
 
-        given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
+        Product product = productService.findOne(productId);
 
-        Product foundProduct = productService.findOne(product.getId());
+        assertEquals("상의", product.getName());
 
-        assertThat(foundProduct.getName()).isEqualTo("Product 1");
+        verify(productRepository, times(1)).findById(productId);
     }
 
     @Test
-    public void testFindOneWithNonExistentId() {
-        Long nonExistentId = 999L;
+    public void testFindOneProductNotFound() {
+        Long productId = 11L;
+        when(productRepository.findById(productId)).thenReturn(Optional.empty());
 
-        given(productRepository.findById(nonExistentId)).willReturn(Optional.empty());
+        assertThrows(ProductNotFoundException.class, () -> productService.findOne(productId));
 
-        assertThrows(ProductNotFoundException.class, () -> {
-            productService.findOne(nonExistentId);
-        });
+        verify(productRepository, times(1)).findById(productId);
+    }
+
+    @Test
+    public void testUpdateProduct() {
+        mockProducts = new ArrayList<>();
+        mockProducts.add(new Product(1L, "상의", 1000, "상의.jpg", mockCategories.getFirst()));
+
+        Long productId = 1L;
+        ProductRequest productRequest = new ProductRequest("하의", 1200, "하의.jpg", "패션");
+        Category mockCategory = mockCategories.getFirst();
+        Product existingProduct = mockProducts.getFirst();
+        when(productRepository.findById(productId)).thenReturn(Optional.of(existingProduct));
+        when(categoryRepository.findByName("패션")).thenReturn(Optional.of(mockCategory));
+
+        Product updatedProduct = productService.update(productId, productRequest);
+
+        assertEquals("하의", updatedProduct.getName());
+        assertEquals(1200, updatedProduct.getPrice());
+        assertEquals("하의.jpg", updatedProduct.getImageUrl());
+        assertEquals(mockCategory, updatedProduct.getCategory());
+
+        verify(productRepository, times(1)).findById(productId);
+        verify(categoryRepository, times(1)).findByName("패션");
+        verify(productRepository, times(1)).save(any(Product.class));
+    }
+
+    @Test
+    public void testUpdateProductNotFound() {
+        Long productId = 11L;
+        ProductRequest productRequest = new ProductRequest("하의", 1200, "하의.jpg", "패션");
+        when(productRepository.findById(productId)).thenReturn(Optional.empty());
+
+        assertThrows(ProductNotFoundException.class, () -> productService.update(productId, productRequest));
+
+        verify(productRepository, times(1)).findById(productId);
+        verify(categoryRepository, never()).findByName(any());
+        verify(productRepository, never()).save(any());
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Long productId = 1L;
+        Product existingProduct = mockProducts.getFirst();
+        when(productRepository.findById(productId)).thenReturn(Optional.of(existingProduct));
+
+        Product deletedProduct = productService.delete(productId);
+
+        assertEquals(existingProduct, deletedProduct);
+
+        verify(productRepository, times(1)).findById(productId);
+        verify(productRepository, times(1)).delete(existingProduct);
+    }
+
+    @Test
+    public void testDeleteProductNotFound() {
+        Long productId = 11L;
+        when(productRepository.findById(productId)).thenReturn(Optional.empty());
+
+        assertThrows(ProductNotFoundException.class, () -> productService.delete(productId));
+
+        verify(productRepository, times(1)).findById(productId);
+        verify(productRepository, never()).delete(any());
     }
 }
