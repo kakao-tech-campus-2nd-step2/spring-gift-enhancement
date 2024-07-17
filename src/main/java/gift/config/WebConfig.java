e3098ec9c4418a1e88865733f722977b1d44e6c6
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-17 22:25:47 +0900

    feat: Argument Resolver등록 및 interceptor path관련 설정 추가

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -1,21 +1,39 @@
 package gift.config;
 
+import gift.member.application.MemberService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final MemberService memberService;
 
-    public WebConfig(AuthInterceptor authInterceptor) {
+    public WebConfig(AuthInterceptor authInterceptor, MemberService memberService) {
         this.authInterceptor = authInterceptor;
+        this.memberService = memberService;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/wishlist/**");
+                .addPathPatterns("/api/wishlist/**")
+                .addPathPatterns("/api/products/**")
+                .addPathPatterns("/api/member/**")
+                .addPathPatterns("/api/categories/**")
+                .excludePathPatterns("/api/member/join")
+                .excludePathPatterns("/api/member/login");
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberArgumentResolver(memberService));
+
     }
 }
--- src/main/java/gift/member/application/MemberResponse.java
+++ src/main/java/gift/member/application/MemberResponse.java
@@ -10,4 +10,7 @@ public record MemberResponse(
     public static MemberResponse from(Member member) {
         return new MemberResponse(member.getId(), member.getEmail(), member.getPassword());
     }
+    public Member toEntity() {
+        return new Member(id, email, password);
+    }
 }
