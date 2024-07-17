c013807a86b359921bbe41b1e3434262c22d23de
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-17 21:22:33 +0900

    fix: category를 찾지 못할 경우 null이 아닌 예외를 던지도록 수정

--- src/main/java/gift/service/CategoryService.java
+++ src/main/java/gift/service/CategoryService.java
@@ -1,6 +1,7 @@
 package gift.service;
 
 import gift.dto.CategoryResponseDTO;
+import gift.exceptions.CustomException;
 import gift.model.Category;
 import gift.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,9 @@ public class CategoryService {
     public CategoryResponseDTO getAllCategories() {
         List<Category> categoryList = categoryRepository.findAll();
         return new CategoryResponseDTO(categoryList);
+    }
+
+    public Category getCategoryById(Long categoryId) {
+        return categoryRepository.findById(categoryId).orElseThrow(CustomException::categoryNotFoundException);
     }
 }
