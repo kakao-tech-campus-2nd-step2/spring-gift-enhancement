0341cd937c7b5780f7f4c79477e7b36d8196ebfd
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-17 22:00:42 +0900

    feat : 카테고리 엔티티 만들기

---
+++ src/main/java/gift/model/Category.java
@@ -0,0 +1,47 @@
+package gift.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    protected Category() {
+    }
+
+    public Category(Long id, String name) {
+        validateName(name);
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("카테고리 이름을 입력하세요.");
+        }
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("카테고리 이름을 입력하세요.");
+        }
+    }
+
+}
