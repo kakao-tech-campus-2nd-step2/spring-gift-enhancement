5a7857ce802faf7f0c2abdf123a921a8024e7b80
donghyuun <bomul0524@gmail.com>
2024-07-17 16:36:57 +0900

    refactor: 코드 reformat

--- src/main/java/gift/domain/user/dto/UserDTO.java
+++ src/main/java/gift/domain/user/dto/UserDTO.java
@@ -19,20 +19,20 @@ public class UserDTO {
     public UserDTO() {
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public User toUser() {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,26 +11,54 @@ CREATE TABLE product
     image_url VARCHAR(255)
 );
 
-ALTER TABLE product ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE product
+    ALTER COLUMN id RESTART WITH 1;
 
 -- 유저 --
 CREATE TABLE users
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    email     VARCHAR(255) NOT NULL UNIQUE,
-    password  VARCHAR(255) NOT NULL
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE users ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE users
+    ALTER COLUMN id RESTART WITH 1;
 
 -- 장바구니 --
-CREATE TABLE IF NOT EXISTS cart (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    product_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS cart
+(
+    id
+    INT
+    AUTO_INCREMENT
+    PRIMARY
+    KEY,
+    user_id
+    INT
+    NOT
+    NULL,
+    product_id
+    INT
+    NOT
+    NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
-);
+    FOREIGN
+    KEY
+(
+    user_id
+) REFERENCES users
+(
+    id
+) ON DELETE CASCADE,
+    FOREIGN KEY
+(
+    product_id
+) REFERENCES product
+(
+    id
+)
+  ON DELETE CASCADE
+    );
 
-ALTER TABLE cart ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE cart
+    ALTER COLUMN id RESTART WITH 1;
