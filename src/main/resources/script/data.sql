3ca5887683cfaf020a087d70872da2a6bd56714d
HSHwan <tnghks2866@gmail.com>
2024-07-17 16:21:18 +0900

    chore: option 테이블 초기 설정 추가

--- src/main/resources/script/data.sql
+++ src/main/resources/script/data.sql
@@ -8,6 +8,14 @@ VALUES ('product1', 4500, 'https://shop.io/product/1/image.jpg', 1),
        ('product2', 8000, 'https://shop.io/product/2/image.jpg', 3),
        ('product3', 20000, 'https://shop.io/product/3/image.jpg', 2);
 
+INSERT INTO option(name, quantity, product_id)
+VALUES ('옵션1', 100, 1),
+       ('옵션2', 5, 1),
+       ('옵션1', 20, 2),
+       ('옵션2', 3, 2),
+       ('옵션1', 60, 3),
+       ('옵션2', 1, 3);
+
 INSERT INTO member(email, password)
 VALUES ('admin@email.com', 'password'),
        ('testuser@gmail.com', 'user123'),
