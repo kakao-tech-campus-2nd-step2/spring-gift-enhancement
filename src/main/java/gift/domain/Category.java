ac4d4637533873e298ff0563b357df479a5ae5aa
sunandrabbit <sunandrabbit@gamil.com>
2024-07-17 14:42:15 +0900

    feat(Category) : id를 포함한 생성자 추가

--- src/main/java/gift/domain/Category.java
+++ src/main/java/gift/domain/Category.java
@@ -34,6 +34,14 @@ public class Category {
         this.description = description;
     }
 
+    public Category(Long id, String name, String color, String imageUrl, String description) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
     public Long getId() {
         return id;
     }
