e82d0e4b20e1b4f26612e71ede5d053afb28c937
Awhn <ahnshwan@outlook.com>
2024-07-17 20:58:08 +0900

    fix : delete unnecessary method

--- src/main/java/gift/repository/CategoryRepository.java
+++ src/main/java/gift/repository/CategoryRepository.java
@@ -3,11 +3,7 @@ package gift.repository;
 import gift.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    List<Category> findAll();
     Category findById(int id);
     void deleteById(int id);
-    Category save(Category category);
 }
