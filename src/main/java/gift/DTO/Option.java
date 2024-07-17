1f28b5f81170cfba735cb27e4e213626b7958fb7
test1 <test>
2024-07-17 18:07:39 +0900

    feat : Option 생성

---
+++ src/main/java/gift/DTO/Option.java
@@ -0,0 +1,49 @@
+package gift.DTO;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Option {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false,unique = true)
+  private String name;
+
+  @Column(nullable = false)
+  private int quantity;
+
+  public Option(Long id, String name, int quantity){
+    this.id=id;
+    this.name=name;
+    this.quantity=quantity;
+  }
+
+  public Option(String name, int quantity){
+    this.name=name;
+    this.quantity=quantity;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+}
