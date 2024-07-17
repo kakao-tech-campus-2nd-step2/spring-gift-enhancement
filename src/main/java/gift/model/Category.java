24f676014e0a292291a41339d9aa955dcf6f9074
mac <20j.code@gmail.com>
2024-07-17 21:44:52 +0900

    feat : Category 엔티티 추가

---
+++ src/main/java/gift/model/Category.java
@@ -0,0 +1,54 @@
+package gift.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="CATEGORY_TABLE")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CATEGORY_ID")
+    private Long id;
+
+    @Column(name = "CATEGORY_NAME")
+    private String name;
+
+    @Column(name = "CATEGORY_COLOR",length = 7)
+    private String color;
+
+    @Column(name = "CATEGORY_DESCRIPTION")
+    private String description;
+
+    @Column(name = "CATEGORY_IMAGE_URL")
+    private String imageUrl;
+
+    public Category() {
+    }
+
+    //getter
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
