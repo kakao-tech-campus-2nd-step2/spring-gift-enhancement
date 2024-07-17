e47c1278dc68bdbb5f309ca0e7dbfe1c3985ebe5
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-17 14:49:31 +0900

    feat: option Entity , option Repository

---
+++ src/main/java/gift/entity/Option.java
@@ -0,0 +1,37 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Option() {
+    }
+
+    public Option(String name, Integer quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+}
