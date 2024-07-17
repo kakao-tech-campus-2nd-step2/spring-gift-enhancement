1ec8cd0a25cd0d9cbff3224f2d1eb229a07047fe
Dalsungmin <cnww123@naver.com>
2024-07-17 09:53:19 +0900

    Feat: 카테고리 조회 메서드 테스트
    카테고리를 생성해 저장한 뒤 ID로 조회 후 그 이름이 저장한 값과 같은지 비교

---
+++ src/test/java/gift/Product/CategoryRepositoryTest.java
@@ -0,0 +1,26 @@
+package gift.Product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gift.model.Category;
+import gift.model.Product;
+import gift.repository.CategoryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class CategoryRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void findById() {
+        Category category = new Category("교환권");
+        Category expected = categoryRepository.save(category);
+        Category actual = categoryRepository.findById(expected.getId()).get();
+        assertThat(expected.getCategoryName()).isEqualTo(actual.getCategoryName());
+    }
+}
