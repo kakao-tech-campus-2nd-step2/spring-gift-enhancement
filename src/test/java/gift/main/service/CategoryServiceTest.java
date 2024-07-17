cb2a9b196320f748754dfdb97a6966333707ef3c
JinSeoHyun <15roa1121@gmail.com>
2024-07-17 23:23:34 +0900

    fetch() : Category에 수정 로직 추가, 해당 로직 테스트
    [구현 내용]
    1. 수정 시, 동일한 이름이나 고유번호 X
    2. 테스트 코드에서 동일한 이름과 고유번호에 대한 에러 발생 테스트

--- src/test/java/gift/main/service/CategoryServiceTest.java
+++ src/test/java/gift/main/service/CategoryServiceTest.java
@@ -1,7 +1,68 @@
-//package gift.main.service;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CategoryServiceTest {
-//
-//}
+package gift.main.service;
+
+import gift.main.Exception.CustomException;
+import gift.main.dto.CategoryRequest;
+import gift.main.entity.Category;
+import gift.main.repository.CategoryRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@SpringBootTest
+class CategoryServiceTest {
+    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
+    private final EntityManager entityManager;
+    @Autowired
+    CategoryServiceTest(CategoryRepository categoryRepository, CategoryService categoryService, EntityManager entityManager) {
+        this.categoryRepository = categoryRepository;
+        this.categoryService = categoryService;
+        this.entityManager = entityManager;
+    }
+
+    @Test
+    public void updateCategoryTest() {
+        //given
+        Category category = categoryRepository.findByName("패션").get();
+        CategoryRequest udateCategoryRequest = new CategoryRequest(1000, "의류");
+
+        //when
+        categoryService.updateCategory(category.getId(), udateCategoryRequest);
+
+        //then
+        assertThat(categoryRepository.findByUniNumber(1000).get())
+                .isEqualTo(category);
+
+    }
+
+    @Test
+    public void addDuplicateNameCategoryTest() {
+        //given
+        Category category = categoryRepository.findByName("패션").get();
+        final String DUPLICATE_NAME = category.getName();
+        CategoryRequest categoryRequest = new CategoryRequest(100, DUPLICATE_NAME);
+
+
+        //when - then
+        assertThatThrownBy(() -> categoryService.addCategory(categoryRequest))
+                .isInstanceOf(CustomException.class);
+    }
+
+    @Test
+    public void updateDuplicateUniNumberCategoryTest() {
+        //given
+        Category category = categoryRepository.findByName("패션").get();
+        final int DUPLICATE_UNI_NUMER = category.getUniNumber();
+        CategoryRequest categoryRequest = new CategoryRequest(DUPLICATE_UNI_NUMER, "의류");
+
+
+        //when - then
+        assertThatThrownBy(() -> categoryService.addCategory(categoryRequest))
+                .isInstanceOf(CustomException.class);
+
+    }
+}
