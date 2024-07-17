5d5821e32cde7f69a9ceec32116ff05f8ffdde9e
sim-mer <gyu2781@gmail.com>
2024-07-18 08:38:56 +0900

    refactor(product): Category, CategoryDto 1:1 정적 팩토리 메서드 추가

--- src/main/java/gift/product/business/dto/CategoryDto.java
+++ src/main/java/gift/product/business/dto/CategoryDto.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public record CategoryDto(Long id, String name) {
 
+    public static CategoryDto from(Category category) {
+        return new CategoryDto(category.getId(), category.getName());
+    }
+
     public static List<CategoryDto> of(List<Category> categories) {
         return categories.stream()
-            .map(category -> new CategoryDto(category.getId(), category.getName()))
+            .map(CategoryDto::from)
             .toList();
     }
 }
