fbe96417169b67da32155e0c58f54428c7a4e2c0
test1 <test>
2024-07-17 18:53:57 +0900

    feat : getOptionById 기능 생성

--- src/main/java/gift/Controller/OptionController.java
+++ src/main/java/gift/Controller/OptionController.java
@@ -2,7 +2,10 @@ package gift.Controller;
 
 import gift.DTO.OptionDto;
 import gift.Service.OptionService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,19 @@ public class OptionController {
       .buildAndExpand(addedOptionDto).toUri();
 
     return ResponseEntity.created(location).body(addedOptionDto);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<OptionDto>> getAllOptions(){
+    List<OptionDto> optionDtos = optionService.getAllOptions();
+
+    return ResponseEntity.ok(optionDtos);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<OptionDto> getOptionById(@PathVariable Long id){
+    OptionDto optionDto = optionService.getOptionById(id);
+
+    return ResponseEntity.ok(optionDto);
   }
 }
--- src/main/java/gift/Service/OptionService.java
+++ src/main/java/gift/Service/OptionService.java
@@ -4,20 +4,36 @@ import gift.ConverterToDto;
 import gift.DTO.Option;
 import gift.DTO.OptionDto;
 import gift.Repository.OptionRepository;
+import java.util.List;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OptionService {
+
   private final OptionRepository optionRepository;
 
-  public OptionService(OptionRepository optionRepository){
-    this.optionRepository=optionRepository;
+  public OptionService(OptionRepository optionRepository) {
+    this.optionRepository = optionRepository;
   }
 
   public OptionDto addOption(OptionDto optionDto) {
-    Option option = new Option(optionDto.getName(),optionDto.getQuantity());
+    Option option = new Option(optionDto.getName(), optionDto.getQuantity());
     Option addedOption = optionRepository.save(option);
 
     return ConverterToDto.convertToOptionDto(addedOption);
+  }
+
+  public List<OptionDto> getAllOptions() {
+    List<Option> options = optionRepository.findAll();
+    List<OptionDto> optionDtos = options.stream().map(ConverterToDto::convertToOptionDto).toList();
+
+    return optionDtos;
+  }
+
+  public OptionDto getOptionById(Long id) {
+    Option option = optionRepository.findById(id).orElseThrow(()->new EmptyResultDataAccessException("해당 데이터가 없습니다",1));
+
+    return ConverterToDto.convertToOptionDto(option);
   }
 }
