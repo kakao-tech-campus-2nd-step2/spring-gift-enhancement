c6abb48d612dba45aab0c44a1fcfd7e83caf1513
ez23re <ez23re@gmail.com>
2024-07-17 14:16:17 +0900

    3주차 클론

---
+++ src/main/java/gift/product/dto/ProductDto.java
@@ -0,0 +1,54 @@
+package gift.product.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.URL;
+
+public class ProductDto {
+    private Long product_id;
+
+    private String name;
+
+    @Positive(message = "올바른 가격을 입력하세요.")
+    private int price;
+
+    @NotBlank(message = "이미지 URL을 입력하세요.")
+    @URL(message = "잘못된 URL 입니다.")
+    private String imgUrl;
+
+    public ProductDto() {}
+
+    public ProductDto(Long product_id, String name, int price, String imgUrl) {
+        this.product_id = product_id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public void product_id() {
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void name(String name) {
+        this.name = name;
+    }
+
+    public int price() {
+        return price;
+    }
+
+    public void price(int price) {
+        this.price = price;
+    }
+
+    public String imgUrl() {
+        return imgUrl;
+    }
+
+    public void imgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
