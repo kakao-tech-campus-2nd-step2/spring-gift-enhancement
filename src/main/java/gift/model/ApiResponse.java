a5a3ffae2c019bfb55a06df8b61d10057bf96095
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-17 09:11:35 +0900

    부산대 BE_김상해 4주차 과제(0단계) (#43)
    * feat: 3주차 파일 불러오기
    
    - git merge로 하는 방법?
    - git merge로 해보았으나 git merge conflicts 발생
    - 동일명 파일 있으면 에러 발생
    
    * chore: lombok 추가
    
    * feat: ApiReponse 클래스 추가
    
    * feat: HttpResult 클래스 추가
    
    * refactor: 멤버 컨트롤러 리턴 타입 수정
    
    - ResponseEntity<Map<String, Object>> 에서 ResponseEntity<ApiResponse>로 수정
    
    * chore: 파일 포매팅

---
+++ src/main/java/gift/model/ApiResponse.java
@@ -0,0 +1,17 @@
+package gift.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@Getter
+public class ApiResponse {
+
+    private HttpResult result;
+    private String message;
+    private HttpStatus httpStatus;
+
+}
