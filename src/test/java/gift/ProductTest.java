bb5b4dc183c75cc7cfb4c6acdef6dcc6025f3254
Awhn <ahnshwan@outlook.com>
2024-07-17 22:01:04 +0900

    fix : delete unused import statement

--- src/test/java/gift/ProductTest.java
+++ src/test/java/gift/ProductTest.java
@@ -4,7 +4,6 @@ import gift.entity.Category;
 import gift.entity.Product;
 import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
