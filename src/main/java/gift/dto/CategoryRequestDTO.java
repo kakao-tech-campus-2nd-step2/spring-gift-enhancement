70c6814f067e8f33ec0e4439d8278316d9e63102
ajy4304 <ajy4304@naver.com>
2024-07-17 15:22:32 +0900

    feat(CategoryRequestDTO, CategoryResponseDTO): DTO 생성

---
+++ src/main/java/gift/dto/CategoryRequestDTO.java
@@ -0,0 +1,13 @@
+package gift.dto;
+
+public class CategoryRequestDTO {
+    private String name;
+
+    public CategoryRequestDTO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
