d1b4dfb4ff06e82246c47f392f856e82fa0c97f5
Dalsungmin <cnww123@naver.com>
2024-07-17 10:00:25 +0900

    Refactor: 카테고리를 포함한 상품 조회
    카테고리를 포함한 상품을 생성해야 하므로 생성자로 카테고리를 생성,
    이때 생성된 ID를 통해 완전한 category를 category1에 저장한뒤 product 생성해 진행

--- src/test/java/gift/Product/ProductRepositoryTest.java
+++ src/test/java/gift/Product/ProductRepositoryTest.java
@@ -2,21 +2,48 @@ package gift.Product;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import gift.initializer.CategoryInitializer;
+import gift.model.Category;
 import gift.model.Product;
+import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
+import gift.service.CategoryService;
 import java.util.Optional;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+    // this.categoryRepository = categoryRepository;
+
+
+    private CategoryInitializer categoryInitializer;
+
+    private CategoryService categoryService;
+
+    @BeforeEach
+    public void set() {
+        this.categoryService = new CategoryService(categoryRepository);
+        this.categoryInitializer = new CategoryInitializer(categoryRepository);
+    }
+
+
     @Test
     void testFindById() {
-        Product expected = new Product("카푸치노", 3000, "example.com");
+        Category category = new Category("교환권");
+        categoryRepository.save(category);
+        Category category1 = categoryService.getCategoryById(1L);
+        Product expected = new Product("카푸치노", 3000, "example.com",category1);
         productRepository.save(expected);
         Optional<Product> actual = productRepository.findById(expected.getId());
         assertThat(actual.isPresent()).isTrue();
