4ed56e2c8803eee845ba64f5bc8de93a994432a4
Dalsungmin <cnww123@naver.com>
2024-07-17 09:18:52 +0900

    Feat: 카테고리 추가 서비스 메서드
    카테고리를 추가할때 컨트롤러로부터 카테고리 자료형 자체로 받아 레포지토리로 전달한다.

---
+++ src/main/java/gift/service/CategoryService.java
@@ -0,0 +1,23 @@
+package gift.service;
+
+
+import gift.exception.ProductNotFoundException;
+import gift.model.Category;
+import gift.repository.CategoryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+}
