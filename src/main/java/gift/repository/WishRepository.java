2b90b0a2c225bd6b2d06ff02c081d3849203d23f
rdme0 <we6610@gmail.com>
2024-07-17 20:30:45 +0900

    :recycle: refactor: 메서드 이름 리팩터링

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -11,6 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WishRepository extends JpaRepository<Wish, Long> {
     Optional<Wish> findByMemberAndProduct(Member member, Product product);
     void deleteByMemberAndProductId(Member member, Long productId);
-    void deleteByProductId(Long productId);
+    void deleteAllByProductId(Long productId);
     Page<Wish> findByMember(Member member, Pageable pageable);
 }
