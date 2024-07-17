d084704a39f93243502148acaab266672f68bc95
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-17 14:57:12 +0900

    Refactor: category name 유니크 추가

--- src/main/java/gift/model/Category.java
+++ src/main/java/gift/model/Category.java
@@ -7,7 +7,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
     @Column(nullable = false)
     private String color;
