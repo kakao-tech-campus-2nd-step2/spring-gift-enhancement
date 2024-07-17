eec3a9b7b633727141e01e5ea4080be7dd1a6451
dong_yxxn <g0326kdy@gmail.com>
2024-07-17 14:59:35 +0900

    feat(ProductDTO): 설계

--- src/main/java/gift/product/domain/ProductDTO.java
+++ src/main/java/gift/product/domain/ProductDTO.java
@@ -1,2 +1,60 @@
-package gift.product.domain;public class ProductDTO {
+package gift.product.domain;
+
+public class ProductDTO {
+    Long id;
+    String name;
+    Long price;
+    String imageUrl;
+    Long categoryId;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, Long price, String imageUrl, Long categoryId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.categoryId = categoryId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
 }
