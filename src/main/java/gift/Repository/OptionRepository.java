414d5972820073b70f6d9c08eeed8025bdd9a9e6
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-18 01:37:37 +0900

    feat : OptionRepository클래스 추가

--- src/main/java/gift/Repository/OptionRepository.java
+++ src/main/java/gift/Repository/OptionRepository.java
@@ -1,12 +1,14 @@
 package gift.Repository;
 
-import gift.Model.Entity.CategoryEntity;
+import gift.Model.Entity.OptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
-    Optional<CategoryEntity> findByName(String name);
+public interface OptionRepository extends JpaRepository<OptionEntity, Long> {
+    Optional<OptionEntity> findByName(String name);
+    List<OptionEntity> findByProductId(Long productId);
 }
