b4e205c0634c45b3cf1429aa72a40fdde1c9a00e
ez23re <ez23re@gmail.com>
2024-07-18 15:31:42 +0900

    feat : 테스트 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,13 @@
 -- 카테고리 데이터 삽입
-INSERT INTO category (name) VALUES ('Electronics'), ('Books');
+INSERT INTO category (categoryId, name, color, imgUrl) VALUES
+(1, 'Clothes', 'black', 'https://www.google.com'),
+(2, 'Foods', 'white', 'https://www.google.com');
 
 -- 상품 데이터 삽입
 INSERT INTO products (name, price, imgUrl, categoryId) VALUES
-('Laptop', 1000, 'https://www.google.com', 1),
-('Novel', 20, 'https://www.google.com', 2);
+('Product1', 35000, 'https://www.google.com', 1),
+('Product2', 28000, 'https://www.google.com', 2);
 
 -- 회원 데이터 삽입
 INSERT INTO members (email, password) VALUES
-('user1@example.com', 'password1'),
-('user2@example.com', 'password2');
-
--- 위시리스트 데이터 삽입
-INSERT INTO wishLists (memberId, productId) VALUES
-(1, 1),
-(2, 2);
+('test@example.com', 'password');
