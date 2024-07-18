e743b21b8b4a6eb62b35deaad3534824b7b8c277
안원모 <tommy45231@naver.com>
2024-07-18 16:26:09 +0900

    Refacotr : 관리자 화면에 카테고리 반영

--- src/main/java/gift/controller/ProductWebController.java
+++ src/main/java/gift/controller/ProductWebController.java
@@ -1,6 +1,9 @@
 package gift.controller;
 
+import gift.domain.Category;
+import gift.domain.CategoryName;
 import gift.domain.Product;
+import gift.service.CategoryService;
 import gift.service.ProductService;
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,26 +19,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/web/products")
 public class ProductWebController {
     private final ProductService productService;
+    private final CategoryService categoryService;
 
-    public ProductWebController(ProductService productService) {
+    public ProductWebController(ProductService productService, CategoryService categoryService) {
         this.productService = productService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping
     public String getProductsPage(Model model) {
         List<Product> products = productService.findAllProducts();
+        List<Category> categories = categoryService.findAllCategories();
         model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
         return "products";
     }
 
     @PostMapping(consumes = "application/x-www-form-urlencoded;charset=UTF-8")
     public String postProduct(@RequestParam String name, @RequestParam BigDecimal price,
-        @RequestParam String imageUrl, @RequestParam String description) {
+        @RequestParam String imageUrl, @RequestParam String description,
+        @RequestParam CategoryName categoryName) {
+        Category category = categoryService.getCategoryByName(categoryName);
         Product product = new Product.ProductBuilder()
             .name(name)
             .price(price)
             .imageUrl(imageUrl)
             .description(description)
+            .category(category)
             .build();
         productService.createProduct(product);
         return "redirect:/web/products";
@@ -52,18 +62,23 @@ public class ProductWebController {
     @GetMapping("/edit/{id}")
     public String getEditForm(@PathVariable Long id, Model model) {
         Product product = productService.getProductById(id);
+        List<Category> categories = categoryService.findAllCategories();
         model.addAttribute("product", product);
+        model.addAttribute("categories", categories);
         return "productEdit";
     }
 
     @PostMapping(value = "/edit/{id}", consumes = "application/x-www-form-urlencoded;charset=UTF-8")
     public String editProduct(@PathVariable Long id, @RequestParam String name,
-        @RequestParam BigDecimal price, @RequestParam String imageUrl) {
+        @RequestParam BigDecimal price, @RequestParam String imageUrl,
+        @RequestParam CategoryName categoryName) {
+        Category category = categoryService.getCategoryByName(categoryName);
         Product product = new Product.ProductBuilder()
             .id(id)
             .name(name)
             .price(price)
             .imageUrl(imageUrl)
+            .category(category)
             .build();
         productService.updateProduct(id, product);
         return "redirect:/web/products";
