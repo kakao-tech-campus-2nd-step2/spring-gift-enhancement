3b7a32ac070ce5820ebca4d7db22f171ebd31a56
momnpa333 <momnpa333@gmail.com>
2024-07-18 18:27:24 +0900

    feat: option-product 양방향 매핑

--- src/main/java/gift/model/product/Product.java
+++ src/main/java/gift/model/product/Product.java
@@ -2,6 +2,7 @@ package gift.model.product;
 
 import gift.global.validate.ProductNameValidator;
 import gift.model.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +10,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 public class Product extends BaseTimeEntity {
@@ -31,6 +34,9 @@ public class Product extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Option> options;
 
     protected Product() {
     }
