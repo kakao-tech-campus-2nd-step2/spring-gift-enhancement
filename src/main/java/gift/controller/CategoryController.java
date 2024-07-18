c0c3d48e6dae5cb5c14cd54839ffdd968d0fee04
안원모 <tommy45231@naver.com>
2024-07-18 16:01:14 +0900

    Feat : CategoryController 추가

---
+++ src/main/java/gift/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package gift.controller;
+
+import gift.domain.Category;
+import gift.service.CategoryService;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getCategories() {
+        return categoryService.findAllCategories();
+    }
+}
