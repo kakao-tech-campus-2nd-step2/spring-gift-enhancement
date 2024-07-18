bcfe11585efae279be61ce4431da175a92003cf2
ajy4304 <ajy4304@naver.com>
2024-07-18 15:28:04 +0900

    refactor(ProductService): 엔티티 대신 DTO 반환으로 변경

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -22,28 +22,33 @@ public class ProductService {
     @Autowired
     private CategoryService categoryService;
 
+    // 모든 상품 조회
     public Page<ProductResponseDTO> getAllProducts(int page, int size) {
         Page<Product> products = productRepository.findAll(PageRequest.of(page, size));
-        return products.map(ProductResponseDTO::new);
+        return products.map(ProductResponseDTO::fromEntity);
     }
 
+    // 특정 ID의 상품 조회
     public Optional<ProductResponseDTO> getProductById(Long id) {
         Optional<Product> productOpt = productRepository.findById(id);
-        return productOpt.map(ProductResponseDTO::new);
+        return productOpt.map(ProductResponseDTO::fromEntity);
     }
 
+    // 상품 엔티티 조회
     public Product findProductEntityById(Long id) {
         return productRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("상품 정보를 찾을 수 없습니다."));
     }
 
+    // 상품 생성
     public Page<ProductResponseDTO> createProduct(ProductRequestDTO productRequestDTO, int page, int size) {
         Category category = categoryService.findCategoryEntityById(productRequestDTO.getCategoryId());
-        Product product = new Product(productRequestDTO.getName(), productRequestDTO.getPrice(), productRequestDTO.getImageUrl(), category);
+        Product product = productRequestDTO.toEntity(category);
         productRepository.save(product);
         return getAllProducts(page, size);
     }
 
+    // 상품 업데이트
     public Optional<ProductResponseDTO> updateProduct(Long id, ProductRequestDTO productRequest) {
         Optional<Product> existingProductOpt = productRepository.findById(id);
         if (existingProductOpt.isPresent()) {
@@ -54,11 +59,12 @@ public class ProductService {
             existingProduct.setPrice(productRequest.getPrice());
             existingProduct.setCategory(category);
             productRepository.save(existingProduct);
-            return Optional.of(new ProductResponseDTO(existingProduct));
+            return Optional.of(ProductResponseDTO.fromEntity(existingProduct));
         }
         return Optional.empty();
     }
 
+    // 상품 삭제
     public boolean deleteProduct(Long id) {
         if (productRepository.findById(id).isPresent()) {
             productRepository.deleteById(id);
