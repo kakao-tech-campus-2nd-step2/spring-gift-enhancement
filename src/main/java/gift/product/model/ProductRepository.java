4da8f998600523d7799961e301a25b4937b174b1
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-19 01:24:32 +0900

    feat: productRepository 수정 - product 조회시 option도 함께 조회

--- src/main/java/gift/product/model/ProductRepository.java
+++ src/main/java/gift/product/model/ProductRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Tuple;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,16 +13,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = {"options"})
     Optional<Product> findByIdAndIsActiveTrue(Long id);
 
-    // 특정 ID의 Product와 관련 Wish 개수 조회
-    @Query("SELECT p AS product, COUNT(w) AS wishCount FROM Product p LEFT JOIN Wish w ON w.product = p WHERE p.id = :id AND p.isActive = true GROUP BY p")
+    @Query("SELECT p AS product, COUNT(w) AS wishCount FROM Product p LEFT JOIN Wish w ON w.product = p LEFT JOIN FETCH p.options WHERE p.id = :id AND p.isActive = true GROUP BY p")
     Optional<Tuple> findProductByIdWithWishCount(@Param("id") Long id);
 
-    @Query("SELECT p AS product, COUNT(w) AS wishCount FROM Product p LEFT JOIN Wish w ON w.product = p WHERE p.isActive = true GROUP BY p")
+    @Query("SELECT p AS product, COUNT(w) AS wishCount FROM Product p LEFT JOIN Wish w ON w.product = p LEFT JOIN FETCH p.options WHERE p.isActive = true GROUP BY p")
     Page<Tuple> findAllActiveProductsWithWishCountPageable(Pageable pageable);
 
-    @Query("SELECT p AS product, COUNT(w) AS wishCount FROM Product p LEFT JOIN Wish w ON w.product = p WHERE p.category.id = :categoryId AND p.isActive = true GROUP BY p")
+    @Query("SELECT p AS product, COUNT(w) AS wishCount FROM Product p LEFT JOIN Wish w ON w.product = p LEFT JOIN FETCH p.options WHERE p.category.id = :categoryId AND p.isActive = true GROUP BY p")
     Page<Tuple> findActiveProductsByCategoryWithWishCount(Long categoryId, Pageable pageable);
-
 }
