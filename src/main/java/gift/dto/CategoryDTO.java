b4a810f3ef25631da7350a5c0ee9db76f005f281
donghyeon <thornappl2@gmail.com>
2024-07-18 22:25:02 +0900

    feat: Category 엔티티, DTO 수정

--- src/main/java/gift/dto/CategoryDTO.java
+++ src/main/java/gift/dto/CategoryDTO.java
@@ -1,27 +1,65 @@
 package gift.dto;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
 
 public class CategoryDTO {
 
     private Long id;
 
     @NotBlank(message = "카테고리 이름은 필수입니다")
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    @NotNull
+    @URL(message = "유효한 URL 형식이 아닙니다")
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    private String description;
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- src/main/java/gift/model/Category.java
+++ src/main/java/gift/model/Category.java
@@ -2,6 +2,8 @@ package gift.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Table
@@ -15,10 +17,24 @@ public class Category {
     @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private String color;
+
+    @NotNull
+    @URL(message = "유효한 URL 형식이 아닙니다")
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    private String description;
+
     protected Category() {}
 
-    public Category(String name) {
+    public Category(Long id, String name, String color, String imageUrl, String description) {
+        this.id = id;
         this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
     }
 
     public Long getId() {
@@ -29,4 +45,15 @@ public class Category {
         return name;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
