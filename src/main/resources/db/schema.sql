fb68bf142b78e9ae873119e301732057fac64e4a
wvssm <neoramiregi@gmail.com>
2024-07-18 17:31:23 +0900

    feat(schema.sql): WISHES, OPTIONS, PRODUCTS, CATEGORIES, MEMBERS 테이블 생성

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,24 +1,62 @@
-DROP TABLE IF EXISTS products;
-CREATE TABLE products (
-                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          name VARCHAR(255),
-                          price INT,
-                          imageUrl VARCHAR(255)
+DROP TABLE IF EXISTS WISHES;
+DROP TABLE IF EXISTS OPTIONS;
+DROP TABLE IF EXISTS PRODUCTS;
+DROP TABLE IF EXISTS CATEGORIES;
+DROP TABLE IF EXISTS MEMBERS;
+
+-- Create table for categories
+CREATE TABLE CATEGORIES (
+                            ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            NAME VARCHAR(255) NOT NULL UNIQUE,
+                            COLOR VARCHAR(255) NOT NULL,
+                            IMAGE_URL VARCHAR(255) NOT NULL,
+                            DESCRIPTION VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS members;
-CREATE TABLE members (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       email VARCHAR(255),
-                       password VARCHAR(255)
+-- Create table for products
+CREATE TABLE PRODUCTS (
+                          ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          NAME VARCHAR(15) NOT NULL,
+                          PRICE INT NOT NULL,
+                          IMAGE_URL VARCHAR(255) NOT NULL,
+                          CATEGORY_ID BIGINT,
+                          CONSTRAINT FK_CATEGORY
+                              FOREIGN KEY (CATEGORY_ID)
+                                  REFERENCES CATEGORIES (ID)
+                                  ON DELETE SET NULL
 );
 
-DROP TABLE IF EXISTS wishes;
-CREATE TABLE wishes (
-                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        member_id BIGINT NOT NULL,
-                        product_id BIGINT NOT NULL,
-                        quantity INT NOT NULL,
-                        FOREIGN KEY (member_id) REFERENCES members(id),
-                        FOREIGN KEY (product_id) REFERENCES products(id)
+-- Create table for options
+CREATE TABLE OPTIONS (
+                         ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         NAME VARCHAR(255) NOT NULL UNIQUE,
+                         QUANTITY INT,
+                         PRODUCT_ID BIGINT,
+                         CONSTRAINT FK_PRODUCT
+                             FOREIGN KEY (PRODUCT_ID)
+                                 REFERENCES PRODUCTS (ID)
+                                 ON DELETE SET NULL
+);
+
+-- Create table for members
+CREATE TABLE MEMBERS (
+                         ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         EMAIL VARCHAR(255) NOT NULL UNIQUE,
+                         PASSWORD VARCHAR(255) NOT NULL
+);
+
+-- Create table for wishes
+CREATE TABLE WISHES (
+                        ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        MEMBER_ID BIGINT,
+                        PRODUCT_ID BIGINT,
+                        QUANTITY INT NOT NULL,
+                        CONSTRAINT FK_MEMBER
+                            FOREIGN KEY (MEMBER_ID)
+                                REFERENCES MEMBERS (ID)
+                                ON DELETE CASCADE,
+                        CONSTRAINT FK_PRODUCT_WISH
+                            FOREIGN KEY (PRODUCT_ID)
+                                REFERENCES PRODUCTS (ID)
+                                ON DELETE CASCADE
 );
