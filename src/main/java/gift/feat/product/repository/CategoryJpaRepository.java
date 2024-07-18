53c665b93fac4f7e3a8657061ad22cb6a5cc16c2
yooonwodyd <dbswodyd00@naver.com>
2024-07-18 19:00:37 +0900

    [feat] 카테고리 엔티티 및 관련 레포지토리 추가

---
+++ src/main/java/gift/feat/product/repository/CategoryJpaRepository.java
@@ -0,0 +1,13 @@
+package gift.feat.product.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gift.feat.product.domain.Category;
+
+@Repository
+public interface CategoryJpaRepository extends JpaRepository<Category, Long> {
+	Optional<Category> findByName(String name);
+}
