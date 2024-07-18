06bdf8593f98d3ffcfaa8175ec1b59570ed14e55
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-18 12:45:00 +0900

    feat: 옵션 서비스에 옵션엔티티전환, 옵션이름중복체크 메서드 추가

--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -2,6 +2,7 @@ package gift.service;
 
 import gift.dto.request.OptionRequest;
 import gift.entity.Option;
+import gift.exception.OptionDuplicateException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +10,22 @@ import java.util.List;
 @Service
 public class OptionService {
 
-    public List<Option> getOptions(List<OptionRequest> options) {
+    public List<Option> convertToOptions(List<OptionRequest> options) {
         return options.stream()
                 .map(optionRequest -> new Option(optionRequest.name(), optionRequest.quantity()))
                 .toList();
+    }
+
+    public Option convertToOption(OptionRequest request) {
+        return new Option(request.name(), request.quantity());
+    }
+
+    public void checkDuplicateOptionName(List<Option> existingOptions, String newOptionName) {
+        boolean isDuplicate = existingOptions.stream()
+                .anyMatch(option -> option.getName().equals(newOptionName));
+
+        if (isDuplicate) {
+            throw new OptionDuplicateException(newOptionName);
+        }
     }
 }
