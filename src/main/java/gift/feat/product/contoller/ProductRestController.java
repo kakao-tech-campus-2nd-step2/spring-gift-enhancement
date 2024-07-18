4e7f129f5e62789fd9f41c26f9cad1b3c296a93a
yooonwodyd <dbswodyd00@naver.com>
2024-07-18 19:03:34 +0900

    [refactor] 불필요한 주석 제거
    (cherry picked from commit a04358483349aea01853d8d932ce8e60365eb4a3)

--- src/main/java/gift/feat/product/contoller/ProductRestController.java
+++ src/main/java/gift/feat/product/contoller/ProductRestController.java
@@ -1,7 +1,6 @@
 package gift.feat.product.contoller;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import gift.feat.product.domain.Product;
 import gift.feat.product.domain.SearchType;
-import gift.feat.product.contoller.dto.ProductRequestDto;
+import gift.feat.product.contoller.dto.ProductCreateRequest;
 import gift.feat.product.contoller.dto.ProductResponseDto;
 import gift.feat.product.service.ProductService;
 import jakarta.validation.Valid;
@@ -22,12 +21,10 @@ public class ProductRestController {
 
 	private final ProductService productService;
 
-
 	@PostMapping("/api/v1/product")
 	@ResponseBody
-	public ResponseEntity<Long> registerProduct(@RequestBody @Valid ProductRequestDto productRequestDto) {
-		// 등록된 상품의 ID를 반환.
-		return ResponseEntity.ok(productService.saveProduct(productRequestDto));
+	public ResponseEntity<Long> registerProduct(@RequestBody @Valid ProductCreateRequest productCreateRequest) {
+		return ResponseEntity.ok(productService.saveProduct(productCreateRequest));
 	}
 
 	// 모든 상품을 페이징해서 조회
@@ -41,9 +38,6 @@ public class ProductRestController {
 		return ResponseEntity.ok(productService.getProductsWithPaging(pageable, searchType, searchValue).map(ProductResponseDto::from));
 	}
 
-
-
-
 	@GetMapping("/api/v1/product/{id}")
 	@ResponseBody
 	public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long id) {
@@ -53,10 +47,8 @@ public class ProductRestController {
 
 	@PutMapping("/api/v1/product/{id}")
 	@ResponseBody
-	public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductRequestDto productRequestDto) {
-
-		// 수정된 상품 ID를 같이 반환한다.
-		return ResponseEntity.ok(productService.updateProduct(id, productRequestDto));
+	public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductCreateRequest productCreateRequest) {
+		return ResponseEntity.ok(productService.updateProduct(id, productCreateRequest));
 	}
 
 	@DeleteMapping("/api/v1/product/{id}")
