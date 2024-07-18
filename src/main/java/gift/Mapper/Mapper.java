0306f4770b2171e5f0f9186d2a12b7fc601f7d56
BOMIN LYU <rbm0524@naver.com>
2024-07-18 16:41:25 +0900

    feat : Category 관련 매핑 추가

--- src/main/java/gift/Mapper/Mapper.java
+++ src/main/java/gift/Mapper/Mapper.java
@@ -1,12 +1,11 @@
 package gift.Mapper;
 
-import gift.Entity.Member;
-import gift.Entity.Product;
-import gift.Entity.Wishlist;
-import gift.Entity.WishlistId;
+import gift.Entity.*;
+import gift.Model.CategoryDto;
 import gift.Model.MemberDto;
 import gift.Model.ProductDto;
 import gift.Model.WishlistDto;
+import gift.Service.CategoryService;
 import gift.Service.ProductService;
 import gift.Service.MemberService;
 import gift.Service.WishlistService;
@@ -22,12 +21,14 @@ public class Mapper {
     private final ProductService productService;
     private final MemberService memberService;
     private final WishlistService wishlistService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public Mapper(@Lazy ProductService productService, @Lazy MemberService memberService, @Lazy WishlistService wishListService) {
+    public Mapper(@Lazy ProductService productService, @Lazy MemberService memberService, @Lazy WishlistService wishListService, @Lazy CategoryService categoryService) {
         this.productService = productService;
         this.memberService = memberService;
         this.wishlistService = wishListService;
+        this.categoryService = categoryService;
     }
 
 
@@ -37,6 +38,7 @@ public class Mapper {
 
         Optional<ProductDto> productDtoOptional = productService.getProductById(wishlistDto.getProductId());
         ProductDto productDto = productDtoOptional.get();
+        productDto.setCategoryId(productDtoOptional.get().getCategoryId());
 
         WishlistId id = new WishlistId(memberDto.getId(), productDto.getId());
         return new Wishlist(id, memberDtoToEntity(memberDto), productDtoToEntity(productDto), wishlistDto.getProductName(), wishlistDto.getCount(), wishlistDto.getPrice());
@@ -47,12 +49,25 @@ public class Mapper {
         return new WishlistDto(wishlist.getUserId(), wishlist.getProductId(), wishlist.getCount(), 0, wishlist.getProductName(), wishlist.getPrice());
     }
 
+    public Category categoryDtoToEntity(CategoryDto categoryDto) {
+        return new Category(categoryDto.getId(), categoryDto.getName());
+    }
+
+    public CategoryDto categoryToDto(Category category) {
+        return new CategoryDto(category.getCategoryId(), category.getName());
+    }
+
+    //categoryService를 포함하기엔 뭔가 일관성이 깨지는 느낌인데???
     public Product productDtoToEntity(ProductDto productDto) {
-        return new Product(productDto.getId(), productDto.getName(), productDto.getPrice(), productDto.getImageUrl(), productDto.isDeleted());
+        Category category = categoryService.getCategoryById(productDto.getCategoryId()).get();
+        CategoryDto categoryDto = categoryToDto(category);
+        return new Product(productDto.getId(), productDto.getName(), categoryDtoToEntity(categoryDto), productDto.getPrice(), productDto.getImageUrl(), productDto.isDeleted());
     }
 
     public ProductDto productToDto(Product product) {
-        return new ProductDto(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), product.isDeleted());
+        Category category = categoryService.getCategoryById(product.getCategory().getCategoryId()).get();
+
+        return new ProductDto(product.getId(), product.getName(), categoryToDto(category).getId(), product.getPrice(), product.getImageUrl(), product.isDeleted());
     }
 
     public Member memberDtoToEntity(MemberDto memberDto) {
@@ -62,4 +77,5 @@ public class Mapper {
     public MemberDto memberToDto(Member member) {
         return new MemberDto(member.getId(), member.getEmail(), member.getName(), member.getPassword(), member.isAdmin());
     }
+
 }
