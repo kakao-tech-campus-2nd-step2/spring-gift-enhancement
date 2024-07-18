60670260ed9e0a4781370d15b72749f065f424d3
wvssm <neoramiregi@gmail.com>
2024-07-18 16:05:18 +0900

    refactor(MemberTest): Product 생성 시 category 파라미터 추가

--- src/test/java/gift/domain/MemberTest.java
+++ src/test/java/gift/domain/MemberTest.java
@@ -13,9 +13,11 @@ class MemberTest {
     private Product product;
     private Member member;
     private Wish wish;
+    private Category category;
     @BeforeEach
     void setUp(){
-        product = new Product("상품1",1000,"http://product1");
+        category = new Category("교환권","#FF5733","https://example.com/images/exchange_voucher.jpg","다양한 상품으로 교환 가능한 교환권");
+        product = new Product("상품1", 1000, "http://product1", category);
         member = new Member("a@a.com","1234");
         wish = new Wish(member,product,1);
     }
@@ -24,7 +26,6 @@ class MemberTest {
     @DisplayName("멤버의 위시리스트에 위시 추가 테스트")
     void addWish() {
         // when
-        member.addWish(wish);
 
         // then
         List<Wish> wishes = member.getWishes();
