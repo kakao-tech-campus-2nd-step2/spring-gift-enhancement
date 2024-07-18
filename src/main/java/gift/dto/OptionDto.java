54c34b0b6004ac7a973fa6f1991ffa1b747c0837
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-18 19:59:26 +0900

    feat: Option name 유효성 검사

--- src/main/java/gift/dto/OptionDto.java
+++ src/main/java/gift/dto/OptionDto.java
@@ -1,13 +1,18 @@
 package gift.dto;
 
+import gift.validation.ValidName;
 import gift.vo.Option;
 import gift.vo.Product;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record OptionDto (
     Long id,
 
     Product product,
 
+    @ValidName
+    @NotEmpty(message = "옵션명을 입력해 주세요.")
     String name,
 
     int quantity
