eefc8104bda8f209408d83585cc09f3a2c7b5402
momnpa333 <momnpa333@gmail.com>
2024-07-18 21:00:22 +0900

    refact: 안쓰는 import 문 삭제

--- src/main/java/gift/model/product/Category.java
+++ src/main/java/gift/model/product/Category.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
@@ -65,4 +64,6 @@ public class Category extends BaseTimeEntity {
         this.description = description;
         this.imageUrl = imageUrl;
     }
+
 }
+
