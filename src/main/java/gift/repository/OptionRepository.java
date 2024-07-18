898b478f0ce903703cd5436e4761dab845cc38cd
mingjuu <tisu79@naver.com>
2024-07-18 13:30:07 +0900

    faet : 옵션 레포 생성

--- src/main/java/gift/repository/OptionRepository.java
+++ src/main/java/gift/repository/OptionRepository.java
@@ -1,2 +1,11 @@
-package gift.repository;public class OptionRepository {
+package gift.repository;
+
+import gift.domain.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+    List<Option> findByProductId(Long productId);
+    boolean existsByProductIdAndName(Long productId, String name);
 }
