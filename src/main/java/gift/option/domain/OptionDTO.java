49b4d0df469e0ab51176b9536601c6fe402d7c9c
dong_yxxn <g0326kdy@gmail.com>
2024-07-18 14:43:35 +0900

    refactor(OptionDTO): 생성자 추가

--- src/main/java/gift/option/domain/OptionDTO.java
+++ src/main/java/gift/option/domain/OptionDTO.java
@@ -6,4 +6,48 @@ public class OptionDTO {
     private Long quantity;
     private Long productId;
 
+
+    // constructor
+    public OptionDTO() {
+    }
+
+    public OptionDTO(Long id, String name, Long quantity, Long productId) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.productId = productId;
+    }
+
+    //getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
 }
