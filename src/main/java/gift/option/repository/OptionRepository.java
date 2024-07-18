2c686681908313dcf3339648e2f55275453163d6
dong_yxxn <g0326kdy@gmail.com>
2024-07-18 14:26:02 +0900

    refactor(OptionRepo): Repository 어노테이션 추가

--- src/main/java/gift/option/repository/OptionRepository.java
+++ src/main/java/gift/option/repository/OptionRepository.java
@@ -2,7 +2,9 @@ package gift.option.repository;
 
 import gift.option.domain.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface OptionRepository extends JpaRepository<Option, Long> {
 
 }
