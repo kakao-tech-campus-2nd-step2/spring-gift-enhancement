66419d211518890237df758c680b20cc66e6ec67
dong_yxxn <g0326kdy@gmail.com>
2024-07-18 14:54:05 +0900

    feat(OptionService): find, save 기능 구현

--- src/main/java/gift/option/service/OptionService.java
+++ src/main/java/gift/option/service/OptionService.java
@@ -1,2 +1,54 @@
-package gift.option.service;public class OptionService {
+package gift.option.service;
+
+import gift.option.domain.Option;
+import gift.option.domain.OptionDTO;
+import gift.option.repository.OptionRepository;
+import gift.product.domain.Product;
+import gift.product.repository.ProductRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OptionService {
+
+    private final OptionRepository optionRepository;
+    private final ProductRepository productRepository;
+
+    public OptionService(OptionRepository optionRepository, ProductRepository productRepository) {
+        this.optionRepository = optionRepository;
+        this.productRepository = productRepository;
+    }
+
+    public List<OptionDTO> findAll(){
+        return optionRepository.findAll()
+            .stream().map(this::convertToDTO)
+            .toList();
+    }
+
+    public List<OptionDTO> findAllByProductID(Long productId){
+        return optionRepository.findAllByProductId(productId)
+            .stream().map(this::convertToDTO)
+            .toList();
+    }
+
+    public Option save(OptionDTO optionDTO){
+        return optionRepository.save(convertToEntity(optionDTO));
+    }
+
+    private OptionDTO convertToDTO(Option option){
+        return new OptionDTO(
+            option.getId(),
+            option.getName(),
+            option.getQuantity(),
+            option.getProduct().getId()
+        );
+    }
+
+    private Option convertToEntity(OptionDTO optionDTO){
+        Option option = new Option(optionDTO.getId(), optionDTO.getName(), optionDTO.getQuantity());
+
+        option.setProduct(productRepository.findById(optionDTO.getProductId())
+                    .orElseThrow(() -> new IllegalArgumentException("productId" + optionDTO.getProductId() + "가 없다.")));
+        return option;
+    }
 }
