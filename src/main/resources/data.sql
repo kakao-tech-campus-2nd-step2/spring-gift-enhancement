0d5eeef8e18eeaed254e38328dd36ddd244bba57
ez23re <ez23re@gmail.com>
2024-07-18 15:11:03 +0900

    refactor : SQL 다시 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,17 @@
-INSERT INTO products (id, name, price, imgUrl) VALUES
-('1000', 'NIKE', 28500, 'www.google.com'),
-('456', 'nike', 71200, 'www.google.com');
+-- 카테고리 데이터 삽입
+INSERT INTO category (name) VALUES ('Electronics'), ('Books');
+
+-- 상품 데이터 삽입
+INSERT INTO products (name, price, imgUrl, categoryId) VALUES
+('Laptop', 1000, 'https://www.google.com', 1),
+('Novel', 20, 'https://www.google.com', 2);
+
+-- 회원 데이터 삽입
+INSERT INTO members (email, password) VALUES
+('user1@example.com', 'password1'),
+('user2@example.com', 'password2');
+
+-- 위시리스트 데이터 삽입
+INSERT INTO wishLists (memberId, productId) VALUES
+(1, 1),
+(2, 2);
