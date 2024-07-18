104b118c581f368b5acf41829809eec40e157f03
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-19 01:06:32 +0900

    test: 상품 카테고리의 생성 실패 성공 테스트 케이스 추가
    상품 카테고리 생성 실패 성공 테스트 케이스 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -9,7 +9,7 @@ import org.hibernate.validator.constraints.Length;
 public record ProductRequest(
         @Pattern(regexp = "^[\s\\-\\&\\(\\)\\[\\]\\+\\/\\_a-zA-z0-9ㄱ-ㅎ가-힣]*$", message = "허용되지 않은 형식의 이름입니다.")
         @Length(max = 15, message = "이름의 길이는 15자를 초과할 수 없습니다.")
-        @Length(min = 1, message = "이름의 길이는 최소 1자 이상이어야 합니다.")
+        @NotBlank(message = "이름의 길이는 최소 1자 이상이어야 합니다.")
         String name,
         @PositiveOrZero(message = "금액은 0보다 크거나 같아야 합니다.")
         Integer price,
---
+++ src/test/java/gift/service/ProductCategoryServiceTest.java
@@ -0,0 +1,5 @@
+package gift.service;
+
+class ProductCategoryServiceTest {
+
+}
