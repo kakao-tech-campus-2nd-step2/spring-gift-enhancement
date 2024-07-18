d9f15a5dc458886b058a9bc4cc7155522405c7bb
donghyun kim <gimdonghyun5434@gmail.com>
2024-07-18 17:17:16 +0900

    feat : product not found 에러 메세지 생성

--- src/main/java/gift/constants/Messages.java
+++ src/main/java/gift/constants/Messages.java
@@ -3,4 +3,6 @@ package gift.constants;
 public class Messages {
     public static final String NOT_FOUND_MEMBER_BY_EMAIL_MESSAGE = "해당 email을 가진 member가 존재하지 않습니다";
     public static final String NOT_FOUND_WISH_MESSAGE = "해당 wish가 존재하지 않습니다.";
+    public static final String NOT_FOUND_CATEGORY_MESSAGE = "해당 category가 존재하지 않습니다.";
+    public static final String NOT_FOUND_PRODUCT_MESSAGE = "해당 product가 존재하지 않습니다";
 }
