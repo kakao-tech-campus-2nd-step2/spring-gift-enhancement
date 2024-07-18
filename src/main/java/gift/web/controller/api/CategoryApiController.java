7614c41502b07dcbbe2cdb9213e1bb8d86660fd2
나제법 <skwpqjq@gmail.com>
2024-07-18 21:01:13 +0900

    feat: CategoryApiController.java 카테고리 수정, 삭제 API 구현

--- src/main/java/gift/web/controller/api/CategoryApiController.java
+++ src/main/java/gift/web/controller/api/CategoryApiController.java
@@ -2,18 +2,22 @@ package gift.web.controller.api;
 
 import gift.service.CategoryService;
 import gift.web.dto.request.category.CreateCategoryRequest;
+import gift.web.dto.request.category.UpdateCategoryRequest;
 import gift.web.dto.response.category.CreateCategoryResponse;
 import gift.web.dto.response.category.ReadAllCategoriesResponse;
 import gift.web.dto.response.category.ReadCategoryResponse;
+import gift.web.dto.response.category.UpdateCategoryResponse;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +51,18 @@ public class CategoryApiController {
     public ResponseEntity<ReadCategoryResponse> readCategory(@PathVariable Long id) {
         ReadCategoryResponse response = categoryService.readCategory(id);
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UpdateCategoryResponse> updateCategory(@PathVariable Long id, @Validated @RequestBody UpdateCategoryRequest request) {
+        UpdateCategoryResponse response = categoryService.updateCategory(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
