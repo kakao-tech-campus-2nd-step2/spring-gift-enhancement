784c6338b59e91b7b7d92ec7b98e178d223ffc27
yooonwodyd <dbswodyd00@naver.com>
2024-07-18 19:02:50 +0900

    [feat] 반환에 Optional 추가
    (cherry picked from commit c7d92328bb9084854bedde6e3d665bf3839a8516)

--- src/main/java/gift/feat/user/repository/UserJpaRepository.java
+++ src/main/java/gift/feat/user/repository/UserJpaRepository.java
@@ -1,5 +1,7 @@
 package gift.feat.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import gift.feat.user.User;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
-	User findByEmail(String email);
+	Optional<User> findByEmail(String email);
 }
