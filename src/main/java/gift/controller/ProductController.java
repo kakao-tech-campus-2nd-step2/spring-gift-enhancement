b43450f53b246ba0a372f89a2ed6a04f7b3cf70e
donghyun kim <gimdonghyun5434@gmail.com>
2024-07-18 17:20:14 +0900

    feat : product에 category 객체 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,7 +1,11 @@
 package gift.controller;
 
+import gift.domain.Category;
 import gift.domain.Product;
+import gift.dto.CategoryResponseDto;
+import gift.dto.ProductRequestDto;
 import gift.exception.ProductNotFoundException;
+import gift.service.CategoryService;
 import gift.service.ProductService;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,10 +24,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 
     private final ProductService productService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public ProductController(ProductService productService){
+    public ProductController(ProductService productService,CategoryService categoryService){
         this.productService = productService;
+        this.categoryService = categoryService;
     }
 
     // 목록 페이지
@@ -36,41 +43,44 @@ public class ProductController {
     // id 클릭 시 상품 상세보기
     @GetMapping("/{id}")
     public String findProductById(@PathVariable Long id,Model model){
-        try{
-            Product product = productService.findById(id);
-            model.addAttribute("product",product);
-            return "product";
-        }catch(ProductNotFoundException e){
-            return "products";
-        }
+        Product product = productService.findById(id);
+        model.addAttribute("productDto",new ProductRequestDto(product.getId(),product.getName(),
+            product.getPrice(), product.getImageUrl(),product.getCategory().getId()));
+        Category category = categoryService.findById(product.getCategory().getId());
+        model.addAttribute("category",category);
+        return "product";
     }
 
     // 수정
     @GetMapping("/{id}/edit")
-    public String editForm(@PathVariable Long id, Model model)
-    {
+    public String editForm(@PathVariable Long id, Model model) {
         Product product = productService.findById(id);
-        model.addAttribute("product",product);
+        model.addAttribute("productDto",new ProductRequestDto(product.getId(),product.getName(),
+            product.getPrice(), product.getImageUrl(),product.getCategory().getId()));
+        List<CategoryResponseDto> categories = categoryService.findAll();
+        model.addAttribute("categories",categories);
         return "editForm";
     }
 
     @PostMapping("/{id}/edit")
-    public String edit(@PathVariable Long id,@Valid @ModelAttribute Product product)
+    public String edit(@PathVariable Long id,@Valid @ModelAttribute ProductRequestDto productRequestDto)
     {
-        productService.updateProduct(id, product);
+        productService.updateProduct(id, productRequestDto);
         return "redirect:/products";
     }
 
     // 추가
     @GetMapping("/add")
     public String addForm(Model model){
-        model.addAttribute("product",new Product());
+        model.addAttribute("productDto",new ProductRequestDto(null,null,0,null,null));
+        List<CategoryResponseDto> categories = categoryService.findAll();
+        model.addAttribute("categories",categories);
         return "addForm";
     }
 
     @PostMapping("/add")
-    public String addProduct(@Valid @ModelAttribute Product product){
-        productService.save(product);
+    public String addProduct(@Valid @ModelAttribute ProductRequestDto productRequestDto){
+        productService.save(productRequestDto);
         return "redirect:/products";
     }
 
