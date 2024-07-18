f427c2ec2422ac32c45d2572f265409046eb5dde
sunandrabbit <sunandrabbit@gamil.com>
2024-07-18 16:01:35 +0900

    feat : Option에 대한 validation 추가

--- src/main/java/gift/domain/Option.java
+++ src/main/java/gift/domain/Option.java
@@ -1,20 +1,22 @@
 package gift.domain;
 
-import gift.entity.OptionEntity;
-import gift.entity.ProductEntity;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 
 public class Option {
 
-
     private Long id;
 
-
+    @NotBlank(message = "옵션 이름을 입력해 주세요.")
+    @Size(min = 1, max = 50, message = "1~50자 사이로 적어주세요")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9()\\[\\]+\\-&/_]*$", message = "유효한 이름이 아닙니다")
     private String name;
 
-
+    @Size(min = 1, max = 99999999, message = "0초과 1억미만의 범위로 입력 가능합니다.")
     private Long quantity;
-
 
     private Long productId;
 
