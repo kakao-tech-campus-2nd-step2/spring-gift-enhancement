2d1cd0b3f1caba0b05e4f4f085a9be74ba573864
pjhcsols <pjhcsols@naver.com>
2024-07-18 11:41:47 +0900

    Feat: 카테고리 서비스 클래스 로직 생성[박한솔]

---
+++ src/main/java/gift/service/category/CategoryService.java
@@ -0,0 +1,40 @@
+package gift.service.category;
+
+import gift.domain.category.Category;
+import gift.repository.category.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Category not found with id: " + id));
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
