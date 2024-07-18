c197d9619c855e84ebbbd54add2205e4f590a0cf
UiChan Lee <junglkls1234@gmail.com>
2024-07-18 16:48:21 +0900

    Feat: Repo관련 커스텀 예외 생성

---
+++ src/main/java/gift/exception/RepositoryRelatedException.java
@@ -0,0 +1,7 @@
+package gift.exception;
+
+public class RepositoryRelatedException extends RuntimeException{
+    public RepositoryRelatedException(String message) {
+        super(message);
+    }
+}
