b57e27740ff3da92cdcde443877144e2495a0a01
GamJaDo <kim0615157@gmail.com>
2024-07-18 18:05:18 +0900

    feat (dto사용, 상품의 카테고리만 수정하는 api 추가)

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -8,20 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import gift.dto.CategoryUpdateRequest;
+import gift.dto.ProductRequest;
+import gift.dto.ProductUpdateRequest;
 import gift.entity.Product;
 import gift.service.CategoryService;
 import gift.service.ProductService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin/products")
 public class AdminController {
 
-    private final ProductService productService;
     private final CategoryService categoryService;
+    private final ProductService productService;
     
-    public AdminController(ProductService productService, CategoryService categoryService) {
-    	this.productService = productService;
+    public AdminController(CategoryService categoryService, ProductService productService) {
     	this.categoryService =categoryService;
+    	this.productService = productService;
     }
 
     @GetMapping
@@ -34,14 +38,14 @@ public class AdminController {
 
     @GetMapping("/new")
     public String addProductForm(Model model) {
-        model.addAttribute("product", new Product("", 0, "", null));
+        model.addAttribute("productRequest", new ProductRequest("아이스 아메리카노 T", 4500, "https://example.com/image.jpg", "교환권"));
         model.addAttribute("categories", categoryService.getAllCategories());
         return "product-form";
     }
 
     @PostMapping
-    public String addProduct(@ModelAttribute Product product, BindingResult bindingResult) {
-        productService.createProduct(product, bindingResult);
+    public String addProduct(@ModelAttribute @Valid ProductRequest request, BindingResult bindingResult) {
+        productService.createProduct(request, bindingResult);
         return "redirect:/admin/products";
     }
 
@@ -51,15 +55,26 @@ public class AdminController {
         if (product == null) {
             return "redirect:/admin/products";
         }
-        model.addAttribute("product", product);
+
+        ProductUpdateRequest productUpdateRequest = new ProductUpdateRequest(product.getName(), 
+        		product.getPrice(), product.getImageUrl());
+        model.addAttribute("productUpdateRequest", productUpdateRequest);
         model.addAttribute("categories", categoryService.getAllCategories());
         return "product-form";
     }
 
     @PostMapping("/edit/{id}")
-    public String editProduct(@PathVariable("id") Long id, @ModelAttribute Product updatedProduct, BindingResult bindingResult) {
-        productService.updateProduct(id, updatedProduct, bindingResult);
+    public String editProduct(@PathVariable("id") Long id, @ModelAttribute @Valid ProductUpdateRequest request, 
+    		BindingResult bindingResult) {
+        productService.updateProduct(id, request, bindingResult);
         return "redirect:/admin/products";
+    }
+    
+    @PutMapping("/edit/{id}/catetory")
+    public String updateProductCategory(@PathVariable("id") Long id, @ModelAttribute @Valid CategoryUpdateRequest request, 
+    		BindingResult bindingResult) {
+    	productService.updateProductCategory(id, request, bindingResult);
+    	return "redirect:/admin/products";
     }
 
     @DeleteMapping("/{id}")
