ba7e3a387588f644c26d110403358500f5a887ce
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-18 20:18:25 +0900

    fix : WishListController wishListRequest, wishListResponse 적용

--- src/main/java/gift/wishList/WishListController.java
+++ src/main/java/gift/wishList/WishListController.java
@@ -24,21 +24,21 @@ public class WishListController {
     //생성
     @PostMapping
     @Transactional
-    public WishListDTO addWishes(@LoginUser User user, @RequestBody WishListDTO wishListDTO) {
+    public WishListResponse addWishes(@LoginUser User user, @RequestBody WishListRequest wishListDTO) {
         return wishListService.addWish(wishListDTO, user);
     }
 
     //조회(userid)
     @GetMapping
     @Transactional
-    public List<WishListDTO> getWishesByUser(@LoginUser User user) {
+    public List<WishListResponse> getWishesByUser(@LoginUser User user) {
         return wishListService.findByUser(user);
     }
 
     //수정(count) -> 0이면 삭제
     @PutMapping("/{id}")
     @Transactional
-    public WishListDTO updateWishesCount(@PathVariable long id, @RequestBody CountDTO count) {
+    public WishListResponse updateWishesCount(@PathVariable long id, @RequestBody CountDTO count) {
         if (count.count == 0) {
             wishListService.deleteByID(id);
             return null;
@@ -49,18 +49,18 @@ public class WishListController {
     //삭제(id)
     @DeleteMapping("/{id}")
     @Transactional
-    public WishListDTO deleteWishes(@PathVariable long id) {
+    public WishListResponse deleteWishes(@PathVariable long id) {
         wishListService.deleteByID(id);
         return null;
     }
 
     @GetMapping("/pages")
     @Transactional
-    public ResponseEntity<Page<WishListDTO>> getWishListsPage(@LoginUser User user,
-                                                              @RequestParam(required = false, defaultValue = "0", value = "page") int page,
-                                                              @RequestParam(required = false, defaultValue = "10", value = "size") @Min(1) @Max(20) int size,
-                                                              @RequestParam(required = false, defaultValue = "id", value = "sortBy") String sortBy,
-                                                              @RequestParam(required = false, defaultValue = "asc", value = "sortDirection") String sortDirection) {
+    public ResponseEntity<Page<WishListResponse>> getWishListsPage(@LoginUser User user,
+                                                                  @RequestParam(required = false, defaultValue = "0", value = "page") int page,
+                                                                  @RequestParam(required = false, defaultValue = "10", value = "size") @Min(1) @Max(20) int size,
+                                                                  @RequestParam(required = false, defaultValue = "id", value = "sortBy") String sortBy,
+                                                                  @RequestParam(required = false, defaultValue = "asc", value = "sortDirection") String sortDirection) {
 
         return ResponseEntity.ok(wishListService.getWishListsPages(page, size, user, sortBy, sortDirection));
     }
