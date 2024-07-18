ae14195336b224429e8c16a939acbd1a194827a2
mingjuu <tisu79@naver.com>
2024-07-18 09:48:15 +0900

    fix : 테이블명과 클래스명 동기화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS categories (
+CREATE TABLE IF NOT EXISTS category (
                                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                           name VARCHAR(255) NOT NULL UNIQUE
     );
