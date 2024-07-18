d080293e9ce2412d5db53e5d54f0789fd45bb017
안원모 <tommy45231@naver.com>
2024-07-18 16:29:30 +0900

    Refacotr : 관리자 화면 html 수정 , db수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,12 +4,23 @@ CREATE TABLE IF NOT EXISTS member (
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS category (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(7),
+    image_url VARCHAR(255),
+    description VARCHAR(255),
+    UNIQUE (name)
+);
+
 CREATE TABLE IF NOT EXISTS product (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     description VARCHAR(255),
-    image_url VARCHAR(255)
+    image_url VARCHAR(255),
+    category_id BIGINT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 CREATE TABLE IF NOT EXISTS wish (
