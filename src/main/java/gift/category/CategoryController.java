b4e3b0ba9f382883498759b686cbfa6c95a098a1
doyooon <ian661087@gmail.com>
2024-07-18 18:12:33 +0900

    feat Category : 상품 카테고리 구현

---
+++ src/main/java/gift/category/CategoryController.java
@@ -0,0 +1,46 @@
+package gift.category;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Category>> getAllCategories(){
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> addCategory(@RequestBody CategoryRequest categoryRequest){
+        Category category = categoryService.createCategory(categoryRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> updateCategory(@RequestBody CategoryRequest categoryRequest){
+        categoryService.updateCategory(categoryRequest);
+        return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteCategory(@RequestParam Long id){
+        categoryService.deleteCategory(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
