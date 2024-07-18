9cd6237f937171febd73d50665003cd0854a70a5
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-18 23:09:47 +0900

    feat: option repository 에서 id로 option 가져오기 기능 구현

--- src/main/java/gift/product/model/OptionRepository.java
+++ src/main/java/gift/product/model/OptionRepository.java
@@ -1,9 +1,11 @@
 package gift.product.model;
 
 import gift.product.model.dto.option.Option;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
+    Optional<Option> findByIdAndIsActiveTrue(Long id);
 }
