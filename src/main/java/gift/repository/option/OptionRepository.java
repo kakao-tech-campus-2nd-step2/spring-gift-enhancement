75bbe3f6ee432bc1132168bfb32a0ee221d04fff
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-18 21:48:40 +0900

    feat : OptionRepository 생성

--- src/main/java/gift/repository/option/OptionRepository.java
+++ src/main/java/gift/repository/option/OptionRepository.java
@@ -1,2 +1,12 @@
-package gift.repository.option;public class OptionRepository {
+package gift.repository.option;
+
+import gift.model.option.Option;
+import gift.model.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OptionRepository extends JpaRepository<Option, Long> {
+    boolean existsByProductAndName(Product product, String name);
+
 }
