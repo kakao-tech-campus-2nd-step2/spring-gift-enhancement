0f9438b56c93a65a3a5257bb3c3f2db3da6a3b02
us4c0d3 <jwseok312@gmail.com>
2024-07-18 20:07:14 +0900

    refactor(exception): 예외 처리를 enum ErrorCode와 CustomException으로 통일

--- src/main/java/gift/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package gift.exception;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/gift/exception/user/UserUnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package gift.exception.user;
-
-public class UserUnauthorizedException extends RuntimeException {
-
-    public UserUnauthorizedException(String message) {
-        super(message);
-    }
-
-}
