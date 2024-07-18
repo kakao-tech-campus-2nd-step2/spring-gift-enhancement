4b851c4bc1753b70f1ebec2f91e7d29be3de4b7a
suhyeon7497 <nucc1186@gmail.com>
2024-07-18 14:13:52 +0900

    chore: 사용하지 않는 메서드 제거

--- src/main/java/gift/category/model/Category.java
+++ src/main/java/gift/category/model/Category.java
@@ -1,6 +1,5 @@
 package gift.category.model;
 
-import gift.common.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,14 +26,6 @@ public class Category {
     }
 
     public Category(String name, String color, String imageUrl, String description) {
-        this.name = name;
-        this.color = color;
-        this.imageUrl = imageUrl;
-        this.description = description;
-    }
-
-    public Category(Long id, String name, String color, String imageUrl, String description) {
-        this.id = id;
         this.name = name;
         this.color = color;
         this.imageUrl = imageUrl;
