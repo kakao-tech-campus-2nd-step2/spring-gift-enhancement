373ba08a5a7561429b39fb78063186a2fd095b89
Lee Hyungi <geist9110@naver.com>
2024-07-18 22:29:32 +0900

    feat: Add all argument constructor and map to product table

--- src/main/java/gift/option/Option.java
+++ src/main/java/gift/option/Option.java
@@ -1,9 +1,12 @@
 package gift.option;
 
+import gift.product.Product;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Option {
@@ -16,7 +19,18 @@ public class Option {
 
     private int quantity;
 
+    @ManyToOne
+    @JoinColumn(name = "product")
+    private Product product;
+
     protected Option() {
 
+    }
+
+    public Option(long id, String name, int quantity, Product product) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.product = product;
     }
 }
