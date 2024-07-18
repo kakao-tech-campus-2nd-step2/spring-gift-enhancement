9a1997b8a01d96fcc005fd38dc5aafa06c7ec406
mingjuu <tisu79@naver.com>
2024-07-18 09:41:57 +0900

    fix : 안쓰는 함수 제거

--- src/main/java/gift/config/JwtConfig.java
+++ src/main/java/gift/config/JwtConfig.java
@@ -14,15 +14,7 @@ public class JwtConfig {
         return secret;
     }
 
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
     public long getExpirationTime() {
         return expirationTime;
-    }
-
-    public void setExpirationTime(long expirationTime) {
-        this.expirationTime = expirationTime;
     }
 }
