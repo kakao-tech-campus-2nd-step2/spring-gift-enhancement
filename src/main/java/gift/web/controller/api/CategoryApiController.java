4971ce083c176ff38fab7680226040786eef3c6a
나제법 <skwpqjq@gmail.com>
2024-07-18 15:04:45 +0900

    feat: CategoryApiController.java 단일 카테고리 조회 API 추가

--- src/main/java/gift/web/controller/api/CategoryApiController.java
+++ src/main/java/gift/web/controller/api/CategoryApiController.java
@@ -4,11 +4,13 @@ import gift.service.CategoryService;
 import gift.web.dto.request.category.CreateCategoryRequest;
 import gift.web.dto.response.category.CreateCategoryResponse;
 import gift.web.dto.response.category.ReadAllCategoriesResponse;
+import gift.web.dto.response.category.ReadCategoryResponse;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +38,12 @@ public class CategoryApiController {
     @GetMapping
     public ResponseEntity<ReadAllCategoriesResponse> readAllCategories() {
         ReadAllCategoriesResponse response = categoryService.readAllCategories();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReadCategoryResponse> readCategory(@PathVariable Long id) {
+        ReadCategoryResponse response = categoryService.readCategory(id);
         return ResponseEntity.ok(response);
     }
 
