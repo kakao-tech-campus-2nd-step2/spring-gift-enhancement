1cf3ab78d8cfbce38f564dd9fe46d8b3aebda6f9
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-18 20:19:22 +0900

    feat : WishList Service addWish - Product 추가 - 이미 위시리스트가 존재하는 경우 count만 업데이트

--- src/main/java/gift/wishList/WishListService.java
+++ src/main/java/gift/wishList/WishListService.java
@@ -3,7 +3,6 @@ package gift.wishList;
 import gift.option.Option;
 import gift.option.OptionRepository;
 import gift.product.Product;
-import gift.product.ProductRepository;
 import gift.user.User;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class WishListService {
@@ -22,26 +22,34 @@ public class WishListService {
         this.optionRepository = optionRepository;
     }
 
-    public WishListDTO addWish(WishListDTO wishListDTO, User user) {
+    public WishListResponse addWish(WishListRequest wishListDTO, User user) {
         Option option = optionRepository.findById(wishListDTO.getOptionID()).orElseThrow();
+        Product product = option.getProduct();
+
+        Optional<WishList> exist = wishListRepository.findByUserAndOptionId(user, option.getId());
+        if(exist.isPresent()){
+            return updateCount(new CountDTO(wishListDTO.count + exist.get().getCount()), exist.get().getId());
+        }
+
         WishList wishList = new WishList(wishListDTO.getCount());
         user.addWishList(wishList);
         option.addWishList(wishList);
+        product.addWishList(wishList);
         wishListRepository.save(wishList);
-        return new WishListDTO(wishList);
+        return new WishListResponse(wishList);
     }
 
-    public List<WishListDTO> findByUser(User user) {
+    public List<WishListResponse> findByUser(User user) {
         List<WishList> wishLists = wishListRepository.findByUser(user);
-        List<WishListDTO> wishListDTOS = new ArrayList<>();
-        wishLists.forEach((wishList -> wishListDTOS.add(new WishListDTO(wishList))));
+        List<WishListResponse> wishListDTOS = new ArrayList<>();
+        wishLists.forEach((wishList -> wishListDTOS.add(new WishListResponse(wishList))));
         return wishListDTOS;
     }
 
-    public WishListDTO updateCount(CountDTO count, Long id) {
+    public WishListResponse updateCount(CountDTO count, Long id) {
         WishList wishList = wishListRepository.findById(id).orElseThrow();
         wishList.setCount(count.getCount());
-        return new WishListDTO(wishList);
+        return new WishListResponse(wishList);
     }
 
     public void deleteByID(Long id) {
@@ -51,14 +59,14 @@ public class WishListService {
         wishListRepository.deleteById(id);
     }
 
-    public Page<WishListDTO> getWishListsPages(int pageNum, int size, User user, String sortBy, String sortDirection) {
+    public Page<WishListResponse> getWishListsPages(int pageNum, int size, User user, String sortBy, String sortDirection) {
         Pageable pageable = PageRequest.of(pageNum, size, Sort.by(Sort.Order.asc(sortBy)));
         if (Objects.equals(sortDirection, "desc")) {
             pageable = PageRequest.of(pageNum, size, Sort.by(Sort.Order.desc(sortBy)));
         }
 
         Page<WishList> wishLists = wishListRepository.findByUser(user, pageable);
-        return wishLists.map(WishListDTO::new);
+        return wishLists.map(WishListResponse::new);
 
     }
 }
