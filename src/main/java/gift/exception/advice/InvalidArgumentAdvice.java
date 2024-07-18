facd0f9996ece623c69799dcec34450bf08d3573
Giheon Do <dgh0001@naver.com>
2024-07-18 20:22:34 +0900

    feat: add IllegalArgumentException handler

--- src/main/java/gift/exception/advice/InvalidArgumentAdvice.java
+++ src/main/java/gift/exception/advice/InvalidArgumentAdvice.java
@@ -8,11 +8,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class NotFoundControllerAdvice {
+public class InvalidArgumentAdvice {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
