da4e9d723391178dd0f390e49dd3d0fd45869a43
UiChan Lee <junglkls1234@gmail.com>
2024-07-18 17:24:27 +0900

    Feat: OptionController 생성

---
+++ src/main/java/gift/controller/OptionController.java
@@ -0,0 +1,23 @@
+package gift.controller;
+
+import gift.model.product.Option;
+import gift.service.OptionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@RequestMapping("/api/products/{productId}/options")
+@Controller
+public class OptionController {
+    private final OptionService optionService;
+
+    public OptionController(OptionService optionService){
+        this.optionService = optionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Option>> getAllOptions(@PathVariable Long productId) {
+    }
+}
