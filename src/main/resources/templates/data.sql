a1fb1c9550357d8a0555dc234e4708cb7609e073
donghyeon <thornappl2@gmail.com>
2024-07-19 01:00:53 +0900

    sql: 샘플 데이터 수정

--- src/main/resources/templates/data.sql
+++ src/main/resources/templates/data.sql
@@ -1,6 +1,5 @@
 INSERT INTO category (name, color, image_url, description) VALUES
-    ('교환권', '#6c95d1', 'https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png', ''),
-    ('상품권', '#ff6347', 'https://example.com/images/giftcard.jpg', '상품권 설명');
+    ('교환권', '#6c95d1', 'https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png', '');
 
 INSERT INTO product (name, price, image_url, category_id) VALUES
     ('아이스 카페 아메리카노 T', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg', 1);
