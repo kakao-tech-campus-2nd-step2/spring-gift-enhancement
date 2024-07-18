2cadb64d21a98fc340f995c8842d8b27a8a43c0f
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-18 20:12:18 +0900

    feat: Option ADD

--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -1,7 +1,9 @@
 package gift.service;
 
+import gift.dto.OptionDto;
 import gift.repository.OptionRepository;
 import gift.vo.Option;
+import gift.vo.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +12,24 @@ import java.util.List;
 public class OptionService {
 
     private final OptionRepository optionRepository;
+    private final ProductService productService;
 
-    public OptionService(OptionRepository optionRepository) {
+    public OptionService(OptionRepository optionRepository, ProductService productService) {
         this.optionRepository = optionRepository;
+        this.productService = productService;
+    }
+
+    private Product getProduct(OptionDto optionDto) {
+        return productService.getProductById(optionDto.productId());
     }
 
     public List<Option> getOptionsPerProduct(Long id) {
         return optionRepository.findAllByProductId(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 상품의 옵션이 존재하지 않습니다."));
+    }
+
+    public void addOption(OptionDto optionDto) {
+        Option option = optionDto.toOption(getProduct(optionDto));
+        optionRepository.save(option);
     }
 }
