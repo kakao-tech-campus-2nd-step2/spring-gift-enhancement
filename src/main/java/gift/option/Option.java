4ead4ea9db7756e962d2d3430d650b0fea153ab6
suhyeon7497 <nucc1186@gmail.com>
2024-07-18 15:42:28 +0900

    feat: Option entity클래스

---
+++ src/main/java/gift/option/Option.java
@@ -0,0 +1,26 @@
+package gift.option;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    protected Option() {
+
+    }
+
+}
