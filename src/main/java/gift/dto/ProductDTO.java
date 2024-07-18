7424599c16524eb379aff02ea0988bf8bef90098
안원모 <tommy45231@naver.com>
2024-07-18 15:56:41 +0900

    Feat : CategoryRepository 생성

--- src/main/java/gift/dto/ProductDTO.java
+++ src/main/java/gift/dto/ProductDTO.java
@@ -1,9 +1,10 @@
 package gift.dto;
 
+import gift.domain.Category;
+import gift.domain.CategoryName;
 import gift.domain.Product;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
@@ -16,7 +17,7 @@ public class ProductDTO {
     @Pattern(regexp = "^(?!.*(?i)(kakao|카카오).*$).*$", message = "상품 이름에 '카카오'를 사용할 수 없습니다.")
     private String name;
 
-    @NotNull(message = "가격은 필수 입력 항목입니다.")
+    @NotBlank(message = "가격은 필수 입력 항목입니다.")
     @DecimalMin(value = "0.0", inclusive = false, message = "가격은 0보다 커야 합니다.")
     private BigDecimal price;
 
@@ -24,11 +25,17 @@ public class ProductDTO {
 
     private String description;
 
+    @NotBlank(message = "카테고리는 필수 입력 항목입니다.")
+    private CategoryName categoryName;
+
+    public ProductDTO() {}
+
     private ProductDTO(ProductDTOBuilder builder) {
         this.name = builder.name;
         this.price = builder.price;
         this.imageUrl = builder.imageUrl;
         this.description = builder.description;
+        this.categoryName = builder.categoryName;
     }
 
     public String getName() {
@@ -47,11 +54,16 @@ public class ProductDTO {
         return description;
     }
 
+    public CategoryName getCategoryName() {
+        return categoryName;
+    }
+
     public static class ProductDTOBuilder {
         private String name;
         private BigDecimal price;
         private String imageUrl;
         private String description;
+        private CategoryName categoryName;
 
         public ProductDTOBuilder name(String name) {
             this.name = name;
@@ -73,17 +85,23 @@ public class ProductDTO {
             return this;
         }
 
+        public ProductDTOBuilder categoryName(CategoryName categoryName) {
+            this.categoryName = categoryName;
+            return this;
+        }
+
         public ProductDTO build() {
             return new ProductDTO(this);
         }
     }
 
-    public Product toEntity() {
+    public Product toEntity(Category category) {
         return new Product.ProductBuilder()
             .name(this.name)
             .price(this.price)
             .imageUrl(this.imageUrl)
             .description(this.description)
+            .category(category)
             .build();
     }
 }
