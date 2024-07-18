ae752b7b8a31acf4f3ec8c9a55d845fd07eca739
yooonwodyd <dbswodyd00@naver.com>
2024-07-18 19:01:55 +0900

    [feat] 요구사항 변경에 따른 Dto 변경
    (cherry picked from commit 763672b62cece5c72e1dbcbd8d546590c7cd9bbb)

--- src/main/java/gift/feat/product/contoller/dto/ProductResponseDto.java
+++ src/main/java/gift/feat/product/contoller/dto/ProductResponseDto.java
@@ -5,13 +5,15 @@ import gift.feat.product.domain.Product;
 public record ProductResponseDto(
 	String name,
 	Long price,
-	String imageUrl
+	String imageUrl,
+	String categoryName
 ) {
 	static public ProductResponseDto from(Product product) {
 		return new ProductResponseDto(
 			product.getName(),
 			product.getPrice(),
-			product.getImageUrl()
+			product.getImageUrl(),
+			product.getCategory().getName()
 		);
 	}
 }
