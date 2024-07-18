e1965cc3967df792a673ed0ea68516a20b7de090
dong_yxxn <g0326kdy@gmail.com>
2024-07-18 23:22:13 +0900

    refactor(ProductDTO): optionIdList -> optionNameList로 수정

--- src/main/java/gift/product/domain/ProductDTO.java
+++ src/main/java/gift/product/domain/ProductDTO.java
@@ -10,18 +10,20 @@ public class ProductDTO {
     private String imageUrl;
     private Long categoryId;
 
-    private List<Long> optionIdList;
+    private List<String> optionNameList;
 
     public ProductDTO() {
         id = 0L;
     }
 
-    public ProductDTO(Long id, String name, Long price, String imageUrl, Long categoryId) {
+    public ProductDTO(Long id, String name, Long price, String imageUrl, Long categoryId,
+        List<String> optionNameList) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.categoryId = categoryId;
+        this.optionNameList = optionNameList;
     }
 
     public Long getId() {
