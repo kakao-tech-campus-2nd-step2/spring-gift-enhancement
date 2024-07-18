0dea5c0c5de0300a445c1388c94de78549f263f4
seojiu <seojiu5598@gmail.com>
2024-07-18 17:52:23 +0900

    feat(OptionService.java, OptionApiController.java): 각 상품의 옵션 조회 기능 구현

--- src/main/java/gift/controller/OptionApiController.java
+++ src/main/java/gift/controller/OptionApiController.java
@@ -1,7 +1,11 @@
 package gift.controller;
 
+import gift.model.Option;
 import gift.service.OptionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -11,5 +15,11 @@ public class OptionApiController {
 
     public OptionApiController(OptionService optionService) {
         this.optionService = optionService;
+    }
+
+    @GetMapping("/{productId}/options")
+    public ResponseEntity<List<Option>> getOptionsByProductId(@PathVariable Long productId) {
+        List<Option> options = optionService.getOptionsByProductId(productId);
+        return ResponseEntity.ok(options);
     }
 }
--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -1,14 +1,28 @@
 package gift.service;
 
+import gift.model.Option;
+import gift.model.Product;
 import gift.repository.OptionRepository;
+import gift.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class OptionService {
 
     private final OptionRepository optionRepository;
+    private final ProductRepository productRepository;
 
-    public OptionService(OptionRepository optionRepository) {
+    public OptionService(OptionRepository optionRepository, ProductRepository productRepository) {
         this.optionRepository = optionRepository;
+        this.productRepository = productRepository;
+    }
+
+    public List<Option> getOptionsByProductId(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
+        return product.getOptions();
     }
 }
