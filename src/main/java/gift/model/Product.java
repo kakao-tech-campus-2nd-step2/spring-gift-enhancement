55315f1159ef85fb1e345ab23f786269ca1189ac
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-18 15:13:24 +0900

    feat : 불필요한 setter 제거

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -44,23 +44,11 @@ public class Product {
                 return name;
         }
 
-        public void setName(String name) {
-                this.name = name;
-        }
-
         public int getPrice() {
                 return price;
         }
 
-        public void setPrice(int price) {
-                this.price = price;
-        }
-
         public String getImageUrl() {
                 return imageUrl;
-        }
-
-        public void setImageUrl(String imageUrl) {
-                this.imageUrl = imageUrl;
         }
 }
