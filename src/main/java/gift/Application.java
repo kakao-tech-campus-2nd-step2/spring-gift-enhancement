13032a482b09843f37a5c34a6f0c3a6cc358d663
UiChan Lee <junglkls1234@gmail.com>
2024-07-18 15:21:02 +0900

    Refactor: @ConstructorBinding 사용

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,9 +1,12 @@
 package gift;
 
+import gift.jwt.JwtTokenProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtTokenProvider.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
