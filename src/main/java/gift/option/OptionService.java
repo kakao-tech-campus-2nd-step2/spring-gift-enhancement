bc06fb41e4662a4d74df5dae21c1d6b84d5836e6
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-18 14:53:24 +0900

    fix : Option Service checkIfDuplicatedOption -> private로 수정 checkIfDuplicatedOption -> findBy... 에서 existsBy...로 수정

--- src/main/java/gift/option/OptionService.java
+++ src/main/java/gift/option/OptionService.java
@@ -5,6 +5,7 @@ import gift.product.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,22 +18,28 @@ public class OptionService {
         this.productRepository = productRepository;
     }
 
-    public List<OptionResponse> findAllProductOptions(Long id) {
-        return optionRepository.findAllByProductId(id).stream().map(OptionResponse::new).toList();
+    public List<OptionResponse> findAllProductOptions(Long productID) {
+        return optionRepository.findAllByProductId(productID).stream().map(OptionResponse::new).toList();
     }
 
-    public OptionResponse insertProductNewOption(Long id, OptionRequest optionRequest) {
-        if(checkIfDuplicatedOption(id, optionRequest)) throw new IllegalArgumentException("이미 존재하는 옵션명입니다.");
-        Product product = productRepository.findById(id).orElseThrow();
+    public OptionResponse insertProductNewOption(Long productID, OptionRequest optionRequest) {
+        if(checkIfDuplicatedOption(productID, optionRequest)) throw new IllegalArgumentException("이미 존재하는 옵션명입니다.");
+        Product product = productRepository.findById(productID).orElseThrow();
         Option option = new Option(optionRequest, product);
         product.addOptions(option);
         return new OptionResponse(optionRepository.save(option));
     }
 
+    public List<OptionResponse> insertProductNewOptions(Long productID, List<OptionRequest> optionRequests) {
+        List<OptionResponse> responses = new ArrayList<>();
+        optionRequests.forEach((optionRequest) -> responses.add(insertProductNewOption(productID, optionRequest)));
+        return responses;
+    }
+
     @Transactional
-    public OptionResponse updateOption(Long id, Long optionId, OptionRequest optionRequest) {
-        if(checkIfDuplicatedOption(id, optionRequest)) throw new IllegalArgumentException("이미 존재하는 옵션명입니다.");
-        Product product = productRepository.findById(id).orElseThrow();
+    public OptionResponse updateOption(Long productID, Long optionId, OptionRequest optionRequest) {
+        if(checkIfDuplicatedOption(productID, optionRequest)) throw new IllegalArgumentException("이미 존재하는 옵션명입니다.");
+        Product product = productRepository.findById(productID).orElseThrow();
         Option option = optionRepository.findById(optionId).orElseThrow();
         product.removeOption(option);
         option.updateOption(optionRequest);
@@ -40,14 +47,18 @@ public class OptionService {
         return new OptionResponse(option);
     }
 
-    public void deleteOption(Long id, Long optionId) {
-        Product product = productRepository.findById(id).orElseThrow();
+    public void deleteOption(Long productID, Long optionId) {
+        Product product = productRepository.findById(productID).orElseThrow();
         product.removeOption(optionRepository.findById(optionId).orElseThrow());
         optionRepository.deleteById(optionId);
 
     }
 
-    public boolean checkIfDuplicatedOption(Long id, OptionRequest option){
-        return optionRepository.findByNameAndProductId(option.getOptionName(), id).isPresent();
+    private boolean checkIfDuplicatedOption(Long productID, OptionRequest option){
+        return optionRepository.existsByNameAndProductId(option.getOptionName(), productID);
+    }
+
+    public OptionResponse findByOptionID(Long optionID) {
+        return new OptionResponse(optionRepository.findById(optionID).orElseThrow());
     }
 }
