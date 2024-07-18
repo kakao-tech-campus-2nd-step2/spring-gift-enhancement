d08c539c0d913ab9e97540b90bb7949714249162
Geon Kim <kky8586696@naver.com>
2024-07-18 23:08:56 +0900

    conflict 해결

--- src/main/java/gift/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package gift.repository;
-
-import gift.model.category.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
