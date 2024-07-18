524258e176fdf34f54e1976818a231f4e80d2c16
HyeJiJUN <jhj910421@gmail.com>
2024-07-19 01:15:56 +0900

    feat: add Repository annotation in Option&Category Repository

--- src/main/java/gift/repository/OptionReposityory.java
+++ src/main/java/gift/repository/OptionReposityory.java
@@ -2,6 +2,8 @@ package gift.repository;
 
 import gift.model.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OptionReposityory extends JpaRepository<Option, Long> {
 }
