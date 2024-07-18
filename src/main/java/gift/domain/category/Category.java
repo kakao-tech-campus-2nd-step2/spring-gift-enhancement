637b0170d3c7cfcce125eaaacee159356373e581
bokyeong <wntjdals0412@gmail.com>
2024-07-18 16:32:04 +0900

    Feat: Category 엔티티

--- src/main/java/gift/domain/category/Category.java
+++ src/main/java/gift/domain/category/Category.java
@@ -14,8 +14,48 @@ public class Category {
     @Column(unique = true)
     private String name;
 
-    protected Category() {
+    @NotNull
+    @Column(length = 7)
+    private String color;
 
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String imageUrl;
+
+
+    protected Category() {
+    }
+
+    public Category(String name, String color, String description, String imageUrl) {
+        this.name = name;
+        this.color = color;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public void update(String name, String color, String description, String imageUrl) {
+        this.name = name;
+        this.color = color;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 
     public String getName() {
