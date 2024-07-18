1375468a40d2d10ff208cf87b16cacec2a241b17
wvssm <neoramiregi@gmail.com>
2024-07-18 15:13:29 +0900

    feat(Category): Category 생성자 추가

--- src/main/java/gift/domain/Category.java
+++ src/main/java/gift/domain/Category.java
@@ -46,6 +46,21 @@ public class Category {
         }
     }
 
+    public Category(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public Category(Long id, String name, String color, String imageUrl, String description) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
     public Category(Builder builder){
         this.id = builder.id;
         this.name = builder.name;
