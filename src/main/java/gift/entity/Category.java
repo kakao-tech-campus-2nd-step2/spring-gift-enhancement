805fd82b66851df901e73590743d0ab8d39d097a
amm0124 <amm012450@gmail.com>
2024-07-19 14:30:40 +0900

    REFACTORING : category entity id 전략 변경

--- src/main/java/gift/entity/Category.java
+++ src/main/java/gift/entity/Category.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, name="name", unique=true)
