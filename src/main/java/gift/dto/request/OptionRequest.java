b0da76d3efaec427458378230ce5e4aad6d0144d
wvssm <neoramiregi@gmail.com>
2024-07-19 16:02:48 +0900

    refactor(OptionRequest): Long productId 필드 제거

--- src/main/java/gift/dto/request/OptionRequest.java
+++ src/main/java/gift/dto/request/OptionRequest.java
@@ -12,9 +12,7 @@ public record OptionRequest(
 
         @Min(value = 1, message = "옵션 수량은 1 이상이어야 합니다.")
         @Max(value = 99999999, message = "옵션 수량은 1억 미만이어야 합니다.")
-        int quantity,
-
-        Long productId) {
+        int quantity) {
 
     public Option toEntity(Product product) {
         return new Option(this.name, this.quantity, product);
