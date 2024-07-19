c60421283ded0c41f493e0e9ec6865071916fed6
JinSeoHyun <15roa1121@gmail.com>
2024-07-20 04:30:46 +0900

    remove: 프로덕트-위시리스트 양방향 연관관계가 삭제되면서 필요없어진 테스트 삭제

--- src/test/java/gift/main/repository/ProductRepositoryTest.java
+++ src/test/java/gift/main/repository/ProductRepositoryTest.java
@@ -1,14 +1,9 @@
 package gift.main.repository;
 
-import gift.main.dto.CategoryRequest;
-import gift.main.entity.*;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DataJpaTest
@@ -36,20 +31,4 @@ class ProductRepositoryTest {
         this.entityManager = entityManager;
     }
 
-    @Test
-    public void getWishProductOfProduct() {
-        Category category = categoryRepository.save(new Category(new CategoryRequest(111, "Test Category")));
-        User user = userRepository.save(new User("User", "123", "123", Role.ADMIN));
-        Product product = productRepository.save(new Product("Product", 1200, "url", user, category));
-        WishProduct wishProduct = wishProductRepository.save(new WishProduct(product, user));
-        entityManager.flush();
-        entityManager.clear();
-
-        // When
-        Product foundProduct = productRepository.findById(product.getId()).orElseThrow();
-
-        // Then
-        assertThat(foundProduct.getWishProducts().size()).isEqualTo(1);
-
-    }
 }
