023ed8cefcdc18c5177ab204eeecde3fa356eb30
NextrPlue <p.plue1881@gmail.com>
2024-07-19 18:20:23 +0900

    feat(repository): create OptionRepository

---
+++ src/main/java/gift/entity/Option.java
@@ -0,0 +1,49 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Option extends BaseEntity {
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Option() {}
+
+    public Option(String name, int quantity, Product product) {
+        this.name = name;
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
