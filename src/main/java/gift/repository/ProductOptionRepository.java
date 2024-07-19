31e3592465ac9be176b5492bdeb05da53e87237e
Jeje <jjh4450jjh4450@gmail.com>
2024-07-19 15:48:54 +0900

    feat(ProductOption 레포지트리 추가): 카테고리 레포지트리 추가 완료

---
+++ src/main/java/gift/repository/ProductOptionRepository.java
@@ -0,0 +1,14 @@
+package gift.repository;
+
+import gift.entity.ProductOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ProductOption 엔티티를 위한 JPA 레포지토리 인터페이스.
+ */
+@Repository
+public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+    ProductOption getProductOptionById(Long id);
+
+}
