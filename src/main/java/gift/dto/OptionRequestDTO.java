c3010269c24b0fbc10969d2b4d59b658d058f0f0
ajy4304 <ajy4304@naver.com>
2024-07-19 14:15:56 +0900

    feat(OptionDTO): 옵션의 DTO 생성

--- src/main/java/gift/dto/OptionRequestDTO.java
+++ src/main/java/gift/dto/OptionRequestDTO.java
@@ -1,2 +1,35 @@
-package gift.dto;public class OptionRequestDTO {
+package gift.dto;
+
+import gift.model.Option;
+import gift.model.Product;
+
+public class OptionRequestDTO {
+
+    private String name;
+    private int quantity;
+    private Long productId;
+
+    protected OptionRequestDTO() {}
+
+    public OptionRequestDTO(String name, int quantity, Long productId) {
+        this.name = name;
+        this.quantity = quantity;
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Option toEntity(Product product) {
+        return new Option(this.name, this.quantity, product);
+    }
 }
