287b4b54c0a537e0f134a23a7bebd42b653dc17b
amm0124 <amm012450@gmail.com>
2024-07-19 14:49:12 +0900

    FEAT : option repository 구현

---
+++ src/main/java/gift/repository/OptionRepository.java
@@ -0,0 +1,10 @@
+package gift.repository;
+
+
+import gift.entity.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+
+
+}
