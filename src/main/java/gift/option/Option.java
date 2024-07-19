10c22bc60544888db55f4b0a34c952ac3052902b
SeoYeongU <westzeroright@gmail.com>
2024-07-19 17:31:05 +0900

    feat: 옵션 엔티티 클래스 정의

---
+++ src/main/java/gift/option/Option.java
@@ -0,0 +1,44 @@
+package gift.option;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "option")
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    protected Option() {
+    }
+
+    public Option(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+}
