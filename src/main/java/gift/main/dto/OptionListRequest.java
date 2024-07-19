375101016dd59ae65e5b1b7447b4cb63ffd8328d
JinSeoHyun <15roa1121@gmail.com>
2024-07-19 17:35:43 +0900

    fetch : 옵션 관련 응답,요청 객체
    [변경내용]
    OptionResponse: 유효성 검사로직 추가
    OptionListRequest: 내부 요청 객체 중복 메서드 추가

--- src/main/java/gift/main/dto/OptionListRequest.java
+++ src/main/java/gift/main/dto/OptionListRequest.java
@@ -1,26 +1,28 @@
 package gift.main.dto;
+
+import gift.main.Exception.CustomException;
+import gift.main.Exception.ErrorCode;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class OptionListRequest {
-    private List<OptionRequest> optionRequestList;
+@Validated
+public record OptionListRequest(
+        @Valid List<OptionRequest> optionRequests) {
 
+    public OptionListRequest {
+        if (optionRequests.isEmpty()) {
+            throw new CustomException(ErrorCode.EMPTY_OPTION);
+        }
 
-    public OptionListRequest(List<OptionRequest> optionRequestList) {
-        this.optionRequestList = optionRequestList;
-    }
-
-    public List<OptionRequest> getOptionRequestList() {
-        return optionRequestList;
-    }
-
-    @Override
-    public String toString() {
-        return "OptionListRequest{" +
-                "optionRequestList=" + optionRequestList +
-                '}';
-    }
-
-    public int getSize() {
-        return optionRequestList.size();
+        Set<OptionRequest> seen = new HashSet<>();
+        for (OptionRequest optionRequest : optionRequests) {
+            if (!seen.add(optionRequest)) {
+                throw new CustomException(ErrorCode.DUPLICATE_OPTION);
+            }
+        }
     }
 }
--- src/main/java/gift/main/dto/OptionRequest.java
+++ src/main/java/gift/main/dto/OptionRequest.java
@@ -1,4 +1,12 @@
 package gift.main.dto;
 
-public record OptionRequest(String name, int num) {
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record OptionRequest(
+        @Size(max = 50, message = "옵션 이름은 공백을 포함하여 최대 50자까지 입력할 수 있다.")
+        @Pattern(regexp = "^[a-zA-Z0-9\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$",
+                message = "특수 문자는 ( ), [ ], +, -, &, /, _ 만 가능합니다.")
+        String name,
+        int num) {
 }
