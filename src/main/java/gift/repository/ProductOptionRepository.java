cab80ec445ccc642648ad872b6777e6ff40c5d60
나제법 <skwpqjq@gmail.com>
2024-07-19 16:49:40 +0900

    feat: ProductOptionRepository.java 추가

---
+++ src/main/java/gift/repository/ProductOptionRepository.java
@@ -0,0 +1,13 @@
+package gift.repository;
+
+import gift.domain.ProductOption;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+
+    List<ProductOption> findAllByProductId(Long productId);
+
+}
