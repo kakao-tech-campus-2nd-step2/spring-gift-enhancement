130fb5b4f687f61a999c3d5f4673e9989f546213
hyunseo <wndlthsk@naver.com>
2024-07-19 17:49:52 +0900

    feat : 옵션 추가

---
+++ src/main/java/gift/domain/Option.java
@@ -0,0 +1,51 @@
+package gift.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+@Entity
+@Table(name = "option")
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+
+    @Min(1)
+    @Max(99999999)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Option(String name, int quantity, Product product) {
+        this.name = name;
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public Option() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+}
