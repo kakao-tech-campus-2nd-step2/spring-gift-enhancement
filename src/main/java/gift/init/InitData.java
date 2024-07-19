0533890e5fa4886d7a4e41f337ca9c7911ef41d6
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-19 15:07:03 +0900

    update: 주석 추가

--- src/main/java/gift/init/InitData.java
+++ src/main/java/gift/init/InitData.java
@@ -28,11 +28,17 @@ public class InitData {
 
     @PostConstruct
     public void init() {
+//        유저 생성
         userCreator.UserCreator();
+//        카테고리 생성
         categoryCreator.CategoryCreator();
+//        상품 생성
         productCreator.ProductCreator();
+//        위시리스트 생성
         wishCreator.WishCreator();
+//        상품의 옵션 추가
         productOptionCreator.ProductOptionCreator();
+//        상품의 옵션 재고 증감(상품주문)
         productOrderCreator.ProductCreator();
     }
 }
