81c9b702def5921f324bdb2c762c225c49b9297a
seojiu <seojiu5598@gmail.com>
2024-07-19 15:05:55 +0900

    chore(GlobalExceptionHandler.java, ProductService.java): 오류 메세지 수정, 코드 정리

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -38,7 +38,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleOtherExceptions(Exception ex) {
-        return new ResponseEntity<>("An error occurred"+ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> handleOtherExceptions() {
+        return new ResponseEntity<>("An error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
