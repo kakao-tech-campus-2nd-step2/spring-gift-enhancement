f5862e8b2a1235e89832a59e57f580bb9b07fecd
Jeje <jjh4450jjh4450@gmail.com>
2024-07-19 15:35:52 +0900

    feat(ProductOption 엔티티 추가): ProductOption 엔티티 추가 완료

---
+++ src/main/java/gift/entity/ProductOption.java
@@ -0,0 +1,54 @@
+package gift.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * 제품 옵션 엔티티. 데이터베이스 테이블과 매핑된다.
+ */
+@Entity
+public class ProductOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private long quantity;
+    protected ProductOption() {}
+
+    public ProductOption(String name, long quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+}
