f0922eed12003dfc604b00eb07d2026d7db9b6eb
Awhn <ahnshwan@outlook.com>
2024-07-19 17:47:43 +0900

    feat : write repository class to crud with option entities

---
+++ src/main/java/gift/repository/OptionRepository.java
@@ -0,0 +1,10 @@
+package gift.repository;
+
+import gift.entity.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+    Optional<Option> findById(int id);
+}
---
+++ src/main/java/gift/repository/OptionsRepository.java
@@ -0,0 +1,10 @@
+package gift.repository;
+
+import gift.entity.Options;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OptionsRepository extends JpaRepository<Options, Long> {
+    Optional<Options> findByProduct_Id(int product_id);
+}
