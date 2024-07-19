6de50df35651bd01da72912fa953b7d54dbf7e63
sanghee0820 <lsh277604@gmail.com>
2024-07-19 16:52:22 +0900

    fix: RequestMapping 추가

--- src/main/java/gift/product/application/ProductOptionController.java
+++ src/main/java/gift/product/application/ProductOptionController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/products/{productId}/options")
+@RestController
+@RequestMapping("/api/products/{productId}/options")
 public class ProductOptionController {
     private final ProductOptionService productOptionService;
 
