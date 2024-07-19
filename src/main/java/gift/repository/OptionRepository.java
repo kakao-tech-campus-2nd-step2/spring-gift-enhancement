b1478249f4ce206ef2b198148b3db1cc7da5fe3b
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-19 22:18:14 +0900

    feat : OptionRepository 수정

--- src/main/java/gift/repository/OptionRepository.java
+++ src/main/java/gift/repository/OptionRepository.java
@@ -4,7 +4,12 @@ import gift.model.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
 
+    List<Option> findAllByProductId(Long productId);
+
+    boolean existsByProductIdAndName(Long productId, String name);
 }
