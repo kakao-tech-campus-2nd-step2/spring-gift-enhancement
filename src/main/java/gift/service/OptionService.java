a4668936953744ca1a9ba5f7fc4ada3eccfcc388
Awhn <ahnshwan@outlook.com>
2024-07-19 17:49:19 +0900

    feat : write service class for option entities

---
+++ src/main/java/gift/service/OptionService.java
@@ -0,0 +1,52 @@
+package gift.service;
+
+import gift.domain.OptionDTO;
+import gift.entity.Option;
+import gift.entity.Options;
+import gift.repository.OptionRepository;
+import gift.repository.OptionsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class OptionService {
+    private final OptionsRepository optionsRepository;
+    private final OptionRepository optionRepository;
+
+    public OptionService (OptionsRepository optionsRepository, OptionRepository optionRepository) {
+        this.optionsRepository = optionsRepository;
+        this.optionRepository = optionRepository;
+    }
+
+    public Optional<Options> findByProduct_Id(int product_id) {
+        return optionsRepository.findByProduct_Id(product_id);
+    }
+
+    public Option addOption(int product_id, OptionDTO optionDTO) {
+        var savedOption = optionRepository.save(new Option(optionDTO));
+        var options = findByProduct_Id(product_id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
+        options.addOption(savedOption);
+        optionsRepository.save(options);
+        return savedOption;
+    }
+
+    public Option updateOption(int product_id, int id, OptionDTO optionDTO) {
+        try {
+            var savedOption = optionRepository.save(new Option(id, optionDTO));
+            var options = findByProduct_Id(product_id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
+            options.updateOption(savedOption);
+            optionsRepository.save(options);
+            return savedOption;
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("Option not found");
+        }
+    }
+
+    public void deleteOption(int product_id, int id) {
+        var options = findByProduct_Id(product_id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
+        options.deleteOption(id);
+        optionsRepository.save(options);
+    }
+}
