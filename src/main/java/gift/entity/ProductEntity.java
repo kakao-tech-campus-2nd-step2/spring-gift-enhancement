d9de226c099d4a4ba3d4fcb9d79a65f76c8c7f0e
peacefullyquietly <pjh73015@gmail.com>
2024-07-19 14:27:45 +0900

    refactor : 서비스 로직에따른 변경 사항

--- src/main/java/gift/entity/ProductEntity.java
+++ src/main/java/gift/entity/ProductEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
@@ -31,14 +33,19 @@ public class ProductEntity {
     @OneToMany(mappedBy = "productEntity", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<WishListEntity> wishListEntities;
 
+    @ManyToOne(targetEntity = CategoryEntity.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private CategoryEntity category;
+
     public ProductEntity() {}
 
-    public ProductEntity(Long id, String name, int price, String imageUrl) {}
+    public ProductEntity(Long id, String name, int price, String imageUrl, CategoryEntity category) {}
 
-    public ProductEntity(String name, int price, String imageUrl) {
+    public ProductEntity(String name, int price, String imageUrl, CategoryEntity category) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.category = category;
     }
 
     public Long getId() {
@@ -61,12 +68,15 @@ public class ProductEntity {
         return wishListEntities;
     }
 
+    public CategoryEntity getCategory() {return category;}
+
     public static ProductDTO toDTO(ProductEntity productEntity) {
         return new ProductDTO(
             productEntity.getId(),
             productEntity.getName(),
             productEntity.getPrice(),
-            productEntity.getImageUrl()
+            productEntity.getImageUrl(),
+            productEntity.getCategory().getId()
         );
     }
 
