47a62531a855f90009d40381fda0854d319c6f9d
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-19 16:54:05 +0900

    feat : 옵션 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,7 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 -- 기존 데이터 삭제 (테이블이 존재하는 경우에만 삭제)
 DELETE FROM wishlist WHERE EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'WISHLIST');
+DELETE FROM options WHERE EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'OPTIONS');
 DELETE FROM products WHERE EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'PRODUCTS');
 DELETE FROM users WHERE EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'USERS');
 DELETE FROM categories WHERE EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'CATEGORIES');
@@ -11,6 +12,7 @@ DELETE FROM categories WHERE EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHE
 ALTER TABLE products ALTER COLUMN id RESTART WITH 1;
 ALTER TABLE users ALTER COLUMN id RESTART WITH 1;
 ALTER TABLE categories ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE options ALTER COLUMN id RESTART WITH 1;
 
 -- 새로운 카테고리 데이터 삽입
 INSERT INTO categories (id, name, color, image_url, description) VALUES
@@ -26,6 +28,11 @@ INSERT INTO users (id, email, password) VALUES
 -- 새로운 제품 데이터 삽입
 INSERT INTO products (id, name, price, image_url, category_id) VALUES
     (1, 'Sample Product', 1000, 'https://via.placeholder.com/150', 1);
+
+-- 새로운 옵션 데이터 삽입
+INSERT INTO options (id, product_id, name, quantity) VALUES
+                                                         (1, 1, 'Option 1', 100),
+                                                         (2, 1, 'Option 2', 200);
 
 -- 새로운 위시리스트 데이터 삽입
 INSERT INTO wishlist (user_id, product_id)
