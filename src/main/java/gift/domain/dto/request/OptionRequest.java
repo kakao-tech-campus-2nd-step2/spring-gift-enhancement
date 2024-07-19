6339b497e93044cb1cef4c8b702614daf7a07640
lja3723 <lja3723@gmail.com>
2024-07-19 22:46:44 +0900

    feat: add Option DTO

---
+++ src/main/java/gift/domain/dto/request/OptionRequest.java
@@ -0,0 +1,33 @@
+package gift.domain.dto.request;
+
+import gift.domain.annotation.RestrictedSpecialChars;
+import gift.domain.entity.Option;
+import gift.domain.entity.Product;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
+public record OptionRequest(
+    @NotNull
+    @Size(min = 1, max = 50)
+    @RestrictedSpecialChars
+    String name,
+    @NotNull
+    @Size(min = 1, max = 100_000_000)
+    Integer quantity
+) {
+
+    public static OptionRequest of(Option option) {
+        return new OptionRequest(option.getName(), option.getQuantity());
+    }
+
+    public static List<OptionRequest> of(List<Option> options) {
+        return options.stream()
+            .map(OptionRequest::of)
+            .toList();
+    }
+
+    public Option toEntity(Product product) {
+        return new Option(product, name, quantity);
+    }
+}
