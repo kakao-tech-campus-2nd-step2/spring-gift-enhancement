5a39b6cf891268cf198452fc1f423f64ff966a7d
나제법 <skwpqjq@gmail.com>
2024-07-19 23:40:38 +0900

    feat: ProductOptionService.java createOption() 추가

--- src/main/java/gift/service/ProductOptionService.java
+++ src/main/java/gift/service/ProductOptionService.java
@@ -1,8 +1,12 @@
 package gift.service;
 
+import gift.domain.ProductOption;
 import gift.repository.ProductOptionRepository;
+import gift.web.dto.request.productoption.CreateProductOptionRequest;
+import gift.web.dto.response.productoption.CreateProductOptionResponse;
 import gift.web.dto.response.productoption.ReadAllProductOptionsResponse;
 import gift.web.dto.response.productoption.ReadProductOptionResponse;
+import gift.web.validation.exception.client.AlreadyExistsException;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +19,28 @@ public class ProductOptionService {
 
     public ProductOptionService(ProductOptionRepository productOptionRepository) {
         this.productOptionRepository = productOptionRepository;
+    }
+
+    public CreateProductOptionResponse createOption(Long productId, CreateProductOptionRequest request) {
+        String optionName = request.getName();
+        validateOptionNameExists(productId, optionName);
+
+        ProductOption createdOption = productOptionRepository.save(request.toEntity(productId));
+
+        return CreateProductOptionResponse.fromEntity(createdOption);
+    }
+
+    /**
+     * 상품 옵션 이름이 이미 존재하는지 확인합니다.<br>
+     * 이미 존재한다면 BadRequestException을 발생시킵니다.
+     * @param productId 상품 ID
+     * @param name 상품 옵션 이름
+     */
+    private void validateOptionNameExists(Long productId, String name) {
+        boolean isExistsOptionName = productOptionRepository.existsByNameAndProductId(name, productId);
+        if (isExistsOptionName) {
+            throw new AlreadyExistsException(name);
+        }
     }
 
     public ReadAllProductOptionsResponse readAllOptions(Long productId) {
