8a88bf58f6bc52b5292517978c0598528018c9de
doyooon <ian661087@gmail.com>
2024-07-19 16:16:05 +0900

    feat OptionRequest, Option.java : Option Entity 생성, 제약 사항 추가

---
+++ src/main/java/gift/option/OptionRequest.java
@@ -0,0 +1,41 @@
+package gift.option;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class OptionRequest {
+    private Long optionId;
+
+    @Size(min = 1, max = 50)
+    @Pattern(regexp = "^[A-Za-z가-힣0-9()\\[\\]\\-&/_+\\s]*$", message = "( ), [ ], +, -, &, /, _ 를 제외한 특수문자는 사용할 수 없습니다.")
+    @NotNull
+    private String name;
+
+    @Size(min = 1,max = 1_000_000_000)
+    @NotNull
+    private int quantity;
+
+    @NotNull
+    private Long productId;
+
+    public Long getOptionId(){
+        return optionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Option toEntity(){
+        return new Option(this.optionId, this.name,this.quantity,this.productId);
+    }
+}
