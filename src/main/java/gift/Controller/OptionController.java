af5a53d6fafdf6245c59249a8ce47f1041e87f74
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-07-19 14:21:02 +0900

    Feat: option Controller 작성

--- src/main/java/gift/Controller/OptionController.java
+++ src/main/java/gift/Controller/OptionController.java
@@ -1,5 +1,42 @@
 package gift.Controller;
 
-public class OptionController {
+import gift.Model.Option;
+import gift.Service.OptionService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class OptionController {
+    private final OptionService optionService;
+
+    public OptionController(OptionService optionService){
+        this.optionService = optionService;
+    }
+
+    @GetMapping("/api/products/{productId}/options")
+    public ResponseEntity<List<Option>> getOptions(@PathVariable(name = "productId") Long productId){
+        return ResponseEntity.ok().body(optionService.getAllOptions(productId));
+    }
+
+    @PostMapping("/api/products/options/add/{productId}")
+    public ResponseEntity<Option> addOption(@RequestBody Option option, @PathVariable(name = "productId") Long productId){
+        return ResponseEntity.ok().body(optionService.addOption(option, productId));
+    }
+
+    @PutMapping("/api/products/options/update/{productId}/{optionId}")
+    public ResponseEntity<Option> updateOption(@RequestBody Option option, @PathVariable(name = "productId") Long productId, @PathVariable(value = "optionId") Long optionId){
+        return ResponseEntity.ok().body(optionService.updateOption(option,productId,optionId));
+    }
+
+    @DeleteMapping("/api/products/options/delete/{productId}/{optionId}")
+    public ResponseEntity<Option> deleteOption(@PathVariable(value = "productId") Long productId, @PathVariable(value = "optionId") Long optionId){
+        return ResponseEntity.ok().body(optionService.deleteOption(productId,optionId));
+    }
 }
