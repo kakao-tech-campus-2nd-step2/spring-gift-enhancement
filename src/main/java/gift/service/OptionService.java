23c7c340f268f362e8b96a0e503205522643d9fd
Shsin9797 <aaa8141@naver.com>
2024-07-19 17:55:01 +0900

    feat : Option Service 유효성 검사 기능 추가

--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -1,6 +1,7 @@
 package gift.service;
 
 import gift.entity.Option;
+import gift.entity.Product;
 import gift.repository.OptionRepositoryInterface;
 
 import java.util.List;
@@ -13,14 +14,19 @@ public class OptionService {
     }
 
     public Option getById(Long optionId) {
-        return optionRepositoryInterface.findById(optionId);
+        return optionRepositoryInterface.findById(optionId).get();
     }
+
     public List<Option> getAllOptions() {
         return optionRepositoryInterface.findAll();
     }
 
-    public Option save(Option option) {
-        return optionRepositoryInterface.save(option);
+    public Option save(Option option,Product product) {
+        if ( checkValidOptionName(option.getName(),product) &&
+                checkValidOptionQuantity(option.getQuantity())){
+            return optionRepositoryInterface.save(option);
+        }
+        return null;
     }
 
     public void delete(Option option) {
@@ -30,17 +36,55 @@ public class OptionService {
     public Option update(Option option) {
         return optionRepositoryInterface.save(option);
     }
+
     public Option getOptionByName(String optionName) {
         return optionRepositoryInterface.findOptionByName(optionName);
     }
 
-    public boolean checkValidOptionName(String optionName) {
-
+    public boolean checkValidOptionName(String optionName,Product product) {
+        if (checkNameLength(optionName, 1, 50) &&
+                checkNameCharIsOkay(optionName) &&
+                checkNameRedundancy(optionName,product)) {
+            return true;
+        }
+        throw new IllegalArgumentException();
     }
 
-    public boolean checkValidOptionQuantity(Long optionQuantity){
 
+    public boolean checkValidOptionQuantity(Long optionQuantity) {
+        if (optionQuantity < 100000000 && optionQuantity > 1) {
+            return true;
+        }
+        throw new IllegalArgumentException();
     }
 
+    public boolean checkNameLength(String name, int maxLen, int minLen) {
+        if (name.length() <= maxLen && name.length() >= minLen) {
+            return true;
+        }
+        return false;
+    }
 
+    private boolean checkNameRedundancy(String optionName, Product product) {
+        for (int i = 0; i < product.getOptions().size(); i++) {
+            if (optionName.equals(product.getOptions().get(i).getName())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkNameCharIsOkay(String optionName) {
+
+        String okchar = "()[]+-&/_ ";
+        for (char nameChar : optionName.toCharArray()) {
+            if (okchar.contains(optionName) ||
+                    Character.isAlphabetic(nameChar) ||
+                    Character.isDigit(nameChar) ||
+                    optionName.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
