efd70745e340f37d93f097c93013ae266664ec16
seojiu <seojiu5598@gmail.com>
2024-07-19 16:37:19 +0900

    test(OptionServiceTest.java): 테스트 클래스 생성

---
+++ src/test/java/gift/ServiceTest/OptionServiceTest.java
@@ -0,0 +1,22 @@
+package gift.ServiceTest;
+
+import gift.repository.OptionRepository;
+import gift.service.OptionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class OptionServiceTest {
+
+    @Mock
+    private OptionRepository optionRepository;
+
+    @InjectMocks
+    private OptionService optionService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+}
