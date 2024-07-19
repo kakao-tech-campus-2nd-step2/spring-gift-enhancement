e079a4550ba48192e93834ed105e4dab53419809
dong_yxxn <g0326kdy@gmail.com>
2024-07-19 15:07:24 +0900

    feat(OptionRepo): findByProductId() 추가

--- src/main/java/gift/option/repository/OptionRepository.java
+++ src/main/java/gift/option/repository/OptionRepository.java
@@ -2,6 +2,7 @@ package gift.option.repository;
 
 import gift.option.domain.Option;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
     List<Option> findAllByProductId(Long productId);
+    Optional<Option> findByProductId(Long productId);
 }
