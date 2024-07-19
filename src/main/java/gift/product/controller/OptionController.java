c79867593aa41bf6789a558135a200be2a83d9fd
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-19 17:13:39 +0900

    feat: option controller cud 구현

--- src/main/java/gift/product/controller/OptionController.java
+++ src/main/java/gift/product/controller/OptionController.java
@@ -1,12 +1,20 @@
 package gift.product.controller;
 
+import gift.product.model.dto.option.CreateOptionRequest;
 import gift.product.model.dto.option.OptionResponse;
+import gift.product.model.dto.option.UpdateOptionRequest;
+import gift.product.model.dto.product.Product;
 import gift.product.service.OptionService;
 import gift.product.service.ProductService;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +29,30 @@ public class OptionController {
         this.productService = productService;
     }
 
-    @GetMapping("/{id}/options")
+    @GetMapping("/{postId}/options")
     public ResponseEntity<List<OptionResponse>> findOptionsByProductId(@PathVariable Long id) {
         List<OptionResponse> options = optionService.findOptionsByProductId(id);
         return ResponseEntity.ok().body(options);
+    }
+
+    @PostMapping("/{postId}/options")
+    public ResponseEntity<String> addOption(@PathVariable Long postId,
+                                            @Valid @RequestBody CreateOptionRequest createOptionRequest) {
+        Product product = productService.findProduct(postId);
+        optionService.addOption(product, createOptionRequest);
+        return ResponseEntity.ok().body("ok");
+    }
+
+    @PutMapping("/options/{optionId}")
+    public ResponseEntity<String> addOptionList(@PathVariable Long optionId,
+                                                @Valid @RequestBody UpdateOptionRequest updateOptionRequest) {
+        optionService.updateOption(optionId, updateOptionRequest);
+        return ResponseEntity.ok().body("ok");
+    }
+
+    @DeleteMapping("/options/{optionId}")
+    public ResponseEntity<String> deleteOption(@PathVariable Long optionId) {
+        optionService.deleteOption(optionId);
+        return ResponseEntity.ok().body("ok");
     }
 }
