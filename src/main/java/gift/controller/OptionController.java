d967fbe4914b6aa1e4a790de8d594bf8730039c1
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-19 17:27:24 +0900

    feat: OptionController 클래스 구현

---
+++ src/main/java/gift/controller/OptionController.java
@@ -0,0 +1,40 @@
+package gift.controller;
+
+import gift.dto.OptionRequestDto;
+import gift.dto.OptionResponseDto;
+import gift.service.OptionService;
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class OptionController {
+
+    private OptionService optionService;
+
+    @Autowired
+    public OptionController(OptionService optionService) {
+        this.optionService = optionService;
+    }
+
+    @GetMapping("/{productId}/options")
+    public ResponseEntity<List<OptionResponseDto>> getOptionsByProductId(@PathVariable Long productId) {
+        List<OptionResponseDto> optionList = optionService.getOptionsByProductId(productId);
+        return new ResponseEntity<>(optionList, HttpStatus.OK);
+    }
+
+    @PostMapping("/{productId}/options")
+    public ResponseEntity<String> addOptionToProduct(@PathVariable Long productId, @Valid @RequestBody OptionRequestDto optionRequestDto) {
+        optionService.addOptionToProduct(productId, optionRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
