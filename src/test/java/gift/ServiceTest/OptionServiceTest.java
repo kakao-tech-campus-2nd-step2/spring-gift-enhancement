c046c8e1fadc554d93dc5d09ea7712e3a4ecde0c
seojiu <seojiu5598@gmail.com>
2024-07-19 17:13:48 +0900

    test(OptionServiceTest.java): 주어진 옵션 ID가 존재하지 않을 때 예외를 던지는지 테스트

--- src/test/java/gift/ServiceTest/OptionServiceTest.java
+++ src/test/java/gift/ServiceTest/OptionServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,6 +54,20 @@ public class OptionServiceTest {
             optionService.decreaseOptionQuantity(optionId, 3);
         });
         assertEquals("옵션의 수량은 최소 1개 이상이어야 합니다.", exception.getMessage());
+        verify(optionRepository, times(1)).findById(optionId);
+        verify(optionRepository, times(0)).save(any(Option.class));
+    }
+
+    @Test
+    void decreaseNotFoundOptionQuantity() {
+        Long optionId = 1L;
+
+        when(optionRepository.findById(optionId)).thenReturn(Optional.empty());
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            optionService.decreaseOptionQuantity(optionId, 3);
+        });
+        assertEquals("존재하지 않는 옵션입니다.", exception.getMessage());
         verify(optionRepository, times(1)).findById(optionId);
         verify(optionRepository, times(0)).save(any(Option.class));
     }
