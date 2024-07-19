fd3fda94d33247038bdeaad3c1909ba8ec151d5b
sanghee0820 <lsh277604@gmail.com>
2024-07-19 16:58:50 +0900

    feat: ProductOption 반환 기능 추가

--- src/main/java/gift/product/service/ProductOptionService.java
+++ src/main/java/gift/product/service/ProductOptionService.java
@@ -4,6 +4,8 @@ import gift.product.exception.ProductNotFoundException;
 import gift.product.persistence.ProductOptionRepository;
 import gift.product.persistence.ProductRepository;
 import gift.product.service.command.ProductOptionCommand;
+import gift.product.service.dto.ProductOptionInfo;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,5 +35,23 @@ public class ProductOptionService {
         var productOption = productOptionRepository.findByProductIdAndId(productId, optionId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품 옵션입니다."));
         productOption.modify(productOptionCommand.name(), productOptionCommand.quantity());
+    }
+
+    @Transactional(readOnly = true)
+    public ProductOptionInfo getProductOptionInfo(Long productId, Long optionId) {
+        var productOption = productOptionRepository.findByProductIdAndId(productId, optionId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품 옵션입니다."));
+
+        return ProductOptionInfo.from(productOption);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductOptionInfo> getAllProductOptions(Long productId) {
+        var productOptions = productOptionRepository.findByProductId(productId);
+
+        var response = productOptions.stream()
+                .map(ProductOptionInfo::from)
+                .toList();
+        return response;
     }
 }
