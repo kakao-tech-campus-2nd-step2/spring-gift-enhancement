7a7a5fcaf504dd7dffbb7476d477794a67651d8d
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-07-19 14:20:25 +0900

    Feat: optionService 작성

--- src/main/java/gift/Service/OptionService.java
+++ src/main/java/gift/Service/OptionService.java
@@ -1,5 +1,42 @@
 package gift.Service;
 
+import gift.Model.Option;
+import gift.Model.Product;
+import gift.Repository.OptionRepository;
+import gift.Repository.ProductRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OptionService {
+    private final OptionRepository optionRepository;
+    private final ProductRepository productRepository;
+
+    public OptionService(OptionRepository optionRepository, ProductRepository productRepository){
+        this.optionRepository = optionRepository;
+        this.productRepository = productRepository;
+    }
+
+    public List<Option> getAllOptions(Long productId){
+        return optionRepository.findAllById(productId);
+    }
+
+    public Option addOption(Option option, Long productId){
+        Product product = productRepository.findProductById(productId);
+        Option newOption = new Option(option.getId(), product, option.getName(), option.getQuantity());
+        return optionRepository.save(newOption);
+    }
+
+    public Option updateOption(Option option, Long productId, Long optionId){
+        Product product = productRepository.findProductById(productId);
+        Option newOption = new Option(optionId, product, option.getName(), option.getQuantity());
+        return optionRepository.save(newOption);
+    }
+
+    public Option deleteOption(Long productId, Long optionId){
+        Option deletedOption = optionRepository.findOptionById(optionId);
+        optionRepository.deleteById(optionId);
+        return deletedOption;
+    }
 
 }
