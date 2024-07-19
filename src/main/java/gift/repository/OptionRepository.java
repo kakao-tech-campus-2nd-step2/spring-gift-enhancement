7e966682aea3d206b8720e599e718dc184419f51
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-19 16:09:25 +0900

    feat : OptionRepository 생성

---
+++ src/main/java/gift/repository/OptionRepository.java
@@ -0,0 +1,10 @@
+package gift.repository;
+
+import gift.model.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OptionRepository extends JpaRepository<Option, Integer> {
+
+}
