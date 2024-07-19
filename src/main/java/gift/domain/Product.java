d9fa4ba097eaa72386f9070dc5bce49ce4689cbf
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-20 01:31:33 +0900

    feat: product-list.html 수정

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -4,6 +4,7 @@ import gift.dto.request.ProductRequest;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -77,4 +78,8 @@ public class Product {
     }
 
     public Category getCategory() { return category; }
+
+    public List<Option> getOptions() {
+        return options;
+    }
 }
