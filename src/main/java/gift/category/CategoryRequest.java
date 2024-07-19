8383e9b48f389e31988fb14df31987845caaf11b
doyooon <74677268+doyooon@users.noreply.github.com>
2024-07-20 00:21:47 +0900

    경북대 BE_권도윤 4주차 과제 (1단계) (#223)
    * feat : JPA 코드 옮기기
    
    * docs README.md : 피드백 반영 사항 추가
    
    * refactor Product.java, ProductService.java : 비즈니스 정책에 대한 유효성 검사가 도메인에서 이뤄지도록 변경
    
    * refactor ProductTest.java : 스프링 컨테이너가 띄워지지 않는 환경으로 단위테스트 작성
    
    * refactor WishController : 메서드 반환 타입을 구체적으로 작성
    
    * feat Wish.java : 지연 로딩으로 fetch 변경
    
    * feat WishListResponse, WishPageResponse, WishResponse : 직렬화 문제를 해결하기 위해 DTO 추가
    
    * refactor WishController, WishService.java : DTO를 반환하도록 리팩토링
    
    * refactor WishServiceTest.java : 반환 타입 수정
    
    * docs README.md : 상품 카테고리 기능 요구 사항 추가
    
    * docs README.md : 상품 카테고리 요구사항 추가
    
    * feat Category : 상품 카테고리 구현
    
    * feat Product : 도메인에 상품 카테고리 속성 추가
    
    * feat CategoryControllerTest, CategoryServiceTest : CategoryTest 작성

---
+++ src/main/java/gift/category/CategoryRequest.java
@@ -0,0 +1,63 @@
+package gift.category;
+
+public class CategoryRequest {
+    private Long id;
+
+    private String name;
+
+    private String color;
+
+    private String imageUrl;
+
+    private String description;
+
+    protected CategoryRequest(){
+    }
+
+    public CategoryRequest(String name, String color, String imageUrl, String description) {
+        this.name = name;
+        this.color = color;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category toEntity(){
+        return new Category(this.name, this.color, this.imageUrl, this.description);
+    }
+}
