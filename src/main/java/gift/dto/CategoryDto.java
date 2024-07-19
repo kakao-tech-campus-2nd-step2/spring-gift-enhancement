e2e962acba2322985549198eb538c7636fda488b
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-19 15:41:59 +0900

    refactor: Category NotNull 추가

--- src/main/java/gift/dto/CategoryDto.java
+++ src/main/java/gift/dto/CategoryDto.java
@@ -1,12 +1,20 @@
 package gift.dto;
 
 import gift.vo.Category;
+import jakarta.validation.constraints.NotEmpty;
 
 public record CategoryDto (
         Long id,
+        
+        @NotEmpty(message = "카테고리명을 입력해 주세요")
         String name,
+
+        @NotEmpty(message = "색상을 입력해주세요")
         String color,
+
+        @NotEmpty(message = "이미지 url을 입력해 주세요")
         String imageUrl,
+
         String description
 ) {
     public Category toCategory() {
