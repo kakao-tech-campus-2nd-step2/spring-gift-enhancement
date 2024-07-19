0c95e821f01be60abd32a5e41427b005f3508cfc
Kangboom <jkl6147@naver.com>
2024-07-19 17:48:36 +0900

    feat: Add bidirectional relationships with CascadeType.REMOVE

--- src/main/java/gift/domain/category/entity/Category.java
+++ src/main/java/gift/domain/category/entity/Category.java
@@ -1,10 +1,14 @@
 package gift.domain.category.entity;
 
+import gift.domain.product.entity.Product;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -24,6 +28,9 @@ public class Category {
 
     @Column
     private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Product> products;
 
     protected Category() {
     }
--- src/main/java/gift/domain/product/entity/Product.java
+++ src/main/java/gift/domain/product/entity/Product.java
@@ -3,13 +3,17 @@ package gift.domain.product.entity;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import gift.domain.category.entity.Category;
+import gift.domain.option.entity.Option;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "product")
@@ -31,6 +35,9 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Option> options;
 
     protected Product() {
     }
