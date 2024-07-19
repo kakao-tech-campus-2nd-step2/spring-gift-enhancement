6e23c235052cef9222e2863646fbfee0cfb09a1d
sanghee0820 <lsh277604@gmail.com>
2024-07-19 14:51:05 +0900

    feat: Option Domain 추가

---
+++ src/main/java/gift/product/domain/ProductOption.java
@@ -0,0 +1,45 @@
+package gift.product.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ProductOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    protected ProductOption() {
+    }
+
+    public ProductOption(String name, Integer quantity, Product product) {
+        this.name = name;
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductOption{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", product=" + product +
+                '}';
+    }
+}
