97f80088a705a3fe85699741dd8e14c65d078c55
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-19 14:17:25 +0900

    feat : subtractOption 추가

--- src/main/java/gift/repository/option/OptionRepository.java
+++ src/main/java/gift/repository/option/OptionRepository.java
@@ -2,11 +2,13 @@ package gift.repository.option;
 
 import gift.model.option.Option;
 import gift.model.product.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
     boolean existsByProductAndName(Product product, String name);
+    Optional<Option> findByProductAndId(Product product, Long optionId);
 
 }
