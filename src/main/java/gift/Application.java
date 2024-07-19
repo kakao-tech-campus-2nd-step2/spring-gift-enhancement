09da98fdf2dd377b4ea008686fac98275af77c7c
ez23re <ez23re@gmail.com>
2024-07-19 10:12:04 +0900

    수정중

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,12 +2,8 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "gift.product.repository")
-@EntityScan(basePackages = "gift.product.entity")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
