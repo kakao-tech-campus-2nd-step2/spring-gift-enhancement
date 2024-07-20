eb0a090c55b2b0024520b665eb57f0a03fdcf65f
나제법 <skwpqjq@gmail.com>
2024-07-20 17:56:58 +0900

    feat: CreateProductRequest.java 상품 옵션 필드 추가

--- src/main/java/gift/web/dto/request/product/CreateProductRequest.java
+++ src/main/java/gift/web/dto/request/product/CreateProductRequest.java
@@ -2,11 +2,17 @@ package gift.web.dto.request.product;
 
 import gift.domain.Category;
 import gift.domain.Product;
+import gift.domain.ProductOption;
+import gift.web.dto.request.productoption.CreateProductOptionRequest;
 import gift.web.validation.constraints.RequiredKakaoApproval;
 import gift.web.validation.constraints.SpecialCharacter;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
@@ -26,11 +32,16 @@ public class CreateProductRequest {
     @NotNull
     private final Long categoryId;
 
-    public CreateProductRequest(String name, Integer price, URL imageUrl, Long categoryId) {
+    @Valid
+    @NotEmpty
+    private final List<CreateProductOptionRequest> productOptions;
+
+    public CreateProductRequest(String name, Integer price, URL imageUrl, Long categoryId, List<CreateProductOptionRequest> productOptions) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.categoryId = categoryId;
+        this.productOptions = productOptions;
     }
 
     public String getName() {
@@ -49,12 +60,21 @@ public class CreateProductRequest {
         return categoryId;
     }
 
+    public List<CreateProductOptionRequest> getProductOptions() {
+        return Collections.unmodifiableList(productOptions);
+    }
+
     public Product toEntity(Category category) {
+        List<ProductOption> productOptions = this.productOptions.stream()
+            .map(CreateProductOptionRequest::toEntity)
+            .toList();
+
         return new Product.Builder()
             .name(this.name)
             .price(this.price)
             .imageUrl(this.imageUrl)
             .category(category)
+            .productOptions(productOptions)
             .build();
     }
 }
