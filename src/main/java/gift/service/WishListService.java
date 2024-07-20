ac99e6e723259ce433c6301aa531b62bd80ccbd1
UiChan Lee <junglkls1234@gmail.com>
2024-07-20 16:16:15 +0900

    Refactor: WishList 호출 수정

--- src/main/java/gift/service/WishListService.java
+++ src/main/java/gift/service/WishListService.java
@@ -1,22 +1,35 @@
 package gift.service;
 
+import gift.dto.MemberDto;
 import gift.dto.WishDto;
+import gift.model.member.Member;
 import gift.model.wish.Wish;
+import gift.repository.MemberRepository;
 import gift.repository.WishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class WishListService {
     private final WishRepository wishRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     public WishListService(WishRepository wishRepository){
         this.wishRepository = wishRepository;
     }
 
-    public Page<Wish> getAllWishes(Pageable pageable) {
-        return wishRepository.findAll(pageable);
+    public Page<Wish> getAllWishes(MemberDto memberDto, Pageable pageable) {
+        Optional<Member> member = memberRepository.findByEmail(memberDto.email());
+        if(!member.isPresent()){
+            throw new RuntimeException("error");
+        }
+        return wishRepository.findAllByMemberId(member.get().getId(), pageable);
     }
 
     public void insertWish(WishDto wishDto) {
