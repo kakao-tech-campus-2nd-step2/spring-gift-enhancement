7b0df23dc63312b59765e5829b3284f62f913adf
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-21 02:48:18 +0900

    test : Domain Test 추가

--- src/test/java/gift/domain/ProductTest.java
+++ src/test/java/gift/domain/ProductTest.java
@@ -1,26 +1,30 @@
 package gift.domain;
 
+import gift.common.exception.AlreadyExistName;
 import gift.model.category.Category;
+import gift.model.option.Option;
 import gift.model.product.Product;
 import gift.model.product.ProductRequest;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 public class ProductTest {
     @Test
     @DisplayName("상품 객체 생성 테스트")
     void create() {
         // given
         Category category = createCategory("카테고리", "blue", "image", "description");
+        Option option = createOption("Option", 100, null);
         String name = "아메리카노";
         int price = 4500;
         String imageUrl = "americano";
 
         // when
-        Product product = createProduct(name, price, imageUrl, category);
+        Product product = createProduct(name, price, imageUrl, category, List.of(option));
+
 
         // then
         Assertions.assertThat(product.getName()).isEqualTo(name);
@@ -32,7 +36,8 @@ public class ProductTest {
     void updateProduct() {
         // given
         Category category = createCategory("카테고리", "blue", "image", "description");
-        Product product = createProduct("아메리카노", 4500, "americano", category);
+        Option option = createOption("Option", 100, null);
+        Product product = createProduct("아메리카노", 4500, "americano", category, List.of(option));
         ProductRequest request = new ProductRequest("카페라떼", 5000, "caffelatte", category.getId());
 
         // when
@@ -43,13 +48,30 @@ public class ProductTest {
         Assertions.assertThat(product.getImageUrl()).isEqualTo(request.imageUrl());
     }
 
+    @DisplayName("상품에 옵션 추가 시 이름 중복 검증 테스트")
+    @Test
+    void validOptionName() {
+        // given
+        Category category = createCategory("카테고리", "blue", "image", "description");
+        Option option = createOption("Option", 100, null);
+        Product product = createProduct("Americano", 4500, "americano", category, List.of(option));
 
-    private Product createProduct(String name, int price, String url, Category category) {
-        return new Product(name, price, url, category);
+        // when, then
+        org.junit.jupiter.api.Assertions.assertThrows(AlreadyExistName.class, () ->
+            product.addOption("Option", 100));
+    }
+
+
+    private Product createProduct(String name, int price, String url, Category category, List<Option> options) {
+        return new Product(name, price, url, category, options);
     }
 
     private Category createCategory(String name, String color, String imageUrl, String description) {
         return new Category(name, color, imageUrl, description);
+    }
+
+    private Option createOption(String name, int quantity, Product product) {
+        return new Option(name, quantity, product);
     }
 
 }
