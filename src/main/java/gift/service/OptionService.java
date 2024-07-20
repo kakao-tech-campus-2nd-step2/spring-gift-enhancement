7e5ac99c6f587dc14d858a5eac6eab621c7ee8f6
GamJaDo <kim0615157@gmail.com>
2024-07-21 01:00:41 +0900

    chore (id를 productId로 명시적으로 수정)

--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -1,7 +1,7 @@
 package gift.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -28,16 +28,16 @@ public class OptionService {
 		this.productRepository = productRepository;
 	}
 
-	public List<OptionResponse> getOptions(long id) {
-		List<Option> options = optionRepository.findByProductId(id);
+	public List<OptionResponse> getOptions(Long productId) {
+		List<Option> options = optionRepository.findByProductId(productId);
 		return toOptionResponses(options);
 	}
 	
-	public void addOption(long id, OptionRequest request, BindingResult bindingResult) {
+	public void addOption(Long productId, OptionRequest request, BindingResult bindingResult) {
 		validateBindingResult(bindingResult);
 		
-		Product product = findProductById(id);
-		validateDuplicateOption(id, request.getName());
+		Product product = findProductById(productId);
+		validateDuplicateOption(productId, request.getName());
 		
 		Option option = request.toEntity(product);
 		optionRepository.save(option);
@@ -52,22 +52,19 @@ public class OptionService {
         }
     }
 
-	private Product findProductById(long id) {
-    	return productRepository.findById(id)
+	private Product findProductById(Long productId) {
+    	return productRepository.findById(productId)
     			.orElseThrow(() -> new InvalidProductException("Product not found"));
     }
 
-	private List<OptionResponse> toOptionResponses(List<Option> optinos) {
-		List<OptionResponse> OptionResponses = new ArrayList<>();
-		for (Option option : optinos) {
-			OptionResponse response = option.toDto();
-			OptionResponses.add(response);
-		}
-		return OptionResponses;
+	private List<OptionResponse> toOptionResponses(List<Option> options) {
+		return options.stream()
+				.map(Option::toDto)
+				.collect(Collectors.toList());
 	}
 	
-	private void validateDuplicateOption(long id, String optionName) {
-		if (optionRepository.findByProductIdAndName(id, optionName).isPresent()) {
+	private void validateDuplicateOption(Long productId, String optionName) {
+		if (optionRepository.findByProductIdAndName(productId, optionName).isPresent()) {
 			throw new DuplicateOptionException("Options with the same name cannot appear within a product.");
 		}
 	}
