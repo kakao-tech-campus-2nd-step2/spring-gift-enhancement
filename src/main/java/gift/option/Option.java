5dd0faf8cda10bab41a23a72d63b37a4b6bfda32
SeoYeongU <westzeroright@gmail.com>
2024-07-20 17:43:34 +0900

    feat: 상품에 옵션 추가 기능
    - 상품에 옵션 추가
    - 상품의 옵션 조회
    - 옵션 정보 수정
    - 옵션 삭제
    - 옵션에 해당하는 상품 조회

--- src/main/java/gift/option/Option.java
+++ src/main/java/gift/option/Option.java
@@ -1,11 +1,16 @@
 package gift.option;
 
+import gift.product.Product;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "option")
@@ -21,12 +26,17 @@ public class Option {
     @Column(name = "quantity", nullable = false)
     private int quantity;
 
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Product product;
+
     protected Option() {
     }
 
-    public Option(String name, int quantity) {
+    public Option(String name, int quantity, Product product) {
         this.name = name;
         this.quantity = quantity;
+        this.product = product;
     }
 
     public Long getId() {
@@ -39,6 +49,15 @@ public class Option {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void update(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
     }
 
 }
