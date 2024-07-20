4b01def8ed876b343149af5a24f7a20ac29387f0
Kangboom <jkl6147@naver.com>
2024-07-20 15:04:12 +0900

    refactor: update fetch strategy for associations

--- src/main/java/gift/domain/product/entity/Product.java
+++ src/main/java/gift/domain/product/entity/Product.java
@@ -3,17 +3,14 @@ package gift.domain.product.entity;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import gift.domain.category.entity.Category;
-import gift.domain.option.entity.Option;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table(name = "product")
@@ -32,7 +29,7 @@ public class Product {
     @Column
     private String imageUrl;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
 
