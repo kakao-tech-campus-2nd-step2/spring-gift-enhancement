2059bca57f265a21303ef61d4c3dcd1b2907f1c1
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-21 02:58:38 +0900

    feat : ProductService 수정 상품이 생성될 때 옵션을 자동으로 생성 상품이 삭제될 때 옵션을 자동으로 삭제

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,10 +1,13 @@
 package gift.service;
 
 import gift.model.Category;
+import gift.model.Option;
 import gift.model.Product;
 import gift.dto.ProductDTO;
+import gift.repository.OptionRepository;
 import gift.repository.ProductRepository;
 import gift.repository.WishlistRepository;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,12 +19,14 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final WishlistRepository wishlistRepository;
+    private final OptionRepository optionRepository;
     private final CategoryService categoryService;
 
     public ProductService(ProductRepository productRepository,
-        WishlistRepository wishlistRepository, CategoryService categoryService) {
+        WishlistRepository wishlistRepository, OptionRepository optionRepository ,CategoryService categoryService) {
         this.productRepository = productRepository;
         this.wishlistRepository = wishlistRepository;
+        this.optionRepository = optionRepository;
         this.categoryService = categoryService;
     }
 
@@ -36,7 +41,10 @@ public class ProductService {
     @Transactional
     public void saveProduct(ProductDTO productDTO) {
         Category category = categoryService.findCategoryById(productDTO.categoryId());
-        productRepository.save(toEntity(productDTO, null, category));
+        Product product = toEntity(productDTO, null, category);
+        productRepository.save(product);
+        Option temporaryOption = new Option(null, "임시옵션", 1L, product);
+        optionRepository.save(temporaryOption);
     }
 
     @Transactional
@@ -53,9 +61,11 @@ public class ProductService {
     }
 
     @Transactional
-    public void deleteProductAndWishlist(Long id) {
+    public void deleteProductAndWishlistAndOptions(Long id) {
         Product product = productRepository.findById(id).orElse(null);
         wishlistRepository.deleteByProduct(product);
+        List<Option> options = optionRepository.findAllByProductId(id);
+        optionRepository.deleteAll(options);
         productRepository.delete(product);
     }
 
