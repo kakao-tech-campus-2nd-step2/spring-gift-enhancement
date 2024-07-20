08a53f2a95adbf6aaf7bf1fdea92cc4570d4f496
Kangboom <jkl6147@naver.com>
2024-07-20 16:44:08 +0900

    feat: add custom exception for Option valid

---
+++ src/main/java/gift/domain/option/exception/OptionNameLengthException.java
@@ -0,0 +1,7 @@
+package gift.domain.option.exception;
+
+public class OptionNameLengthException extends OptionValidException{
+    public OptionNameLengthException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/gift/domain/option/exception/OptionQuantityRangeException.java
@@ -0,0 +1,7 @@
+package gift.domain.option.exception;
+
+public class OptionQuantityRangeException extends OptionValidException{
+    public OptionQuantityRangeException(String message) {
+        super(message);
+    }
+}
