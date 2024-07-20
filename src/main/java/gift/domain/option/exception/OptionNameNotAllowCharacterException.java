c9d347f2b691815992262b8315173a50ac5c933a
Kangboom <jkl6147@naver.com>
2024-07-20 23:43:37 +0900

    feat: add allowed character exception

---
+++ src/main/java/gift/domain/option/exception/OptionNameNotAllowCharacterException.java
@@ -0,0 +1,7 @@
+package gift.domain.option.exception;
+
+public class OptionNameNotAllowCharacterException extends OptionValidException{
+    public OptionNameNotAllowCharacterException(String message) {
+        super(message);
+    }
+}
