7c19a0d555e667f2e11a12c1fbb80cc28d032654
GamJaDo <kim0615157@gmail.com>
2024-07-21 01:00:17 +0900

    chore (entity에 불필요한 어노테이션 제거)

--- src/main/java/gift/entity/Option.java
+++ src/main/java/gift/entity/Option.java
@@ -9,11 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "options")
@@ -23,13 +18,9 @@ public class Option {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank(message = "옵션 이름은 필수로 입력해야 합니다.")
-    @Size(max = 50, message = "옵션 이름은 최대 50자까지 입력 가능합니다.")
-	@Pattern(regexp = "^[a-zA-Z0-9가-힣()\\[\\]+\\-&/_ ]*$", message = "허용되지 않는 특수 문자가 들어가 있습니다.")
+	@Column(nullable = false, length = 50)
 	private String name;
-	
-	@Min(value = 1, message = "옵션 수량은 최소 1개 이상이어야 합니다.")
-	@Max(value = 99999999, message = "옵션 수량은 1억 개 미만이어야 합니다.")
+
 	@Column(nullable = false)
 	private int quantity;
 	
