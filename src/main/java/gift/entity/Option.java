47439d132137c512211918df81f73702c5eb6d4c
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-21 01:49:33 +0900

    feat: Option 엔티티의 product_id와 name 항목 복합키로 설정

--- src/main/java/gift/entity/Option.java
+++ src/main/java/gift/entity/Option.java
@@ -8,23 +8,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "option")
+@Table(name = "option", uniqueConstraints = {@UniqueConstraint(columnNames = {"product_id", "name"})})
 public class Option {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
     private Long quantity;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
     protected Option() {}
