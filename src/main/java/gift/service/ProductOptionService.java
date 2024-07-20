595f821dfc601440e05ca84978e2e9a45e69d05a
나제법 <skwpqjq@gmail.com>
2024-07-20 14:54:06 +0900

    feat: ProductOptionService 옵션 전체 조회, 옵션 수정 기능 구현

--- src/main/java/gift/service/ProductOptionService.java
+++ src/main/java/gift/service/ProductOptionService.java
@@ -3,9 +3,11 @@ package gift.service;
 import gift.domain.ProductOption;
 import gift.repository.ProductOptionRepository;
 import gift.web.dto.request.productoption.CreateProductOptionRequest;
+import gift.web.dto.request.productoption.UpdateProductOptionRequest;
 import gift.web.dto.response.productoption.CreateProductOptionResponse;
 import gift.web.dto.response.productoption.ReadAllProductOptionsResponse;
 import gift.web.dto.response.productoption.ReadProductOptionResponse;
+import gift.web.dto.response.productoption.UpdateProductOptionResponse;
 import gift.web.validation.exception.client.AlreadyExistsException;
 import gift.web.validation.exception.client.ResourceNotFoundException;
 import java.util.List;
@@ -25,24 +27,11 @@ public class ProductOptionService {
     @Transactional
     public CreateProductOptionResponse createOption(Long productId, CreateProductOptionRequest request) {
         String optionName = request.getName();
-        validateOptionNameExists(productId, optionName);
+        validateOptionNameExists(null, productId, optionName);
 
         ProductOption createdOption = productOptionRepository.save(request.toEntity(productId));
 
         return CreateProductOptionResponse.fromEntity(createdOption);
-    }
-
-    /**
-     * 상품 옵션 이름이 이미 존재하는지 확인합니다.<br>
-     * 이미 존재한다면 BadRequestException을 발생시킵니다.
-     * @param productId 상품 ID
-     * @param name 상품 옵션 이름
-     */
-    private void validateOptionNameExists(Long productId, String name) {
-        boolean isExistsOptionName = productOptionRepository.existsByNameAndProductId(name, productId);
-        if (isExistsOptionName) {
-            throw new AlreadyExistsException(name);
-        }
     }
 
     public ReadAllProductOptionsResponse readAllOptions(Long productId) {
@@ -54,9 +43,42 @@ public class ProductOptionService {
     }
 
     @Transactional
+    public UpdateProductOptionResponse updateOption(Long optionId, Long productId, UpdateProductOptionRequest request) {
+        String optionName = request.getName();
+        validateOptionNameExists(optionId, productId, optionName);
+
+        ProductOption option = productOptionRepository.findById(optionId)
+            .orElseThrow(() -> new ResourceNotFoundException("상품 옵션", optionId.toString()));
+
+        ProductOption updateParam = request.toEntity();
+        option.update(updateParam);
+
+        return UpdateProductOptionResponse.fromEntity(option);
+    }
+
+    /**
+     * 상품 옵션 이름이 이미 존재하는지 확인합니다.<br>
+     * 이미 존재한다면 {@link AlreadyExistsException} 을 발생시킵니다.
+     * @param optionId 현재 상품 옵션 아이디 (옵션 새로 생성 시 null)
+     * @param productId 상품 아이디
+     * @param name 상품 옵션 이름
+     */
+    private void validateOptionNameExists(Long optionId, Long productId, String name) {
+        if(optionId == null) {
+            optionId = -1L;
+        }
+        productOptionRepository.findDuplicatedProductOption(optionId, productId, name)
+            .ifPresent(duplicatedOptionId -> {
+                System.out.println("duplicatedOptionId = " + duplicatedOptionId);
+                throw new AlreadyExistsException(name);
+            });
+    }
+
+    @Transactional
     public void deleteOption(Long optionId) {
         ProductOption option = productOptionRepository.findById(optionId)
             .orElseThrow(() -> new ResourceNotFoundException("상품 옵션", optionId.toString()));
+
         productOptionRepository.delete(option);
     }
 }
