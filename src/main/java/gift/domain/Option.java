6992e08743f5d4caf917df1043abda972f2a8c89
BaeJunho <bjh4498@knu.ac.kr>
2024-07-20 19:28:12 +0900

    refactor : 연관 관계 매핑 방식 변경 ( 양방향 -> 단방향 일대다 )

--- src/main/java/gift/domain/Option.java
+++ src/main/java/gift/domain/Option.java
@@ -9,21 +9,14 @@ public class Option extends BaseEntity {
     private String name;
     @Column(name="quantity", nullable = false)
     private Long quantity;
-    @ManyToOne
-    @JoinColumn(
-            name = "product_id",
-            foreignKey = @ForeignKey(name = "fk_option_product_id_ref_product_id"),
-            nullable = false)
-    Product product;
 
     protected Option() {
         super();
     }
 
-    public Option(String name, Long quantity, Product product) {
+    public Option(String name, Long quantity) {
         this.name = name;
         this.quantity = quantity;
-        this.product = product;
     }
 
     public String getName() {
