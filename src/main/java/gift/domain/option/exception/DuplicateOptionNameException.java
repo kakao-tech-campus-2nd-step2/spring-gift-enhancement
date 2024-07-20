a27d63f1db3d81e11bea20d1059902eb4756b66a
Kangboom <jkl6147@naver.com>
2024-07-20 16:45:42 +0900

    refactor: rename file

--- src/main/java/gift/domain/option/exception/DuplicateOptionNameException.java
+++
@@ -1,8 +0,0 @@
-package gift.domain.option.exception;
-
-public class DuplicateOptionNameException extends RuntimeException {
-
-    public DuplicateOptionNameException(String message) {
-        super(message);
-    }
-}
