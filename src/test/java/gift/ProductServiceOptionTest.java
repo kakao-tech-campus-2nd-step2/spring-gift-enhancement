fbdecb5ce3931aa9e0a314036d8eab73c25b679f
hyunseo <wndlthsk@naver.com>
2024-07-20 22:19:15 +0900

    refactor : option test 수정

--- src/test/java/gift/ProductServiceOptionTest.java
+++ src/test/java/gift/ProductServiceOptionTest.java
@@ -4,11 +4,8 @@ import gift.domain.Category;
 import gift.domain.Option;
 import gift.domain.Product;
 import gift.dto.OptionDto;
-import gift.repository.CategoryRepository;
-import gift.repository.OptionRepository;
 import gift.repository.ProductRepository;
-import gift.service.OptionService;
-import java.util.ArrayList;
+import gift.service.ProductService;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -18,44 +15,34 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class OptionServiceTest {
+public class ProductServiceOptionTest {
     @Mock
     private ProductRepository productRepository;
 
-    @Mock
-    private CategoryRepository categoryRepository;
-
-    @Mock
-    private OptionRepository optionRepository;
-
     @InjectMocks
-    private OptionService optionService;
+    private ProductService productService;
 
-    private Category category;
     private Product product;
 
     @BeforeEach
     public void setUp() {
-        category = new Category("category", "color", "image", "");
+        Category category = new Category("category", "color", "image", "");
         product = new Product(5L, "Test Product", 100, "image.jpg", category);
     }
 
     @Test
     public void testGetOptionsByProductId() {
         Option option = new Option("Test Option", 10, product);
-        List<Option> options = new ArrayList<>();
-        options.add(option);
-        when(optionRepository.findAllByProductId(product.getId())).thenReturn(options);
+        product.addOption(option);
+        given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
 
-        List<OptionDto> optionDtoList = optionService.getOptionsByProductId(product.getId());
+        List<OptionDto> optionDtoList = productService.getOptionsByProductId(product.getId());
 
         assertEquals(1, optionDtoList.size());
         assertEquals("Test Option", optionDtoList.get(0).getName());
@@ -65,10 +52,10 @@ public class OptionServiceTest {
     @Test
     public void testSaveOption_ProductNotFound() {
         OptionDto optionDto = new OptionDto(1L, "Test Option", 100);
-        when(productRepository.findById(product.getId())).thenReturn(Optional.empty());
+        given(productRepository.findById(product.getId())).willReturn(Optional.empty());
 
         assertThatExceptionOfType(NoSuchElementException.class)
-            .isThrownBy(()->optionService.saveOption(product.getId(), optionDto));
+            .isThrownBy(()->productService.saveOption(product.getId(), optionDto));
     }
 
     @Test
@@ -76,13 +63,12 @@ public class OptionServiceTest {
         given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
 
         Option existingOption = new Option("Test Option", 5, product);
-        List<Option> options = new ArrayList<>();
-        options.add(existingOption);
-        given(optionRepository.findAllByProductId(product.getId())).willReturn(options);
+        product.addOption(existingOption);
+        given(productRepository.findById(product.getId())).willReturn(Optional.of(product)); // option이 들어간 product 리턴
 
         OptionDto optionDto = new OptionDto("Test Option", 10);
 
         assertThatExceptionOfType(IllegalStateException.class)
-            .isThrownBy(()->optionService.saveOption(product.getId(), optionDto));
+            .isThrownBy(()->productService.saveOption(product.getId(), optionDto));
     }
 }
