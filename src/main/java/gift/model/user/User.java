f014479e39ed0295be45a1b4c8b3c76ca1dc8387
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-21 02:48:54 +0900

    refactor : 불필요한 setter 메서드 삭제

--- src/main/java/gift/model/user/User.java
+++ src/main/java/gift/model/user/User.java
@@ -41,40 +41,20 @@ public class User {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getRole() {
         return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 
     // 비밀번호 검증 메서드
