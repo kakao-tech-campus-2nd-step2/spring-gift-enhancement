2aaeb44808f623704fc1e493c5fd5baea5e9bb79
UiChan Lee <junglkls1234@gmail.com>
2024-07-20 18:45:27 +0900

    Refactor: Wish 예외처리 변경

--- src/main/java/gift/service/WishListService.java
+++ src/main/java/gift/service/WishListService.java
@@ -2,11 +2,11 @@ package gift.service;
 
 import gift.dto.MemberDto;
 import gift.dto.WishDto;
+import gift.exception.ValueNotFoundException;
 import gift.model.member.Member;
 import gift.model.wish.Wish;
 import gift.repository.MemberRepository;
 import gift.repository.WishRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,18 +16,17 @@ import java.util.Optional;
 @Service
 public class WishListService {
     private final WishRepository wishRepository;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    public WishListService(WishRepository wishRepository){
+    public WishListService(WishRepository wishRepository, MemberRepository memberRepository){
         this.wishRepository = wishRepository;
+        this.memberRepository = memberRepository;
     }
 
     public Page<Wish> getAllWishes(MemberDto memberDto, Pageable pageable) {
         Optional<Member> member = memberRepository.findByEmail(memberDto.email());
-        if(!member.isPresent()){
-            throw new RuntimeException("error");
+        if(member.isEmpty()){
+            throw new ValueNotFoundException("Member not exists in Database");
         }
         return wishRepository.findAllByMemberId(member.get().getId(), pageable);
     }
