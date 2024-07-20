80edcc903e901ec71d5a450374e9328f4874f184
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-20 14:54:54 +0900

    refactor: uri 복수형으로 변경

--- src/main/java/gift/category/controller/CategoryAdminController.java
+++ src/main/java/gift/category/controller/CategoryAdminController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin/category")
+@RequestMapping("/api/admin/categories")
 public class CategoryAdminController {
     private final UserService userService;
     private final CategoryService categoryService;
