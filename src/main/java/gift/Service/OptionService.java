a5c1d2f667fe2aff270e033abf0b82a7ae9cb593
Woojae Jeong <woo6388@naver.com>
2024-07-20 22:26:30 +0900

    refactor(OptionService) :  hasSameName(), isBelongToProduct() 메소드 적용

--- src/main/java/gift/Service/OptionService.java
+++ src/main/java/gift/Service/OptionService.java
@@ -47,9 +47,9 @@ public class OptionService {
     @Transactional
     public void editOption(Long productId, Long optionId, RequestOption requestOption) {
         Option option = optionRepository.findById(optionId).orElseThrow(()-> new NoSuchElementException("매칭되는 옵션이 없습니다"));
-        if (!(option.getProduct().getId().equals(productId)))
-            throw new NoSuchElementException("해당 product에는 해당 옵션을 가지고 있지 않습니다");
-        if(!(option.getName().equals(requestOption.name()))) //옵션의 name을 변경하려고 한다면 이미 존재하는 name이 있는지 체크
+        if (!(option.isBelongToProduct(productId)))
+            throw new NoSuchElementException("해당 옵션은 해당 상품에 속하지 않는 옵션입니다");
+        if(!(option.hasSameName(requestOption.name()))) //옵션의 name을 변경하려고 한다면 이미 존재하는 name이 있는지 체크
             optionKeeperService.checkUniqueOptionName(option.getProduct(), requestOption.name());
         option.update(requestOption.name(), requestOption.quantity());
     }
@@ -57,8 +57,8 @@ public class OptionService {
     @Transactional
     public void deleteOption(Long productId, Long optionId) {
         Option option = optionRepository.findById(optionId).orElseThrow(() -> new NoSuchElementException("매칭되는 옵션이 없습니다"));
-        if (!(option.getProduct().getId().equals(productId)))
-            throw new NoSuchElementException("해당 product에는 해당 옵션을 가지고 있지 않습니다");
+        if (!(option.isBelongToProduct(productId)))
+            throw new NoSuchElementException("해당 옵션은 해당 상품에 속하지 않는 옵션입니다");
         optionKeeperService.checkHasAtLeastOneOption(option.getProduct());
         optionRepository.delete(option);
     }
