ca637cb6941bee054e5157e7cabe178c2b10a9e0
Kangboom <jkl6147@naver.com>
2024-07-21 00:20:44 +0900

    refactor: change to record class

--- src/main/java/gift/domain/member/dto/MemberResponse.java
+++ src/main/java/gift/domain/member/dto/MemberResponse.java
@@ -1,23 +1,4 @@
 package gift.domain.member.dto;
 
-public class MemberResponse {
-
-    String email;
-    String password;
-
-    public MemberResponse() {
-    }
-
-    public MemberResponse(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+public record MemberResponse(String email, String password) {
 }
