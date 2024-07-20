71de7ac90c40534c954b3ec236a2bd708de240d3
Kangboom <jkl6147@naver.com>
2024-07-20 17:26:53 +0900

    test: add option_length name and quantity_range test

--- src/test/java/gift/domain/OptionTest.java
+++ src/test/java/gift/domain/OptionTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import gift.domain.option.entity.Option;
 import gift.domain.option.exception.OptionNameDuplicateException;
+import gift.domain.option.exception.OptionNameLengthException;
+import gift.domain.option.exception.OptionQuantityRangeException;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -29,6 +31,48 @@ public class OptionTest {
         Option newOption = createOption("test3");
 
         assertDoesNotThrow(() -> newOption.checkDuplicateName(optionList));
+    }
+
+    @Test
+    @DisplayName("옵션 이름 길이 초과 검사")
+    void optionNameLengthFailTest(){
+        assertThatThrownBy(() -> {
+            new Option("name".repeat(50), 100);
+        }).isInstanceOf(OptionNameLengthException.class)
+            .hasMessage("옵션 이름 50자 초과");
+    }
+
+    @Test
+    @DisplayName("정상 옵션 이름 길이 검사")
+    void optionNameLengthSuccessTest(){
+        assertDoesNotThrow(()->{
+            new Option("name", 100);
+        });
+    }
+
+    @Test
+    @DisplayName("수량 1억개 이상일 경우 테스트")
+    void optionQuantityExceedTest(){
+        assertThatThrownBy(() -> {
+            new Option("name", 100_000_000);
+        }).isInstanceOf(OptionQuantityRangeException.class)
+            .hasMessage("수량은 1개 이상 1억개 미만으로 설정해주세요.");
+    }
+
+    @Test
+    @DisplayName("수량 1개 미만일 경우 테스트")
+    void optionQuantityLackTest(){
+        assertThatThrownBy(() -> {
+            new Option("name", 0);
+        }).isInstanceOf(OptionQuantityRangeException.class)
+            .hasMessage("수량은 1개 이상 1억개 미만으로 설정해주세요.");
+    }
+    @Test
+    @DisplayName("수량 정상 테스트")
+    void optionQuantitySuccessTest(){
+        assertDoesNotThrow(()->{
+            new Option("name", 100);
+        });
     }
 
     Option createOption() {
