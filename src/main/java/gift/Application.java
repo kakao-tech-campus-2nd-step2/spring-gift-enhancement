59506ddbd7cc030cd84228b10cf60421113b285b
momnpa333 <momnpa333@gmail.com>
2024-07-21 16:01:34 +0900

    refact: enable retry

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -3,10 +3,12 @@ package gift;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRetry
 public class Application {
 
     public static void main(String[] args) {
