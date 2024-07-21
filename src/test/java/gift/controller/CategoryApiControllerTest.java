ff440d1121d54faca1e8583b6985f0cb328e07fb
anacnoda77 <sm1108shin@naver.com>
2024-07-21 15:11:39 +0900

    refactor: e2e 테스트 제외한 모든 테스트에 트랜잭션 애노테이션 삽입

--- src/test/java/gift/controller/CategoryApiControllerTest.java
+++ src/test/java/gift/controller/CategoryApiControllerTest.java
@@ -32,10 +32,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
 class CategoryApiControllerTest {
 
     @Autowired
