48022e2e44a5ba82e0e3b9b1985a34b3f004fae8
us4c0d3 <jwseok312@gmail.com>
2024-07-21 14:02:07 +0900

    feat(Option): 누락된 제약조건 추가

--- src/main/java/gift/product/option/entity/Option.java
+++ src/main/java/gift/product/option/entity/Option.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,9 +26,11 @@ public class Option {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Column(length = 50)
     private String name;
 
+    @NotNull
     @Column
     private Integer quantity;
 
