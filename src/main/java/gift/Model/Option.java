c0b8d7b81d4999da4ebe0c92f2d0545562ab6ac9
Woojae Jeong <woo6388@naver.com>
2024-07-21 19:09:19 +0900

    feat(Option) : Option 도메인 유효성 검사 추가

--- src/main/java/gift/Model/Option.java
+++ src/main/java/gift/Model/Option.java
@@ -2,8 +2,15 @@ package gift.Model;
 
 import jakarta.persistence.*;
 
+import java.util.regex.Pattern;
+
 @Entity
 public class Option {
+
+    private static final Pattern NAME_PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣 ()\\[\\]+\\-\\&/_]*$"
+    );
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
@@ -23,6 +30,30 @@ public class Option {
         this.name = name;
         this.quantity = quantity;
         this.product = product;
+    }
+
+    public void validateName(String name) {
+        if (name.length() > 50)
+            throw new IllegalArgumentException("옵션 이름의 길이는 공백포함 최대 50자 입니다");
+
+        if (name.isBlank() || name == null)
+            throw new IllegalArgumentException("옵션 이름은 필수입니다");
+
+        if (!NAME_PATTERN.matcher(name).matches())
+            throw new IllegalArgumentException("옵션 이름에는 허용된 특수 문자만 포함될 수 있습니다: (), [], +, -, &, /, _");
+
+    }
+
+    public void validateQuantity(int quantity) {
+        if (quantity <= 0)
+            throw new IllegalArgumentException("옵션 수량은 최소 1개 이상이여야 합니다");
+        if (quantity > 9999_9999)
+            throw new IllegalArgumentException("옵션 수량은 최대 1억개 미만이여야 합니다");
+    }
+
+        public void validateProduct(Product product) {
+        if (product == null)
+            throw new IllegalArgumentException("옵션에 product 지정은 필수입니다");
     }
 
     public Long getId() {
