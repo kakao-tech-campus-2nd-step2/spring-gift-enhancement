5be547e240d8a3dbb662935146b7d4aac86b03f4
BaeJunho <bjh4498@knu.ac.kr>
2024-07-21 15:50:00 +0900

    chore : 더 이상 사용하지 않는 import 문 제거

--- src/test/java/gift/service/ProductServiceTest.java
+++ src/test/java/gift/service/ProductServiceTest.java
@@ -17,7 +17,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.*;
-import org.springframework.data.jpa.support.PageableUtils;
 
 import java.util.ArrayList;
 import java.util.List;
