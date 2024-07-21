5c23c33ed4e6c794a8b2997e03bc8d27ee0267ca
안원모 <tommy45231@naver.com>
2024-07-21 16:26:29 +0900

    Feat : Option 기능 구현

---
+++ src/main/java/gift/domain/Option.java
@@ -0,0 +1,100 @@
+package gift.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "option", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"product_id", "name"})
+})
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    @JsonBackReference
+    private Product product;
+
+    protected Option() {
+    }
+
+    private Option(OptionBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.quantity = builder.quantity;
+        this.product = builder.product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public OptionBuilder toBuilder() {
+        return new OptionBuilder()
+            .id(this.id)
+            .name(this.name)
+            .quantity(this.quantity)
+            .product(this.product);
+    }
+
+    public static class OptionBuilder {
+        private Long id;
+        private String name;
+        private int quantity;
+        private Product product;
+
+        public OptionBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public OptionBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public OptionBuilder quantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public OptionBuilder product(Product product) {
+            this.product = product;
+            return this;
+        }
+
+        public Option build() {
+            return new Option(this);
+        }
+    }
+}
