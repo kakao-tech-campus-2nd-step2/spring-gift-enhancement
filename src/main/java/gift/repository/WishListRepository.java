8af32d203aea62b9e13a1cdc671e09cc71022cdb
peacefullyquietly <pjh73015@gmail.com>
2024-07-21 14:41:55 +0900

    refactor : @Repository 어노테이션 추가

--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WishListRepository extends JpaRepository<WishListEntity, Long> {
     Optional<WishListEntity> findByUserEntity_IdAndProductEntity_Id(Long userId, Long productId);
     Page<WishListEntity> findByUserEntity_Id(Long userId, Pageable pageable);
