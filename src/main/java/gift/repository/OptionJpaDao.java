bbb2776675fce4874651d8ddcacca3b5a7d9432c
5win <94297900+5win@users.noreply.github.com>
2024-07-21 19:20:42 +0900

    refact: Remove not used method in OptionJpaDao

--- src/main/java/gift/repository/OptionJpaDao.java
+++ src/main/java/gift/repository/OptionJpaDao.java
@@ -1,13 +1,8 @@
 package gift.repository;
 
 import gift.entity.Option;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OptionJpaDao extends JpaRepository<Option, Long> {
 
-    List<Option> findAllByProduct_Id(Long productId);
-
-    Optional<Option> findByNameAndProduct_Id(String optionName, Long productId);
 }
