16246747952b84e1ce113902cc83fdad45155264
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-21 21:43:37 +0900

    chore: 사용되지않는메서드 삭제

--- src/main/java/gift/option/application/command/OptionUpdateCommand.java
+++ src/main/java/gift/option/application/command/OptionUpdateCommand.java
@@ -1,13 +1,8 @@
 package gift.option.application.command;
 
-import gift.option.domain.Option;
-
 public record OptionUpdateCommand(
         Long id,
         String name,
         Integer quantity
 ) {
-    public Option toOption() {
-        return new Option(name, quantity);
-    }
 }
