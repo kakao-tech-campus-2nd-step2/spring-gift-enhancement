a08241387109e85e4f80e81a5c08ce17c771869e
Woojae Jeong <woo6388@naver.com>
2024-07-21 16:16:55 +0900

    refactor : ProductNotFoundException, OptionNotFoundException 적용

--- src/main/java/gift/Service/OptionService.java
+++ src/main/java/gift/Service/OptionService.java
@@ -1,5 +1,7 @@
 package gift.Service;
 
+import gift.Exception.OptionNotFoundException;
+import gift.Exception.ProductNotFoundException;
 import gift.Model.Option;
 import gift.Model.Product;
 import gift.Model.RequestOption;
@@ -26,7 +28,7 @@ public class OptionService {
 
     @Transactional
     public Option addOption(Long productId, RequestOption requestOption){
-        Product product = productRepository.findById(productId).orElseThrow(()->new NoSuchElementException("매칭되는 product가 없습니다"));
+        Product product = productRepository.findById(productId).orElseThrow(()->new ProductNotFoundException("매칭되는 product가 없습니다"));
         optionKeeperService.checkUniqueOptionName(product, requestOption.name());
         Option option = new Option(requestOption.name(), requestOption.quantity(), product);
         return optionRepository.save(option);
@@ -34,7 +36,7 @@ public class OptionService {
 
     @Transactional(readOnly = true)
     public  List<ResponseOptionDTO> getOptions (Long productId){
-        Product product = productRepository.findById(productId).orElseThrow(()-> new NoSuchElementException("매칭되는 product가 없습니다"));
+        Product product = productRepository.findById(productId).orElseThrow(()-> new ProductNotFoundException("매칭되는 product가 없습니다"));
         return optionRepository.findByProduct(product)
                 .stream()
                 .map(it-> new ResponseOptionDTO(
@@ -46,9 +48,9 @@ public class OptionService {
 
     @Transactional
     public void editOption(Long productId, Long optionId, RequestOption requestOption) {
-        Option option = optionRepository.findById(optionId).orElseThrow(()-> new NoSuchElementException("매칭되는 옵션이 없습니다"));
+        Option option = optionRepository.findById(optionId).orElseThrow(()-> new ProductNotFoundException("매칭되는 옵션이 없습니다"));
         if (!(option.isBelongToProduct(productId)))
-            throw new NoSuchElementException("해당 옵션은 해당 상품에 속하지 않는 옵션입니다");
+            throw new OptionNotFoundException("해당 옵션은 해당 상품에 속하지 않는 옵션입니다");
         if(!(option.hasSameName(requestOption.name()))) //옵션의 name을 변경하려고 한다면 이미 존재하는 name이 있는지 체크
             optionKeeperService.checkUniqueOptionName(option.getProduct(), requestOption.name());
         option.update(requestOption.name(), requestOption.quantity());
@@ -56,9 +58,9 @@ public class OptionService {
 
     @Transactional
     public void deleteOption(Long productId, Long optionId) {
-        Option option = optionRepository.findById(optionId).orElseThrow(() -> new NoSuchElementException("매칭되는 옵션이 없습니다"));
+        Option option = optionRepository.findById(optionId).orElseThrow(() -> new ProductNotFoundException("매칭되는 옵션이 없습니다"));
         if (!(option.isBelongToProduct(productId)))
-            throw new NoSuchElementException("해당 옵션은 해당 상품에 속하지 않는 옵션입니다");
+            throw new OptionNotFoundException("해당 옵션은 해당 상품에 속하지 않는 옵션입니다");
         optionKeeperService.checkHasAtLeastOneOption(option.getProduct());
         optionRepository.delete(option);
     }
