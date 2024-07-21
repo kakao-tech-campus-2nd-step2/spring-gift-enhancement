d8073aa5235db79847611ff5993440b9c0ed85f9
JaeBin <201924576@pusan.ac.kr>
2024-07-21 19:00:11 +0900

    feat(option): add subtract test in option test

--- src/test/java/gift/option/domain/OptionTest.java
+++ src/test/java/gift/option/domain/OptionTest.java
@@ -1,11 +1,14 @@
 package gift.option.domain;
 
+import gift.option.exception.OptionNotEnoughException;
 import gift.product.domain.Product;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OptionTest {
+
     @Test
     void testOptionEqualsAndHashCode_ById() {
         OptionName optionName1 = new OptionName("Name1");
@@ -22,5 +25,29 @@ class OptionTest {
         assertThat(option1).isNotEqualTo(option3);
         assertThat(option1.hashCode()).isEqualTo(option2.hashCode());
         assertThat(option1.hashCode()).isNotEqualTo(option3.hashCode());
+    }
+
+    @Test
+    void testSubtract_Success() {
+        OptionName optionName = new OptionName("Option1");
+        OptionCount optionCount = new OptionCount(10L);
+        Product product = new Product();
+
+        Option option = new Option(1L, optionName, optionCount, product);
+        option.subtract(5);
+
+        assertThat(option.getCount().getOptionCountValue()).isEqualTo(5L);
+    }
+
+    @Test
+    void testSubtract_Failure() {
+        OptionName optionName = new OptionName("Option1");
+        OptionCount optionCount = new OptionCount(10L);
+        Product product = new Product();
+
+        Option option = new Option(1L, optionName, optionCount, product);
+
+        assertThatThrownBy(() -> option.subtract(15))
+                .isInstanceOf(OptionNotEnoughException.class);
     }
 }
