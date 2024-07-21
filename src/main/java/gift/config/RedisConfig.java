d990cf12809c2406baff3957b46bb6d2adb9ccf2
hjinshin <gudwls818@gmail.com>
2024-07-21 23:08:55 +0900

    feat: Redis로 옵션 빼기 기능 테스트

---
+++ src/main/java/gift/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package gift.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient(@Value("${redis.host}") String host,
+                                         @Value("${redis.port}") String port
+    ) {
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        return Redisson.create(config);
+    }
+}
