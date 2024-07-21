9bb21cf1dc3b9c9231b83f0d1953e9fdb9cdd61d
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-21 18:47:41 +0900

    feat: productId로 옵션을 찾기 위한 메서드 추가

--- src/main/java/gift/repository/OptionRepository.java
+++ src/main/java/gift/repository/OptionRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface OptionRepository extends JpaRepository<Option, Long> {
     @Query("SELECT o FROM Option o WHERE o.product.id = :productId")
     Page<Option> findAllByProductId(@Param("productId") Long productId, Pageable pageable);
+
+    Optional<Option> findByProductId(Long productId);
 }
