4c9ab72c1a126d457cea6f48674d9db79b08c4a7
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-21 12:50:07 +0900

    refact: DBMS가 바뀌었을때를 대비해 length명시 안한 컬럼에 명시

--- src/main/java/gift/member/domain/Member.java
+++ src/main/java/gift/member/domain/Member.java
@@ -11,10 +11,10 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 255)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String password;
 
     protected Member() {
