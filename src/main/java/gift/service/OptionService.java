2bb8092e8596e835f04b418d49edf662fb9d14e7
minjae <minjae4650@gmail.com>
2024-07-21 20:55:47 +0900

    feat: 옵션 서비스 인터페이스에 수량 차감 메서드 추가
    - 옵션 수량을 차감하는 subtractOptionQuantity 메서드 정의

--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface OptionService {
     Option addOptionToProduct(Long productId, Option option);
     List<Option> getOptionsByProduct(Long productId);
+    void subtractOptionQuantity(Long optionId, int quantity);
 }
