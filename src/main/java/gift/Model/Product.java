188712642a00308111213137c1d59f5e62c16d1a
Woojae Jeong <woo6388@naver.com>
2024-07-21 18:25:51 +0900

    feat(Product) : Product 도메인 유효성 검사 추가

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -2,8 +2,19 @@ package gift.Model;
 
 import jakarta.persistence.*;
 
+import java.util.regex.Pattern;
+
 @Entity
 public class Product {
+
+    private static final Pattern NAME_PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣 ()\\[\\]+\\-\\&/_]*$"
+    );
+
+    private static final Pattern NAME_EXCLUDE_PATTERN = Pattern.compile(
+            "^(?!.*카카오).*$"
+    );
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
@@ -20,10 +31,45 @@ public class Product {
     protected Product() {}
 
     public Product(String name, int price, String imageUrl, Category category) {
+        validateName(name);
+        validatePrice(price);
+        validateImageUrl(imageUrl);
+        validateCategory(category);
+
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.category = category;
+    }
+
+    public void validateName(String name) {
+        if (name.length() > 15)
+            throw new IllegalArgumentException("상품 이름의 길이는 공백포함 최대 15자 입니다");
+
+
+        if (name.isBlank() || name == null)
+            throw new IllegalArgumentException("상품 이름은 필수입니다");
+
+        if (!NAME_PATTERN.matcher(name).matches())
+            throw new IllegalArgumentException("상품 이름에는 허용된 특수 문자만 포함될 수 있습니다: (), [], +, -, &, /, _");
+
+        if (!NAME_EXCLUDE_PATTERN.matcher(name).matches())
+            throw new IllegalArgumentException("상품 이름에 '카카오'가 포함된 문구는 담당 MD와 협의가 필요합니다.");
+    }
+
+    public void validatePrice(int price){
+        if (price < 0 )
+            throw new IllegalArgumentException("가격은 0원 이상이여야 합니다");
+    }
+
+    public void validateImageUrl(String imageUrl){
+        if (imageUrl == null || imageUrl.isBlank())
+            throw new IllegalArgumentException("imageUrl를 입력해주세요");
+    }
+
+    public void validateCategory(Category category){
+        if (category == null)
+            throw  new IllegalArgumentException("상품에 카테고리 지정은 필수입니다");
     }
 
     public Long getId() {
