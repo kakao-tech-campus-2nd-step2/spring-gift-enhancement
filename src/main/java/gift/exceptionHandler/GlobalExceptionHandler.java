846084cc4c8e7892af51753a399d92d0348d7465
5win <94297900+5win@users.noreply.github.com>
2024-07-21 15:49:52 +0900

    refact: Change to use lsf4j for logging

--- src/main/java/gift/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/gift/exceptionHandler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import gift.constants.ErrorMessage;
 import gift.exception.ProductOptionRequiredException;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     /**
      * 입력 값 유효성 검증 실패 시, 에러 핸들링
      *
@@ -19,7 +23,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> inputValidException(MethodArgumentNotValidException e) {
-        System.out.println(e.getStackTrace()[0]);
+        logger.error(String.valueOf(e.getStackTrace()[0]));
         String errorMsg = Objects.requireNonNull(e.getBindingResult().getFieldError(),
             ErrorMessage.ERROR_FIELD_NOT_EXISTS_MSG).getDefaultMessage();
 
@@ -33,7 +37,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> noSuchElementExceptionHandler(NoSuchElementException e) {
-        System.out.println(e.getStackTrace()[0]);
+        logger.error(String.valueOf(e.getStackTrace()[0]));
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
@@ -45,13 +49,13 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException e) {
-        System.out.println(e.getStackTrace()[0]);
+        logger.error(String.valueOf(e.getStackTrace()[0]));
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler(ProductOptionRequiredException.class)
     public ResponseEntity<String> productOptionRequiredException(ProductOptionRequiredException e) {
-        System.out.println(e.getStackTrace()[0]);
+        logger.error(String.valueOf(e.getStackTrace()[0]));
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
