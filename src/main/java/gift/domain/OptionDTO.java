ddc060af983e469eb48b90eff674fbf3f6ca4d00
peacefullyquietly <pjh73015@gmail.com>
2024-07-21 14:41:27 +0900

    refactor : 옵션 수량 최대 값 수정

--- src/main/java/gift/domain/OptionDTO.java
+++ src/main/java/gift/domain/OptionDTO.java
@@ -6,26 +6,20 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class OptionDTO {
-    private Long id;
 
     @NotNull
     @Size(min = 1, max = 50, message = "옵션의 이름은 최소 1자부터 최대 50자 미만입니다.")
     private String name;
 
     @Min(value = 1, message = "수량은 최소 1개 이상, 최대 1억개 미만입니다.")
-    @Max(value = 100_000_000, message = "수량은 최소 1개 이상, 최대 1억개 미만입니다.")
+    @Max(value = 100_000_001, message = "수량은 최소 1개 이상, 최대 1억개 미만입니다.")
     private int quantity;
 
     public OptionDTO() {}
 
-    public OptionDTO(Long id, String name, int quantity) {
-        this.id = id;
+    public OptionDTO(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
@@ -36,9 +30,6 @@ public class OptionDTO {
         return quantity;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public void setName(String name) {
         this.name = name;
