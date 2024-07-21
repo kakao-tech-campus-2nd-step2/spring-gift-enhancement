a2db4ed25b1f7ba6d73a6a20ebdfd5fa1a485e68
ez23re <ez23re@gmail.com>
2024-07-21 12:50:01 +0900

    fix : 잘못된 메소드명 수정

--- src/main/java/gift/member/service/MemberService.java
+++ src/main/java/gift/member/service/MemberService.java
@@ -26,12 +26,12 @@ public class MemberService {
 
     @Transactional
     public Member login(String email, String password) {
-        // 이메일 있는지
+        // 이메일 있는지 확인
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("옳지 않은 이메일 입니다."));
 
-        // 비밀번호가 맞는지
-        if (!member.getPassword().equals(password)) {
+        // 비밀번호가 맞는지 확인
+        if (!member.checkPassword(password)) {
             throw new IllegalArgumentException("옳지 않은 비밀번호 입니다.");
         }
 
