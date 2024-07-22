f15c1466c1ac06aadd7c2356744290288057b240
BOMIN LYU <rbm0524@naver.com>
2024-07-23 00:15:37 +0900

    refactor,feat : getAllOptionsByProductId, subtractOption 수정, getOptionId 추가

--- src/main/java/gift/Service/OptionService.java
+++ src/main/java/gift/Service/OptionService.java
@@ -3,6 +3,7 @@ package gift.Service;
 import gift.Entity.Option;
 import gift.Mapper.Mapper;
 import gift.Model.OptionDto;
+import gift.Model.OrderRequestDto;
 import gift.Repository.OptionJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,10 +24,15 @@ public class OptionService {
     }
 
     public List<OptionDto> getAllOptionsByProductId(long productId) {
-        List<Option> options = optionJpaRepository.findAllById(productId);
+        List<Option> options = optionJpaRepository.findAllByProductId(productId);
         return options.stream()
                 .map(mapper::optionToDto)
                 .collect(Collectors.toList());
+    }
+
+    public OptionDto getOptionById(long optionId) {
+        Option option = optionJpaRepository.findById(optionId).orElseThrow();
+        return mapper.optionToDto(option);
     }
 
     public void addOption(OptionDto optionDto) {
@@ -43,9 +49,10 @@ public class OptionService {
         optionJpaRepository.deleteById(id);
     }
 
-    public void subtractOption(OptionDto optionDto) {
-        Option option = optionJpaRepository.findById(optionDto.getId()).orElseThrow(); // 기존 Option 찾기
-        option.setQuantity(option.getQuantity() - optionDto.getQuantity()); //기존 수량에서 주문 수량을 빼준다.
+    public void subtractOption(OrderRequestDto orderRequestDto) {
+        if(orderRequestDto.getQuantity() <= 0) return; // 주문 수량이 0 이하인 경우 return
+        Option option = optionJpaRepository.findById(orderRequestDto.getOptionId()).orElseThrow(); // 기존 Option 찾기
+        option.setQuantity(option.getQuantity() - orderRequestDto.getQuantity()); //기존 수량에서 주문 수량을 빼준다.
         optionJpaRepository.save(option); // 뺀 이후 update
     }
 }
