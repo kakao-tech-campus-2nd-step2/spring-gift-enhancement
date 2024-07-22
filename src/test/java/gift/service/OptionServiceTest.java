2a50bc9b0d1d45ce5aa63c5a4f68cd3defafff4b
peacefullyquietly <pjh73015@gmail.com>
2024-07-22 15:11:25 +0900

    test : 옵션 수량 차감에 관한 테스트 추가

--- src/test/java/gift/service/OptionServiceTest.java
+++ src/test/java/gift/service/OptionServiceTest.java
@@ -5,6 +5,7 @@ import gift.entity.OptionEntity;
 import gift.entity.ProductEntity;
 import gift.repository.OptionRepository;
 import gift.repository.ProductRepository;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,7 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -126,5 +129,34 @@ public class OptionServiceTest {
         doNothing().when(optionRepository).delete(optionEntity);
 
         optionService.deleteOption(productId, optionId);
+    }
+
+    @Test
+    @DisplayName("없는 옵션")
+    void testSubtractOptionQuantityOptionNotFound() {
+        Long optionId = 1L;
+        when(optionRepository.findById(optionId)).thenReturn(Optional.empty());
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            optionService.subtractOptionQuantity(optionId, 3);
+        });
+
+        assertEquals("해당 옵션이 존재하지 않습니다. ID: 1", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("옵션 수량보다 더 많이 감소 시킬 때 예외 발생")
+    void testFailSubQuantity() {
+        // given
+        Long optionId = 1L;
+        OptionEntity optionEntity = new OptionEntity("Option1", 2);
+        given(optionRepository.findById(optionId)).willReturn(Optional.of(optionEntity));
+
+        // when, then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            optionService.subtractOptionQuantity(optionId, 5);
+        });
+
+        assertEquals("해당 옵션의 재고가 사용하려는 수량보다 부족합니다.", exception.getMessage());
     }
 }
