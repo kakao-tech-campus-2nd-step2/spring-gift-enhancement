bd7a5a68731a2ca95a199705ca52a6834796612d
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-22 11:04:31 +0900

    refactor: 외래키 표시

--- src/main/java/gift/entity/Option.java
+++ src/main/java/gift/entity/Option.java
@@ -16,6 +16,7 @@ public class Option {
     private Integer quantity;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "fk_option_product_id_ref_product_id"))
     private Product product;
 
     protected Option() {
