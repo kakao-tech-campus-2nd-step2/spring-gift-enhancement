dca5f1a254d295d61756de21cfaa763b28556992
Awhn <ahnshwan@outlook.com>
2024-07-22 12:01:32 +0900

    feat : deduct quantity by method

--- src/main/java/gift/repository/OptionRepository.java
+++ src/main/java/gift/repository/OptionRepository.java
@@ -2,9 +2,12 @@ package gift.repository;
 
 import gift.entity.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
     Optional<Option> findById(int id);
+    int searchQuantityById(int id);
+    int updateQuantityWithOptimisticLock(@Param("id") int id, @Param("newQuantity") int newQuantity, @Param("expectedQuantity") int expectedQuantity);
 }
