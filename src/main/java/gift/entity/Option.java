e227636c3f3024e303e0600b4516f9874f28cc26
GamJaDo <kim0615157@gmail.com>
2024-07-22 11:41:50 +0900

    style (로직을 entity에서 수행하도록 변경)

--- src/main/java/gift/entity/Option.java
+++ src/main/java/gift/entity/Option.java
@@ -1,6 +1,7 @@
 package gift.entity;
 
 import gift.dto.OptionResponse;
+import gift.exception.InvalidOptionException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,31 +14,31 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "options")
 public class Option {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false, length = 50)
 	private String name;
 
 	@Column(nullable = false)
 	private int quantity;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "product_id", nullable = false)
 	private Product product;
 
 	public Option(String name, int quantity, Product product) {
 		this.name = name;
-		this.quantity = quantity;
+		this.setQuantity(quantity);
 		this.product = product;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -49,12 +50,22 @@ public class Option {
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	public void setQuantity(int quantity) {
+		if (quantity < 0) {
+			throw new InvalidOptionException("Quantity cannot be less than ZERO.");
+		}
 		this.quantity = quantity;
 	}
-	
+
 	public OptionResponse toDto() {
 		return new OptionResponse(id, name, quantity);
+	}
+
+	public void decreaseQuantity(int quantity) {
+		if (quantity < 0) {
+            throw new InvalidOptionException("Decrease quantity cannot be less than ZERO.");
+        }
+        setQuantity(this.quantity - quantity);
 	}
 }
