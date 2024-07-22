280c2f602cd542a395182ea6ce6f0c51181f5e43
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-22 14:28:51 +0900

    test: 상품 엔티티 메서드 테스트

---
+++ src/test/java/gift/entity/ProductTest.java
@@ -0,0 +1,36 @@
+package gift.entity;
+
+import gift.exception.OptionDuplicateException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ProductTest {
+
+    @Test
+    @DisplayName("옵션 이름 중복 검사 - 같은 이름이 존재하여 실패")
+    void checkDuplicateOptionNameFail() {
+        //Given
+        Product product = new Product("name", 100, "url", new Category("Food", "t", "t", "t"), List.of(new Option("option", 100)));
+        String newOptionName = "option";
+
+        //When Then
+        assertThatThrownBy(() -> product.checkDuplicateOptionName(newOptionName))
+                .isInstanceOf(OptionDuplicateException.class);
+    }
+
+    @Test
+    @DisplayName("옵션 이름 중복 검사 - 성공")
+    void checkDuplicateOptionNameSuccess() {
+        //Given
+        Product product = new Product("name", 100, "url", new Category("Food", "t", "t", "t"), List.of(new Option("option", 100)));
+        String newOptionName = "newName";
+
+        //When Then
+        assertThatThrownBy(() -> product.checkDuplicateOptionName(newOptionName))
+                .isInstanceOf(OptionDuplicateException.class);
+    }
+}
