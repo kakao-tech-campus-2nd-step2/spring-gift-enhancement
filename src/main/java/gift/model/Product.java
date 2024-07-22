9699c35856668ea9bd36aeb9503b401c3bb88ef1
Thornappl2 <51190068+Thornappl2@users.noreply.github.com>
2024-07-22 10:37:14 +0900

    충남대 BE_이동현 4주차 과제 (2단계) (#389)
    * docs: README.md 수정
    
    * docs: README.md 수정
    
    * sql: schema, data sql 수정
    
    * docs: README.md 수정
    
    * feat: Option Entity, DTO 설정
    
    * feat: OptionRepository 구현
    
    * feat: Controller, Serivce 구현
    
    * feat: 옵션 이름 중복 불가 기능 추가
    
    * sql: 샘플 데이터 수정
    
    * feat: Option Entity, DTO 수정
    
    * feat: Product에 Option 적용
    
    * feat: 4주차 2단계 피드백 적용
    
    * refactor: 컨벤션 수정

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -8,6 +8,10 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Table(name = "product")
 public class Product {
@@ -35,14 +39,18 @@ public class Product {
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Option> options;
+
     public Product(){}
 
-    public Product(Long id, String name, int price, String imageUrl, Category category) {
+    public Product(Long id, String name, int price, String imageUrl, Category category, List<Option> options) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.category = category;
+        this.options = options;
     }
 
     public long getId(){
@@ -65,8 +73,23 @@ public class Product {
         return category;
     }
 
-    public static Product createWithId(Long id, String name, int price, String imageUrl, Category category) {
-        return new Product(id, name, price, imageUrl, category);
+    public List<Option> getOptions(){
+        return options;
+    }
+
+    public static Product createWithId(Long id, String name, int price, String imageUrl, Category category, List<Option> options) {
+        return new Product(id, name, price, imageUrl, category, options);
+    }
+
+    public void validateOptions() {
+        if (options != null) {
+            Set<String> optionNames = new HashSet<>();
+            for (Option option : options) {
+                if (!optionNames.add(option.getName())) {
+                    throw new IllegalArgumentException("Duplicate option name: " + option.getName());
+                }
+            }
+        }
     }
 
 }
