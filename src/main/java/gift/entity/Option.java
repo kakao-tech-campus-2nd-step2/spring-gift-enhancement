0df51ce1896ea9a4085800a970119515b5ee05ad
ez23re <ez23re@gmail.com>
2024-07-23 01:45:49 +0900

    옵션 관련 클래스 생성

---
+++ src/main/java/gift/entity/Option.java
@@ -0,0 +1,42 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Option(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+}
