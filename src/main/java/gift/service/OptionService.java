a6e1151df7e797ef798270b8a5d050d9b53f5a05
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-22 13:37:20 +0900

    refactor: 서비스계층에서 엔티티 값을 사용 안하도록 수정, 안쓰는 메서드 제거

--- src/main/java/gift/service/OptionService.java
+++ src/main/java/gift/service/OptionService.java
@@ -3,14 +3,10 @@ package gift.service;
 import gift.dto.request.OptionRequest;
 import gift.entity.Option;
 import gift.entity.Product;
-import gift.exception.InsufficientOptionQuantityException;
-import gift.exception.OptionDuplicateException;
 import gift.exception.OptionNotFoundException;
 import gift.repository.OptionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class OptionService {
@@ -21,32 +17,11 @@ public class OptionService {
         this.optionRepository = optionRepository;
     }
 
-    public void checkDuplicateOptionName(List<Option> existingOptions, String newOptionName) {
-        boolean isDuplicate = existingOptions.stream()
-                .anyMatch(option -> option.getName().equals(newOptionName));
-
-        if (isDuplicate) {
-            throw new OptionDuplicateException(newOptionName);
-        }
-    }
-
-    public Option checkOptionIdExist(Long targetOptionId, List<Option> options) {
-        for (Option option : options) {
-            if (option.getId().equals(targetOptionId)) {
-                return option;
-            }
-        }
-        throw new OptionNotFoundException(targetOptionId);
-    }
-
     @Transactional
     public void subtractOptionQuantity(Long targetOptionId, int subtractQuantity) {
         Option targetOption = optionRepository.findByIdWithPessimisticWriteLock(targetOptionId)
                 .orElseThrow(() -> new OptionNotFoundException(targetOptionId));
 
-        if (targetOption.getQuantity() < subtractQuantity) {
-            throw new InsufficientOptionQuantityException(subtractQuantity);
-        }
         targetOption.subtract(subtractQuantity);
     }
 
